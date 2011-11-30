f0836ac6bdea6d67f8cfd1267f1095a505cc4063
Davert <davert@ukr.net>
2011-11-30 22:56:56 +0200

    fixed packager

--- bin/build_phar.php
+++ bin/build_phar.php
@@ -1,11 +1,12 @@
 #!/usr/bin/env php
 <?php
 
-if (file_exists('../package/codecept.phar')) unlink('../package/codecept.phar');
+if (file_exists(__DIR__.'/../package/codecept.phar')) unlink(__DIR__.'/../package/codecept.phar');
 
-$p = new Phar('../package/codecept.phar');
+$p = new Phar(__DIR__.'/../package/codecept.phar');
 $p->startBuffering();
-$p->buildFromDirectory('..','~\.php$~');
-$p->setStub(file_get_contents('../package/stub.php'));
+$p->buildFromDirectory(__DIR__.'/..','~\.php$~');
+$p->addFile(__DIR__.'/../codecept','codecept');
+$p->setStub(file_get_contents(__DIR__.'/../package/stub.php'));
 $p->stopBuffering();
 $p->compressFiles(Phar::GZ);
