6fe09a710f2ac990b8cd09e20e1281abb6b08c75
Aleksey Asiutin <sutok85@gmail.com>
2011-12-01 22:12:58 +0200

    symfony module documentation updated

--- src/Codeception/Module/PhpBrowser.php
+++ src/Codeception/Module/PhpBrowser.php
@@ -2,6 +2,21 @@
 
 namespace Codeception\Module;
 
+/**
+ * Uses Mink (http://mink.behat.org) with PHP Goutte browser to interact with your application.
+ * Contains all Mink actions with additional ones, listed below.
+ *
+ * Use to perform acceptance tests with non-javascript browser.
+ *
+ * If test fails stores last shown page in 'output' dir.
+ *
+ * __Configuration__
+ *
+ * * start *required* - the url of your app
+ * * output - dir were last shown page should be stored on fail.
+ *
+ */
+
 class PhpBrowser extends \Codeception\Util\Mink {
 
     protected $requiredFields = array('start', 'output');
@@ -13,6 +28,46 @@ class PhpBrowser extends \Codeception\Util\Mink {
         $this->session = $mink->getSession('primary');
     }
 
+    /**
+     * Submits a form located on page.
+     * Specify the form by it's css or xpath selector.
+     * Fill the form fields values as array.
+     *
+     * Skipped fields will be filled by their values from page.
+     * You don't need to click the 'Submit' button afterwards.
+     * This command itself triggers the request to form's action.
+     *
+     * Examples:
+     *
+     * ``` php
+     * <?php
+     * $I->submitForm('#login', array('login' => 'davert', 'password' => '123456'));
+     *
+     * ```
+     *
+     * For sample Sign Up form:
+     *
+     * ``` html
+     * <form action="/sign_up">
+     *     Login: <input type="text" name="user[login]" /><br/>
+     *     Password: <input type="password" name="user[password]" /><br/>
+     *     Do you agree to out terms? <input type="checkbox" name="user[agree]" /><br/>
+     *     Select pricing plan <select name="plan"><option value="1">Free</option><option value="2" selected="selected">Paid</option></select>
+     *     <input type="submit" value="Submit" />
+     * </form>
+     * ```
+     * I can write this:
+     *
+     * ``` php
+     * <?php
+     * $I->submitForm('#userForm', array('user' => array('login' => 'Davert', 'password' => '123456', 'agree' => true)));
+     *
+     * ```
+     * Note, that pricing plan will be set to Paid, as it's selected on page.
+     *
+     * @param $selector
+     * @param $params
+     */
     public function submitForm($selector, $params) {
 
         $form = $this->session->getPage()->find('css',$selector);
@@ -39,17 +94,44 @@ class PhpBrowser extends \Codeception\Util\Mink {
         $url = $form->getAttribute('action');
         $method = $form->getAttribute('method');
 
-
         $this->call($url, $method, $params);
     }
 
+    /**
+      * If your page triggers an ajax request, you can perform it manually.
+      * This action sends a POST ajax request with specified params.
+      * Additional params can be passed as array.
+      *
+      * Example:
+      *
+      * Imagine that by clicking checkbox you trigger ajax request which updates user settings.
+      * We emulate that click by running this ajax request manually.
+      *
+      * ``` php
+      * <?php
+      * $I->sendAjaxPostRequest('/updateSettings', array('notifications' => true); // POST
+      * $I->sendAjaxGetRequest('/updateSettings', array('notifications' => true); // GET
+      *
+      * ```
+      *
+      * @param $uri
+      * @param $params
+      */
     public function sendAjaxPostRequest($uri, $params) {
         $this->session->setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         $this->call($uri, 'post', $params);
         $this->debug($this->session->getPage()->getContent());
     }
 
-
+    /**
+     * If your page triggers an ajax request, you can perform it manually.
+     * This action sends a GET ajax request with specified params.
+     *
+     * See ->sendAjaxPostRequest for examples.
+     *
+     * @param $uri
+     * @param $params
+     */
     public function sendAjaxGetRequest($uri, $params) {
         $this->session->setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         $this->call($uri, 'get', $params);
