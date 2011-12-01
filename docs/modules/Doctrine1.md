4661ad87606936c8e962b29fb98e5655fda09fef
Davert <davert@ukr.net>
2011-12-01 21:45:15 +0200

    symfony module documentation updated

---
+++ docs/modules/Doctrine1.md
@@ -0,0 +1,9 @@
+# Doctrine1
+
+### seeInDatabase
+
+__not documented__
+
+### dontSeeInDatabase
+
+__not documented__
--- src/Codeception/Module/Symfony1.php
+++ src/Codeception/Module/Symfony1.php
@@ -1,6 +1,24 @@
 <?php
 namespace Codeception\Module;
 
+/**
+ * Module that interacts with Symfony 1.4 applications.
+ * Replaces functional testing framework from symfony.
+ * Authorization features uses Doctrine and sfDoctrineGuardPlugin.
+ *
+ * Uses native symfony connections and test classes.
+ * Provides additional invormations on every actions.
+ *
+ * If test fails stores last shown page in 'output' dir.
+ *
+ * __Configuration__
+ *
+ * * app *required* - application you want to test. In most cases it will be 'frontend'
+ * * output - dir were last shown page should be stored. Will be stored to 'sf_log_dir' log path If none specified
+ *
+ *
+ *
+ */
 
 class Symfony1 extends \Codeception\Module
 {
@@ -96,6 +114,11 @@ class Symfony1 extends \Codeception\Module
         }
     }
 
+    /**
+     * Opens the page.
+     *
+     * @param $page
+     */
     public function amOnPage($page)
     {
         $this->browser->get($page);
@@ -103,6 +126,12 @@ class Symfony1 extends \Codeception\Module
         $this->followRedirect();
     }
 
+    /**
+     * Click on link or button and move to next page.
+     * Either link text, css selector, or xpath can be passed
+     *
+     * @param $link
+     */
     public function click($link)
     {
         $this->browser->click($link);
@@ -110,12 +139,38 @@ class Symfony1 extends \Codeception\Module
         $this->followRedirect();
     }
 
+    /**
+     * Check if current page doesn't contain the text specified.
+     * Specify the css selector to match only specific region.
+     *
+     * Examples:
+     * ```` php
+     * $I->dontSee('Login'); // I can suppose user is already logged in
+     * $I->dontSee('Sign Up','h1'); // I can suppose it's not a signup page
+     * ````
+     *
+     * @param $text
+     * @param null $selector
+     */
     public function dontSee($text, $selector = null)
     {
         $res = $this->proceedSee($text, $selector);
         $this->assertNot($res);
     }
 
+    /**
+     * Check if current page contains the text specified.
+     * Specify the css selector to match only specific region.
+     *
+     * Examples:
+     * ```` php
+     * $I->see('Logout'); // I can suppose user is logged in
+     * $I->see('Sign Up','h1'); // I can suppose it's a signup page
+     * ````
+     *
+     * @param $text
+     * @param null $selector
+     */
     public function see($text, $selector = null)
     {
         $res = $this->proceedSee($text, $selector);
@@ -148,6 +203,19 @@ class Symfony1 extends \Codeception\Module
         return array('contains', $text, strip_tags($this->browser->getResponse()->getContent()), "'$text' in " . $response . '. For more details look page snapshot in the log directory');
     }
 
+    /**
+     * Checks if there is a link with text specified.
+     * Specify url to match link with exact this url.
+     *
+     * Examples:
+     * ```` php
+     * $I->seeLink('Logout'); // matches <a href="#">Logout</a>
+     * $I->seeLink('Logout','/logout'); // matches <a href="/logout">Logout</a>
+     * ````
+     *
+     * @param $text
+     * @param null $url
+     */
     public function seeLink($text, $url = null)
     {
         if (!$url) return $this->see($text, 'a');
@@ -159,6 +227,18 @@ class Symfony1 extends \Codeception\Module
         }
     }
 
+    /**
+     * Checks if page doesn't contain the link with text specified.
+     * Specify url to narrow the results.
+     *
+     * Examples:
+     * ```` php
+     * $I->dontSeeLink('Logout'); // I suppose user is not logged in
+     * ````
+     *
+     * @param $text
+     * @param null $url
+     */
     public function dontSeeLink($text, $url = null)
     {
         if (!$url) return $this->dontSee($text, 'a');
@@ -170,12 +250,36 @@ class Symfony1 extends \Codeception\Module
         }
     }
 
+    /**
+     * Assert if the specified checkbox is checked.
+     * Use css selector or xpath to match.
+     *
+     * Example:
+     * ```` php
+     * $I->seeCheckboxIsChecked('#agree'); // I suppose user agreed to terms
+     * $I->seeCheckboxIsChecked('#signup_form input[type=checkbox]'); // I suppose user agreed to terms, If there is only one checkbox in form.
+     * ````
+     *
+     * @param $selector
+     */
     public function seeCheckboxIsChecked($selector)
     {
         $res = $this->proceedCheckboxIsChecked($selector);
         $this->assert($res);
     }
 
+    /**
+     * Assert if the specified checkbox is unchecked.
+     * Use css selector or xpath to match.
+     *
+     * Example:
+     * ```` php
+     * $I->dontSeeCheckboxIsChecked('#agree'); // I suppose user didn't agree to terms
+     * $I->seeCheckboxIsChecked('#signup_form input[type=checkbox]'); // I suppose user didn't check the first checkbox in form.
+     * ````
+     *
+     * @param $selector
+     */
     public function dontSeeCheckboxIsChecked($selector)
     {
         $res = $this->proceedCheckboxIsChecked($selector);
@@ -185,11 +289,14 @@ class Symfony1 extends \Codeception\Module
     protected function proceedCheckboxIsChecked($selector)
     {
         $node = $this->browser->getResponseDomCssSelector()->matchSingle($selector)->getNode();
-        if (!$node) return PHPUnit_Framework_Assert::fail("there is no element $selector on page");
+        if (!$node) return \PHPUnit_Framework_Assert::fail("there is no element $selector on page");
         if ($node->getAttribute('type') != 'checkbox') return PHPUnit_Framework_Assert::fail("there is no checkbox $selector on page");
         return array('equals', $node->getAttribute('checked'), 'checked', "that checkbox $selector is checked");
     }
 
+    /**
+     * Checks if there were at least one email sent through Symfony test mailer.
+     */
     public function seeEmailReceived()
     {
         $this->debug('Emails sent: ' . $this->browser->getContext()->getMailer()->getLogger()->countMessages());
@@ -197,6 +304,38 @@ class Symfony1 extends \Codeception\Module
     }
 
 
+    /**
+     * Submits a form located on page.
+     * Specify the form by it's css or xpath selector.
+     * Fill the form fields values as array.
+     *
+     * Skipped fields will be filled by their values from page.
+     * You don't need to click the 'Submit' button afterwards.
+     * This command itself triggers the request to form's action.
+     *
+     * Examples:
+     * ```` php
+     * $I->submitForm('#login', array('login' => 'davert', 'password' => '123456'));
+     * ````
+     *
+     * For sample Sign Up form:
+     * ```` html
+     * <form action="/sign_up">
+     *     Login: <input type="text" name="user[login]" /><br/>
+     *     Password: <input type="password" name="user[password]" /><br/>
+     *     Do you agree to out terms? <input type="checkbox" name="user[agree]" /><br/>
+     *     Select pricing plan <select name="plan"><option value="1">Free</option><option value="2" selected="selected">Paid</option></select>
+     *     <input type="submit" value="Submit" />
+     * </form>
+     *
+     * ```` php
+     * $I->submitForm('#userForm', array('user' => array('login' => 'Davert', 'password' => '123456', 'agree' => true)));
+     * ````
+     * Note, that pricing plan will be set to Paid, as it's selected on page.
+     *
+     * @param $selector
+     * @param $params
+     */
     public function submitForm($selector, $params)
     {
 
@@ -233,6 +372,24 @@ class Symfony1 extends \Codeception\Module
         $this->call($url, $method, $params);
     }
 
+    /**
+     * If your page triggers an ajax request, you can perform it manually.
+     * This action sends a POST ajax request with specified params.
+     * Additional params can be passed as array.
+     *
+     * Example:
+     *
+     * Imagine that by clicking checkbox you trigger ajax request which updates user settings.
+     * We emulate that click by running this ajax request manually.
+     *
+     * ```` php
+     * $I->sendAjaxPostRequest('/updateSettings', array('notifications' => true); // POST
+     * $I->sendAjaxGetRequest('/updateSettings', array('notifications' => true); // GET
+     * ````
+     *
+     * @param $uri
+     * @param $params
+     */
     public function sendAjaxPostRequest($uri, $params)
     {
         $this->browser->setHttpHeader('X-Requested-With', 'XMLHttpRequest');
@@ -241,6 +398,15 @@ class Symfony1 extends \Codeception\Module
     }
 
 
+    /**
+     * If your page triggers an ajax request, you can perform it manually.
+     * This action sends a GET ajax request with specified params.
+     *
+     * See ->sendAjaxPostRequest for examples.
+     *
+     * @param $uri
+     * @param $params
+     */
     public function sendAjaxGetRequest($uri, $params)
     {
         $this->browser->setHttpHeader('X-Requested-With', 'XMLHttpRequest');
@@ -248,12 +414,22 @@ class Symfony1 extends \Codeception\Module
         $this->debug($this->browser->getResponse()->getContent());
     }
 
+    /**
+     * Emulates click on form's submit button.
+     * You don't need that action if you fill form by ->submitForm action.
+     *
+     * @param $selector
+     */
 
-    public function clickSubmitButton($selector, $data = array())
+    public function clickSubmitButton($selector)
     {
-        $this->browser->click($selector, $data);
+        $this->browser->click($selector);
     }
 
+    /**
+     * Performs validation of Symfony1 form.
+     * Matches the first sfForm instance from controller and returns isValid() value.
+     */
     public function seeFormIsValid()
     {
         $form = $this->getForm();
@@ -261,6 +437,11 @@ class Symfony1 extends \Codeception\Module
         \PHPUnit_Framework_Assert::isFalse($form->hasErrors(), ', seems like there were errors');
     }
 
+    /**
+     * Performs validation of Symfony1 form.
+     * Matches the first sfForm instance from controller and returns getErrorSchema() values.
+     * Shows all errors in debug.
+     */
     public function seeErrorsInForm()
     {
         $form = $this->getForm();
@@ -271,6 +452,13 @@ class Symfony1 extends \Codeception\Module
         }
     }
 
+    /**
+     * Checks for invalid value in Symfony1 form.
+     * Matches the first sfForm instance from controller and returns getErrorSchema() values.
+     * Specify field which should contain error message.
+     *
+     * @param $field
+     */
     public function seeErrorInField($field)
     {
         $form = $this->getForm();
@@ -287,7 +475,7 @@ class Symfony1 extends \Codeception\Module
 
         foreach ($action->getVarHolder()->getAll() as $name => $value)
         {
-            if ($value instanceof sfForm && $value->isBound()) {
+            if ($value instanceof \sfForm && $value->isBound()) {
                 $form = $value;
                 break;
             }
@@ -296,6 +484,14 @@ class Symfony1 extends \Codeception\Module
         return $form;
     }
 
+    /**
+     * Sign's user in with sfGuardAuth.
+     * Uses standard path: /sfGuardAuth/signin for authorization.
+     * Provide username and password.
+     *
+     * @param $username
+     * @param $password
+     */
     public function signIn($username, $password)
     {
         $this->browser->post('/sfGuardAuth/signin', array('signin' => array('username' => $username, 'password' => $password)));
@@ -306,17 +502,37 @@ class Symfony1 extends \Codeception\Module
         $this->followRedirect();
     }
 
+    /**
+     * Sign out is performing by triggering '/logout' url.
+     *
+     */
+
     public function signOut()
     {
         $this->browser->get('/logout');
         $this->followRedirect();
     }
 
+    /**
+     * Log in as sfGuardUser.
+     * Only name of user should be provided.
+     *
+     * @alias amLoggedAs
+     * @param $name
+     */
     public function am($name)
     {
         $this->amLoggedAs($name);
     }
 
+    /**
+     * Log in as sfDoctrineGuardUser.
+     * Only name of user should be provided.
+     * Fetches user by it's username from sfGuardUser table.
+     *
+     * @param $name
+     * @throws \Exception
+     */
     public function amLoggedAs($name)
     {
         if (!class_exists('Doctrine')) throw new \Exception('Doctrine is not installed. Consider using \'signIn\' action instead');
