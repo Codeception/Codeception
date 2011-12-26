1d665d6e91cd7784ea3ad6717d2e8328326df5be
Aleksey Asiutin <sutok85@gmail.com>
2011-12-26 15:00:29 +0200

    installation instructions updated

---
+++ bin/build.php
@@ -0,0 +1,5 @@
+<?php
+chdir(__DIR__.'/..');
+system('php bin/update_docs.php');
+system('php bin/build_pear.php');
+system('php bin/build_phar.php');
--- codecept.bat
+++ codecept.bat
@@ -5,4 +5,4 @@ GOTO RUN
 :USE_PEAR_PATH
 set PHPBIN=%PHP_PEAR_PHP_BIN%
 :RUN
-"%PHPBIN%" "@bin_dir@\codecept" %*
+"%PHPBIN%" "codecept" %*
