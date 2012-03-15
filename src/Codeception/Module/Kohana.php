029e769771275fb1d30285de473bc46d0c9aaff8
Aleksey Asiutin <sutok85@gmail.com>
2012-03-15 17:53:37 +0700

    fixes

--- src/Codeception/Module/Kohana.php
+++ src/Codeception/Module/Kohana.php
@@ -1,5 +1,18 @@
 <?
-
+/**
+ * This module provides integration with [Kohana](http://kohanaframework.org/) v3.
+ * Current status PRE ALFA=)
+ * Functional tests can be run inside Kohana. All commands of this module are just the same as in other modules that share Framework interface.
+ *
+ * INSTALLATION
+ * 0 Thi smodule set $_SERVER['KOHANA_ENV'] = 'testing'
+ * 1 fix your bootstrap/index.php like this https://gist.github.com/2043592
+ * 2 you need install this module https://github.com/nike-17/codeception-kohana 
+ *   or just fix your Cookie class like this https://github.com/nike-17/codeception-kohana/blob/master/classes/cookie.php 
+ * 3 if you have some problem pls feel free to ask me nike-17@ya.ru
+ * Module is created by [Nikita Groshin](nike-17@ya.ru)
+ * 
+ */
 namespace Codeception\Module;
 
 class Kohana extends \Codeception\Util\Framework implements \Codeception\Util\FrameworkInterface {
