ae6dcc90dbe26459bd87a804a14be316844117fd
ilex <ilex@ilex-thinknote.(none)>
2012-04-17 16:41:58 +0700

    Kohana connector was modified

--- src/Codeception/Util/Connector/Kohana.php
+++ src/Codeception/Util/Connector/Kohana.php
@@ -12,32 +12,35 @@ class Kohana extends \Symfony\Component\BrowserKit\Client {
 	}
 
 	public function doRequest($request) {
-		
+
 		$_COOKIE = $request->getCookies();
 		$_SERVER = $request->getServer();
 		$_FILES = $request->getFiles();
 
 		$uri = str_replace('http://localhost', '', $request->getUri());
 
-		if (strtoupper($request->getMethod()) == 'GET') {
-			$_GET = $request->getParameters();
-		}
-		
-		if (strtoupper($request->getMethod()) == 'POST') {
-			$_POST = $request->getParameters();
-		}
-
-	    $_SERVER['KOHANA_ENV'] = 'testing';
+		$_SERVER['KOHANA_ENV'] = 'testing';
 		$_SERVER['REQUEST_METHOD'] = strtoupper($request->getMethod());
 		$_SERVER['REQUEST_URI'] = strtoupper($uri);
 
 		$this->_initRequest();
-		
-		$request = \Request::factory($uri);
-		$request::initial = $request;
-		$content = $request->execute()->render();
 
-		$headers = headers_list();
+		$kohanaRequest = \Request::factory($uri);
+		$kohanaRequest->method($_SERVER['REQUEST_METHOD']);
+
+		if (strtoupper($request->getMethod()) == 'GET') {
+			$kohanaRequest->query($request->getParameters());
+		}
+		if (strtoupper($request->getMethod()) == 'POST') {
+			$kohanaRequest->post($request->getParameters());
+		}
+
+		$kohanaRequest->cookie($_COOKIE);
+
+		$kohanaRequest::$initial = $kohanaRequest;
+		$content = $kohanaRequest->execute()->render();
+
+		$headers = (array)$kohanaRequest->response()->headers();
 		$headers['Content-type'] = "text/html; charset=UTF-8";
 		$response = new Response($content, 200, $headers);
 		return $response;
