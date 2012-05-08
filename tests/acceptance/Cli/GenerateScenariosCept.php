af73fc0029f9f6f34fa41bdb6d6a39350aac8695
Davert <davert@ukr.net>
2012-05-08 11:25:06 +0300

    not fixed :( https://github.com/Codeception/Codeception/issues/16

--- tests/acceptance/Cli/GenerateScenariosCept.php
+++ tests/acceptance/Cli/GenerateScenariosCept.php
@@ -1,6 +1,6 @@
 <?php
 $I = new CliGuy($scenario);
-$I->wantTo('generate scenarios');
+$I->wantTo('generate scenarios сценарий');
 $I->amInPath('tests/data/sandbox');
 $I->executeCommand('generate:scenarios dummy');
 $I->seeFileFound('File_Exists.txt','tests/_data/scenarios');
