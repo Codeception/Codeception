9b583f9142ca31487ccc446a07918d061d827bbd
Aleksey Asiutin <sutok85@gmail.com>
2012-05-08 11:25:06 +0300

    not fixed :( https://github.com/Codeception/Codeception/issues/16

--- src/Codeception/Command/Base.php
+++ src/Codeception/Command/Base.php
@@ -5,4 +5,10 @@ use \Symfony\Component\Yaml\Yaml;
 
 class Base extends \Symfony\Component\Console\Command\Command
 {
+    protected function initialize($input, $output)
+    {
+//        if (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') {
+//            exec('chcp 65001');
+//        }
+    }
 }
