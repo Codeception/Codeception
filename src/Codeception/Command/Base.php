d376a4ab86fc7fa09e6c41f89c74aa4bb3807e74
Aleksey Asiutin <sutok85@gmail.com>
2012-05-09 04:11:39 +0300

    removed hook for windows encoding

--- src/Codeception/Command/Base.php
+++ src/Codeception/Command/Base.php
@@ -5,10 +5,4 @@ use \Symfony\Component\Yaml\Yaml;
 
 class Base extends \Symfony\Component\Console\Command\Command
 {
-    protected function initialize($input, $output)
-    {
-//        if (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') {
-//            exec('chcp 65001');
-//        }
-    }
 }
