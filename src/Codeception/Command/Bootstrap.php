cc5ae1d5884d46ccbca1f0ce5b676c0149490d27
Aleksey Asiutin <sutok85@gmail.com>
2012-06-08 02:02:44 +0300

    repack

--- src/Codeception/Command/Bootstrap.php
+++ src/Codeception/Command/Bootstrap.php
@@ -127,13 +127,13 @@ class Bootstrap extends \Symfony\Component\Console\Command\Command
             ),
         );
 
-        if ((strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') and file_exists('codecept.phar') and (!file_exists('codecept.bat'))) {
+        if ((strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') and file_exists('codecept') and (!file_exists('codecept.bat'))) {
             file_put_contents('codecept.bat','
             @echo off
             if "%PHPBIN%" == "" set PHPBIN=@php_bin@
             "%PHPBIN%" "codecept" %*
             ');
-            $output->writeln('batch file added');
+            $output->writeln('batch file was created added');
         }
 
         $str = $firstline;
