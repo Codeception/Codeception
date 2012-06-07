b62db823d491fe8c3ccd0d508a2f9d707b0f4fe6
Aleksey Asiutin <sutok85@gmail.com>
2012-06-07 23:08:50 +0300

    composer.json updated

--- bin/build_phar.php
+++ bin/build_phar.php
@@ -3,10 +3,31 @@
 
 if (file_exists(__DIR__.'/../package/codecept.phar')) unlink(__DIR__.'/../package/codecept.phar');
 
-$p = new Phar(__DIR__.'/../package/codecept.phar');
+// download composer
+$composer = file_get_contents('http://getcomposer.org/installer');
+chdir(__DIR__.'/../');
+@mkdir("package/phar");
+chdir('package/phar');
+file_put_contents('composer.phar', $composer);
+file_put_contents('composer.json','
+{
+    "require": {
+        "codeception/codeception":  "*",
+        "behat/mink-goutte-driver": "*",
+        "behat/mink-selenium-driver": "*",
+        "behat/mink-selenium2-driver": "*",
+        "behat/mink-sahi-driver": "*",
+        "behat/mink-zombie-driver": "*"
+    }
+}
+');
+
+system('php composer.phar install');
+system('php composer.phar install');
+
+$p = new Phar('codecept.phar');
 $p->startBuffering();
-$p->buildFromDirectory(__DIR__.'/..','~\.php$~');
-$p->addFile(__DIR__.'/../codecept','codecept');
+$p->buildFromDirectory(__DIR__.'/../vendor','~\.php$~');
 $p->setStub(file_get_contents(__DIR__.'/../package/stub.php'));
 $p->stopBuffering();
 $p->compressFiles(Phar::GZ);
