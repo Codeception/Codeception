a0dfc1759e274e0d6366a00bec715d8a56ea0ffb
Tima Maslyuchenko <dudapip3@gmail.com>
2012-06-25 16:11:31 +0300

    prevent resetInstance call if frontcontroller is not defined

--- src/Codeception/Module/ZF1.php
+++ src/Codeception/Module/ZF1.php
@@ -105,7 +105,10 @@ class ZF1 extends \Codeception\Util\Framework implements \Codeception\Util\Frame
         $_GET     = array();
         $_POST    = array();
         $_COOKIE  = array();
-        $this->bootstrap->getBootstrap()->getResource('frontcontroller')->resetInstance();
+        $fc = $this->bootstrap->getBootstrap()->getResource('frontcontroller');
+        if ($fc) {
+            $fc->resetInstance();
+        }
         \Zend_Layout::resetMvcInstance();
         \Zend_Controller_Action_HelperBroker::resetHelpers();
         \Zend_Session::$_unitTestEnabled = true;
