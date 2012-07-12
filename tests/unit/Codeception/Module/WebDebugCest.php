91b305bf8b0a1aef8270d003944568ccfcd793f9
Davert <davert@ukr.net>
2012-07-12 11:41:12 +0300

    tests for webdebug added

---
+++ tests/unit/Codeception/Module/WebDebugCest.php
@@ -0,0 +1,30 @@
+<?php
+use Codeception\Util\Stub;
+
+class WebDebugCest {
+    public $class = '\Codeception\Module\WebDebug';
+
+    public function makeAScreenshot(CodeGuy $I) {
+        $I->wantTo('save screenshot');
+        $I->haveStub($selenium = Stub::makeEmpty('\Codeception\Module\Selenium'));
+        $I->haveFakeClass($stub = Stub::make($this->class, array(
+            'test' => Stub::makeEmpty('\Codeception\TestCase\Cept', array('getFileName' => function () { return 'testtest'; })),
+            'module' => $selenium ))
+        );
+        $I->executeTestedMethodOn($stub)
+            ->seeMethodInvoked($selenium, '_saveScreenshot')
+            ->seeMethodNotInvoked($stub, 'debug');
+    }
+
+    public function generateFilename(CodeGuy $I) {
+        $I->haveFakeClass($stub = Stub::make($this->class, array(
+                'test' => Stub::makeEmpty('\Codeception\TestCase\Cept', array('getFileName' => function () { return 'testtest'; })),
+        )));
+        $I->executeTestedMethod($stub);
+        $I->seeResultEquals(\Codeception\Configuration::logDir().'debug'.DIRECTORY_SEPARATOR.'testtest - 1');
+        $I->executeTestedMethod($stub, 'mytest');
+        $I->seeResultEquals(\Codeception\Configuration::logDir().'debug'.DIRECTORY_SEPARATOR.'testtest - 2 - mytest');
+
+
+    }
+}
