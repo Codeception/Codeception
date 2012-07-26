fa126cd5ac8714a6b7614bbe13e247f14a1b6847
Aleksey Asiutin <sutok85@gmail.com>
2012-07-26 18:31:17 +0300

    fix mine bug with incorrect log path

--- src/Codeception/Configuration.php
+++ src/Codeception/Configuration.php
@@ -66,7 +66,7 @@ class Configuration
     {
         $exceptionClass = class_exists('\ConfigurationException') ? '\ConfigurationException' : '\Exception';
         if (!self::$logDir) throw new $exceptionClass("Path for logs not specified. Please, set log path in global config");
-        $dir = realpath(self::$dir . DIRECTORY_SEPARATOR . self::$logDir . DIRECTORY_SEPARATOR);
+        $dir = realpath(self::$dir . DIRECTORY_SEPARATOR . self::$logDir) . DIRECTORY_SEPARATOR;
         if (!is_writable($dir)) throw new $exceptionClass("Path for logs is not writable. Please, set appropriate access mode for log path.");
         return;
     }
