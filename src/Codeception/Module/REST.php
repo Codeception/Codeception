5eb270d0589dd6d389293f42f5cae7c82cb0491f
tiger-seo <tiger.seo@gmail.com>
2012-08-19 12:34:57 +0300

    small fix to Rest->grabDataFromJsonResponse() phpDoc information

--- src/Codeception/Module/REST.php
+++ src/Codeception/Module/REST.php
@@ -237,8 +237,10 @@ class REST extends \Codeception\Module
      *
      * @param string $path
      *
-     * @version 1.1.2
+     * @since 1.1.2
      * @return string
+     *
+     * @author tiger.seo@gmail.com
      */
     public function grabDataFromJsonResponse($path)
     {
