26abe2187b1b23676c5049cec0f33e566ff97dec
Aleksey Asiutin <sutok85@gmail.com>
2012-09-13 19:22:29 +0300

    phar buld updated

--- bin/build_phar.php
+++ bin/build_phar.php
@@ -3,12 +3,15 @@
 if (file_exists(__DIR__.'/../package/codecept.phar')) unlink(__DIR__.'/../package/codecept.phar');
 
 $root = __DIR__.'/../';
-require $root.'/autoload.php';
+require $root.'/vendor/autoload.php';
+require $root.'/package/Compiler.php';
+
 
 // download composer
 chdir(__DIR__.'/../');
 @mkdir("package/phar");
 chdir('package/phar');
+@unlink('codecep.phar');
 file_put_contents('composer.phar', file_get_contents('http://getcomposer.org/installer'));
 file_put_contents('composer.json','
 {
@@ -22,18 +25,9 @@ file_put_contents('composer.json','
 system('php composer.phar install');
 system('php composer.phar install');
 
-$p = new Phar('codecept.phar');
-$p->startBuffering();
-$p->buildFromDirectory(__DIR__.'/../package/phar/vendor','~\.html.dist$~');
-$p->buildFromDirectory(__DIR__.'/../package/phar/vendor','~\.tpl.dist$~');
-$p->buildFromDirectory(__DIR__.'/../package/phar/vendor','~\.php$~');
-$p->buildFromDirectory(__DIR__.'/../package/phar/vendor','~\.js$~');
-$p->addFile(__DIR__.'/../package/bin','codecept');
-$p->setStub(file_get_contents(__DIR__.'/../package/stub.php'));
-$p->stopBuffering();
-$p->compressFiles(Phar::GZ);
 
-copy('codecept.phar', __DIR__.'/../package/codecept.phar');
+$compiler = new \Codeception\Compiler($root.'/pacakge/phar');
+$compiler->compile($root.'/package/codecept.phar');
 
 chdir('..');
 ob_start();
@@ -42,4 +36,6 @@ ob_start();
 @system('rm -rf phar');
 ob_clean();
 
-echo "PHAR build succesfull";
+echo system('php codecept.phar');
+
+echo "PHAR build finished";
