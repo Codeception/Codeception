c762b5fed17e60dc19458e274e266fa29dc39d1d
Aleksey Asiutin <sutok85@gmail.com>
2012-09-15 12:17:49 +0300

    pear package build improved

--- tests/data/claypit/tests/_helpers/DumbHelper.php
+++ tests/data/claypit/tests/_helpers/DumbHelper.php
@@ -1,8 +1,6 @@
 <?php
 namespace Codeception\Module;
 
-require_once 'PHPUnit/Framework/Assert/Functions.php';
-
 // here you can define custom functions for DumbGuy 
 
 class DumbHelper extends \Codeception\Module
