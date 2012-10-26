6b02264e4a38d20a5ae2dface19a02450bea69de
tiger-seo <tiger.seo@gmail.com>
2012-10-26 07:16:25 +0300

    fix PSR-2 violations

--- src/Codeception/Util/FileSystem.php
+++ src/Codeception/Util/FileSystem.php
@@ -7,21 +7,19 @@ namespace Codeception\Util;
  */
 class FileSystem
 {
-	static public function doEmptyDir($path)
-	{
-		$iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path),
-													\RecursiveIteratorIterator::CHILD_FIRST);
+    public static function doEmptyDir($path)
+    {
+        $iterator = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($path),
+            \RecursiveIteratorIterator::CHILD_FIRST
+        );
 
-		foreach ($iterator as $path)
-		{
-			if ($path->isDir())
-			{
-				rmdir($path->__toString());
-			}
-			else
-			{
-				unlink($path->__toString());
-			}
-		}
-	}
+        foreach ($iterator as $path) {
+            if ($path->isDir()) {
+                rmdir($path->__toString());
+            } else {
+                unlink($path->__toString());
+            }
+        }
+    }
 }
