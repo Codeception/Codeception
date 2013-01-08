dc9780fe332566f726f5830a14ecb09a0745f7a7
Davert <davert@ukr.net>
2013-01-08 02:09:23 +0200

    tests refactored

--- tests/helpers/CliHelper.php
+++ tests/helpers/CliHelper.php
@@ -1,22 +1,8 @@
 <?php
 namespace Codeception\Module;
 
-// here you can define custom functions for TestGuy 
+// here you can define custom functions for CliGuy 
 
 class CliHelper extends \Codeception\Module
 {
-
-    public function _before(\Codeception\TestCase $test) {
-        $this->getModule('Filesystem')->copyDir(\Codeception\Configuration::dataDir().'claypit', \Codeception\Configuration::dataDir().'sandbox');
-    }
-
-    public function _after(\Codeception\TestCase $test) {
-        $this->getModule('Filesystem')->deleteDir(\Codeception\Configuration::dataDir().'sandbox');
-        chdir(\Codeception\Configuration::projectDir());
-    }
-    
-    public function executeCommand($command) {
-        $this->getModule('Cli')->runShellCommmand('php '.\Codeception\Configuration::projectDir().'codecept '.$command);
-    }
-
 }
