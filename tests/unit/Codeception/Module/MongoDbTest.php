786a70c57887562c34ebc8722d3b14575e64412b
Davert <davert@mail.ua>
2013-01-15 23:02:03 +0200

    MongoDb module test added

--- tests/unit/Codeception/Module/MongoDbTest.php
+++ tests/unit/Codeception/Module/MongoDbTest.php
@@ -2,19 +2,58 @@
 
 use Codeception\Util\Stub;
 
-class MongoDbTest extends \Codeception\TestCase\Test
+class MongoDbTest extends \PHPUnit_Framework_TestCase
 {
 
-    protected $mongo;
+    /**
+     * @var \Codeception\Module\MongoDb
+     */
+    protected $module;
+
+    /**
+     * @var \MongoDb
+     */
+    protected $db;
 
     protected function setUp()
     {
-        $this->mongo = new \Codeception\Module\MongoDb();
-        $this->
+        if (!class_exists('\Mongo')) $this->markTestSkipped('Mongo is not installed');
+
+        $mongo = new \Mongo();
+
+        $this->module = new \Codeception\Module\MongoDb();
+        $this->module->_setConfig(array(
+                'dsn' => 'mongodb://localhost:27017/test',
+                'user' => '',
+                'password' => ''
+        ));
+        $this->module->_initialize();
+
+        $this->db = $mongo->selectDB('test');
+        $userCol = $this->db->createCollection('users');
+        $userCol->insert(array('id' => 1, 'email' => 'miles@davis.com'));
+    }
+
+    protected function tearDown()
+    {
+        $this->db->dropCollection('users');
     }
 
     public function testSeeInCollection()
     {
-
+        $this->module->seeInCollection('users', array('email' => 'miles@davis.com'));
     }
+
+    public function testDontSeeInCollection()
+    {
+        $this->module->dontSeeInCollection('users', array('email' => 'davert@davert.com'));
+    }
+
+    public function testGrabFromCollection()
+    {
+        $user = $this->module->grabFromCollection('users', array('id' => 1));
+        $this->assertTrue(isset($user['email']));
+        $this->assertEquals('miles@davis.com',$user['email']);
+    }
+
 }
