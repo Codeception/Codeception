a9dbeaa5fb006b76fd9197179cabd88f56c15293
Aleksey Asiutin <sutok85@gmail.com>
2013-01-15 22:26:08 +0200

    autoload phar and composer conflict fixed

--- tests/helpers/CoverHelper.php
+++ tests/helpers/CoverHelper.php
@@ -7,6 +7,6 @@ class CoverHelper extends \Codeception\Module
 {
     
     public function _before(\Codeception\TestCase $test) {
-        if (strpos(PHP_VERSION, '5.3')===0) $test->markTestSkipped();
+        if (floatval(phpversion()) == '5.3') $test->markTestSkipped();
     }
 }
---
+++ tests/unit/Codeception/Module/MongoDbTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Codeception\Util\Stub;
+
+class MongoDbTest extends \Codeception\TestCase\Test
+{
+
+    protected $mongo;
+
+    protected function setUp()
+    {
+        $this->mongo = new \Codeception\Module\MongoDb();
+        $this->
+    }
+
+    public function testSeeInCollection()
+    {
+
+    }
+}
