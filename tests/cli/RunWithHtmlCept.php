925305522d50fdaeabef4d447aca0547b80c93c4
Davert <davert@mail.ua>
2013-01-16 12:09:33 +0200

    skipping run tests for php5.3

--- tests/cli/RunWithHtmlCept.php
+++ tests/cli/RunWithHtmlCept.php
@@ -1,6 +1,9 @@
 <?php
+
 $I = new CliGuy($scenario);
 $I->wantTo('check xml reports');
 $I->amInPath('tests/data/sandbox');
 $I->executeCommand('run --html');
 $I->seeFileFound('report.html','tests/_log');
+
+if (floatval(phpversion()) == '5.3') $this->markTestSkipped();
--- tests/cli/RunWithJsonCept.php
+++ tests/cli/RunWithJsonCept.php
@@ -6,3 +6,5 @@ $I->executeCommand('run --json');
 $I->seeFileFound('report.json','tests/_log');
 $I->seeInThisFile('"suite":');
 $I->seeInThisFile('"dummy"');
+
+if (floatval(phpversion()) == '5.3') $this->markTestSkipped();
