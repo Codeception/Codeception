100659924dc5090fd8916dfdc1e462dc6b47a02c
Aleksey Asiutin <sutok85@gmail.com>
2013-01-16 12:09:33 +0200

    skipping run tests for php5.3

--- tests/cli/RunWithTapCept.php
+++ tests/cli/RunWithTapCept.php
@@ -4,3 +4,5 @@ $I->wantTo('check tap reports');
 $I->amInPath('tests/data/sandbox');
 $I->executeCommand('run --tap');
 $I->seeFileFound('report.tap.log','tests/_log');
+
+if (floatval(phpversion()) == '5.3') $this->markTestSkipped();
--- tests/cli/RunWithXmlCept.php
+++ tests/cli/RunWithXmlCept.php
@@ -7,3 +7,5 @@ $I->seeFileFound('report.xml','tests/_log');
 $I->seeInThisFile('<?xml');
 $I->seeInThisFile('<testsuite name="dummy"');
 $I->seeInThisFile('<testcase file="FileExistsCept.php"');
+
+if (floatval(phpversion()) == '5.3') $this->markTestSkipped();
