9100fb6176c89adc9a9f54c7434421621376852e
Aleksey Asiutin <sutok85@gmail.com>
2013-01-21 12:42:49 +0200

    generators improved

--- src/Codeception/Command/Base.php
+++ src/Codeception/Command/Base.php
@@ -5,4 +5,30 @@ use \Symfony\Component\Yaml\Yaml;
 
 class Base extends \Symfony\Component\Console\Command\Command
 {
+
+    protected function buildPath($basePath, $testName)
+    {
+        $testName = str_replace('/',DIRECTORY_SEPARATOR, $testName);
+        $dirs = explode(DIRECTORY_SEPARATOR, $testName);
+
+        $path = $basePath;
+        foreach ($dirs as $dir) {
+            $path .= DIRECTORY_SEPARATOR.$dir;
+            @mkdir($path);
+        }
+        return $path;
+    }
+
+    protected function getNamespaces($class)
+    {
+        $namespaces = explode('\\', $class);
+        array_pop($namespaces);
+        return $namespaces;
+    }
+
+    protected function getClassName($class)
+    {
+        $namespaces = explode('\\', $class);
+        return array_pop($namespaces);
+    }
 }
