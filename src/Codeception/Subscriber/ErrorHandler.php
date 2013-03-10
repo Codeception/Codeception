63e0115c280ffd3aa9cc16b74507879d9cfdd140
Aleksey Asiutin <sutok85@gmail.com>
2013-03-10 22:46:15 +0200

    error handler fixed

--- src/Codeception/Subscriber/ErrorHandler.php
+++ src/Codeception/Subscriber/ErrorHandler.php
@@ -23,12 +23,17 @@ class ErrorHandler implements EventSubscriberInterface
             $this->errorLevel = eval("return {$settings['error_level']};");
         }
         error_reporting($this->errorLevel);
-        set_error_handler(array(__CLASS__, 'errorHandler'), $this->errorLevel);
+        set_error_handler(array(__CLASS__, 'errorHandler'));
         register_shutdown_function(array(__CLASS__, 'shutdownHandler'));
     }
 
     public static function errorHandler($errno, $errstr, $errfile, $errline) {
-        if (error_reporting() === 0) return false;
+
+        if (!(error_reporting() & $errno)) {
+            // This error code is not included in error_reporting
+            return false;
+        }
+
         if (strpos($errstr, 'Cannot modify header information')!==false)
             return false;
         throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
@@ -40,6 +45,9 @@ class ErrorHandler implements EventSubscriberInterface
         self::$stopped = true;
         $error = error_get_last();
         if (!is_array($error)) return;
+        echo error_reporting();
+        print_r($error);
+        if (error_reporting() === 0) return;
 
         echo "\n\n\nFATAL ERROR. TESTS NOT FINISHED.\n";
         echo sprintf("%s \nin %s:%d\n", $error['message'], $error['file'], $error['line']);
