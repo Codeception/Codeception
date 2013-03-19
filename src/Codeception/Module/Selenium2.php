558893f3febbd8412c67ceec16da806d9f038675
Aleksey Asiutin <sutok85@gmail.com>
2013-03-19 23:02:20 +0200

    click on context

--- src/Codeception/Module/Selenium2.php
+++ src/Codeception/Module/Selenium2.php
@@ -103,6 +103,19 @@ class Selenium2 extends \Codeception\Util\MinkJS
     // please, add more custom Selenium functions here
 
     /**
+     * Clicks on either link or button (for PHPBrowser) or on any selector for JS browsers.
+     * Link text or css selector can be passed.
+     *
+     * @param $link
+     * @param $context
+     */
+    public function click($link, $context = null) {
+        $url = $this->session->getCurrentUrl();
+        $el = $this->findClickable($link, $context);
+        $el->click();
+    }
+
+    /**
      * Accept alert or confirm popup
      *
      * Example:
