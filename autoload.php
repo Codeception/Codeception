f1db061a83f27197244b5a76f2c5b559f4751e44
Aleksey Asiutin <sutok85@gmail.com>
2013-04-04 22:42:19 +0300

    php53 compatibility fixed  (again)

--- autoload.php
+++ autoload.php
@@ -1,5 +1,12 @@
 <?php
 
+/** spike-fix for PHP 5.3 */
+if (! interface_exists('JsonSerializable')) {
+    interface JsonSerializable {
+        function jsonSerialize();
+    }
+}
+
 if (stream_resolve_include_path(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
 } elseif (stream_resolve_include_path(__DIR__.'/../../autoload.php')) {
