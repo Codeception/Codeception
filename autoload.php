a1bc1cfd96fd091e22ab03cd9b991b0844fc169e
Aleksey Asiutin <sutok85@gmail.com>
2013-04-04 22:32:03 +0300

    bump dependencies + switch autoloader to one provided by Composer

--- autoload.php
+++ autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 if (stream_resolve_include_path(__DIR__ . '/vendor/autoload.php')) {
-    require_once __DIR__ . 'vendor/autoload.php';
+    require_once __DIR__ . '/vendor/autoload.php';
 } elseif (stream_resolve_include_path(__DIR__.'/../../autoload.php')) {
     require_once __DIR__ . '/../../autoload.php';
 }
