7ced1af825cdea7cd1eb05051491cd8bcd2290e1
Davert <davert@mail.ua>
2013-04-06 01:06:54 +0300

    fix to but in tests. C'mon, Travis, make me green

--- tests/cli/GeneratePhpUnitCept.php
+++ tests/cli/GeneratePhpUnitCept.php
@@ -1,6 +1,6 @@
 <?php
 $I = new CliGuy($scenario);
-$I->wa1ntTo('generate sample Test');
+$I->wantTo('generate sample Test');
 $I->amInPath('tests/data/sandbox');
 $I->executeCommand('generate:phpunit dummy Dummy');
 $I->seeFileFound('DummyTest.php');
