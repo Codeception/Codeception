430c61ad0dc483dc51eb0d619f2c9e8a3aa308f3
Ragazzo <some@example.com>
2013-04-10 21:55:52 +0400

    Added exception throw if form not found

--- src/Codeception/Module/PhpBrowser.php
+++ src/Codeception/Module/PhpBrowser.php
@@ -2,6 +2,8 @@
 
 namespace Codeception\Module;
 
+use Codeception\Exception\TestRuntime;
+
 /**
  * Uses [Mink](http://mink.behat.org) with [Goutte](https://github.com/fabpot/Goutte) and [Guzzle](http://guzzlephp.org/) to interact with your application over CURL.
  *
@@ -24,7 +26,7 @@ namespace Codeception\Module;
  *
  * ### Example (`acceptance.suite.yml`)
  *
- *     modules: 
+ *     modules:
  *        enabled: [PhpBrowser]
  *        config:
  *           PhpBrowser:
@@ -47,26 +49,32 @@ class PhpBrowser extends \Codeception\Util\Mink implements \Codeception\Util\Fra
     }
 
     public function submitForm($selector, $params) {
-
         $form = $this->session->getPage()->find('css',$selector);
-	    $fields = $this->session->getPage()->findAll('css', $selector.' input');
-	    $url = '';
-	    foreach ($fields as $field) {
-		    $url .= sprintf('%s=%s',$field->getAttribute('name'), $field->getAttribute('value')).'&';
-	    }
 
-	    $fields = $this->session->getPage()->findAll('css', $selector.' textarea');
-	    foreach ($fields as $field) {
-		    $url .= sprintf('%s=%s',$field->getAttribute('name'), $field->getValue()).'&';
-	    }
+        if ($form === null)
+            throw new TestRuntime("Form with selector: \"$selector\" was not found on given page.");
 
-	    $fields = $this->session->getPage()->findAll('css', $selector.' select');
+        $fields = $this->session->getPage()->findAll('css', $selector.' input');
+        $url = '';
+
         foreach ($fields as $field) {
-   		    $url .= sprintf('%s=%s',$field->getAttribute('name'), $field->getValue()).'&';
-   	    }
+            $url .= sprintf('%s=%s',$field->getAttribute('name'), $field->getAttribute('value')).'&';
+        }
 
-		$url .= '&'.http_build_query($params);
-	    parse_str($url, $params);
+        $fields = $this->session->getPage()->findAll('css', $selector.' textarea');
+
+        foreach ($fields as $field) {
+            $url .= sprintf('%s=%s',$field->getAttribute('name'), $field->getValue()).'&';
+        }
+
+        $fields = $this->session->getPage()->findAll('css', $selector.' select');
+
+        foreach ($fields as $field) {
+            $url .= sprintf('%s=%s',$field->getAttribute('name'), $field->getValue()).'&';
+        }
+
+        $url .= '&'.http_build_query($params);
+        parse_str($url, $params);
         $url = $form->getAttribute('action');
         $method = $form->getAttribute('method');
 
