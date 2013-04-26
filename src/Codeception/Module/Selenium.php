61f0adbdd8da5c2d6bc2ec4263932f1866409f32
Aleksey Asiutin <sutok85@gmail.com>
2013-04-26 19:14:14 +0300

    module SecondDb

--- src/Codeception/Module/Selenium.php
+++ src/Codeception/Module/Selenium.php
@@ -41,19 +41,22 @@ namespace Codeception\Module;
  *
  * ### Example (`acceptance.suite.yml`)
  *
- *     modules: 
+ *     modules:
  *        enabled: [Selenium]
  *        config:
  *           Selenium:
- *              url: 'http://localhost/' 
- *              browser: firefox 
+ *              url: 'http://localhost/'
+ *              browser: firefox
  *
  * ## Public Properties
  *
  * * session - contains Mink Session
  */
 
-class Selenium extends \Codeception\Util\MinkJS
+use Behat\Mink\Driver\SeleniumDriver;
+use Codeception\Util\MinkJS;
+
+class Selenium extends MinkJS
 {
     protected $requiredFields = array('browser', 'url');
 
@@ -62,7 +65,7 @@ class Selenium extends \Codeception\Util\MinkJS
 
     public function _initialize() {
         $client = new \Selenium\Client($this->config['host'], $this->config['port']);
-        $driver = new \Behat\Mink\Driver\SeleniumDriver(
+        $driver = new SeleniumDriver(
             $this->config['browser'], $this->config['url'], $client
         );
 
@@ -107,4 +110,217 @@ class Selenium extends \Codeception\Util\MinkJS
         $this->session->getDriver()->getBrowser()->captureEntirePageScreenshot($filename,'');
     }
 
+    /*
+     * INHERITED ACTIONS
+     */
+
+    public function amOnPage($page)
+    {
+        parent::amOnPage($page);
+    }
+
+    public function dontSee($text, $selector = null)
+    {
+        parent::dontSee($text, $selector);
+    }
+
+    public function see($text, $selector = null)
+    {
+        parent::see($text, $selector);
+    }
+
+    public function seeLink($text, $url = null)
+    {
+        parent::seeLink($text, $url);
+    }
+
+    public function dontSeeLink($text, $url = null)
+    {
+        parent::dontSeeLink($text, $url);
+    }
+
+    public function click($link, $context = null)
+    {
+        parent::click($link, $context);
+    }
+
+    public function dontSeeElement($selector)
+    {
+        parent::dontSeeElement($selector);
+    }
+
+    public function reloadPage()
+    {
+        parent::reloadPage();
+    }
+
+    public function moveBack()
+    {
+        parent::moveBack();
+    }
+
+    public function moveForward()
+    {
+        parent::moveForward();
+    }
+
+    public function fillField($field, $value)
+    {
+        parent::fillField($field, $value);
+    }
+
+    public function selectOption($select, $option)
+    {
+        parent::selectOption($select, $option);
+    }
+
+    public function seeInCurrentUrl($uri)
+    {
+        parent::seeInCurrentUrl($uri);
+    }
+
+    public function dontSeeInCurrentUrl($uri)
+    {
+        parent::dontSeeInCurrentUrl($uri);
+    }
+
+    public function seeCurrentUrlEquals($uri)
+    {
+        parent::seeCurrentUrlEquals($uri);
+    }
+
+    public function dontSeeCurrentUrlEquals($uri)
+    {
+        parent::dontSeeCurrentUrlEquals($uri);
+    }
+
+    public function seeCurrentUrlMatches($uri)
+    {
+        parent::seeCurrentUrlMatches($uri);
+    }
+
+    public function dontSeeCurrentUrlMatches($uri)
+    {
+        parent::dontSeeCurrentUrlMatches($uri);
+    }
+
+    public function grabFromCurrentUrl($uri = null)
+    {
+        return parent::grabFromCurrentUrl($uri);
+    }
+
+    public function attachFile($field, $filename)
+    {
+        parent::attachFile($field, $filename);
+    }
+
+    public function seeCheckboxIsChecked($checkbox)
+    {
+        parent::seeCheckboxIsChecked($checkbox);
+    }
+
+    public function dontSeeCheckboxIsChecked($checkbox)
+    {
+        parent::dontSeeCheckboxIsChecked($checkbox);
+    }
+
+    public function seeInField($field, $value)
+    {
+        parent::seeInField($field, $value);
+    }
+
+    public function dontSeeInField($field, $value)
+    {
+        parent::dontSeeInField($field, $value);
+    }
+
+    public function grabTextFrom($cssOrXPathOrRegex)
+    {
+        return parent::grabTextFrom($cssOrXPathOrRegex);
+    }
+
+    public function grabValueFrom($field)
+    {
+        return parent::grabValueFrom($field);
+    }
+
+    public function grabAttribute()
+    {
+        parent::grabAttribute();
+    }
+
+    public function checkOption($option)
+    {
+        parent::checkOption($option);
+    }
+
+    public function uncheckOption($option)
+    {
+        parent::uncheckOption($option);
+    }
+
+    public function doubleClick($link)
+    {
+        parent::doubleClick($link);
+    }
+
+    public function clickWithRightButton($link)
+    {
+        parent::clickWithRightButton($link);
+    }
+
+    public function moveMouseOver($link)
+    {
+        parent::moveMouseOver($link);
+    }
+
+    public function focus($el)
+    {
+        parent::focus($el);
+    }
+
+    public function blur($el)
+    {
+        parent::blur($el);
+    }
+
+    public function dragAndDrop($el1, $el2)
+    {
+        parent::dragAndDrop($el1, $el2);
+    }
+
+    public function seeElement($selector)
+    {
+        parent::seeElement($selector);
+    }
+
+    public function pressKey($element, $char, $modifier = null)
+    {
+        parent::pressKey($element, $char, $modifier);
+    }
+
+    public function pressKeyUp($element, $char, $modifier = null)
+    {
+        parent::pressKeyUp($element, $char, $modifier);
+    }
+
+    public function pressKeyDown($element, $char, $modifier = null)
+    {
+        parent::pressKeyDown($element, $char, $modifier);
+    }
+
+    public function wait($milliseconds)
+    {
+        parent::wait($milliseconds);
+    }
+
+    public function waitForJS($milliseconds, $jsCondition)
+    {
+        parent::waitForJS($milliseconds, $jsCondition);
+    }
+
+    public function executeJs($jsCode)
+    {
+        parent::executeJs($jsCode);
+    }
 }
