4861d058547f6e9bf3805e7920a82ee4781f00b1
davert <davert>
2013-05-09 02:12:51 +0300

    more precise test for events

--- tests/cli/OrderCest.php
+++ tests/cli/OrderCest.php
@@ -8,7 +8,7 @@ class OrderCest {
         $I->executeCommand('run order LoadingOrderCept.php');
         $I->expect('initialization, bootstrap(B), beforeSuite, before, bootstrap(B), test(T), after, afterSuite');
         $I->seeFileFound('order.txt','tests/_log');
-        $I->seeInThisFile("IB([BT])");
+        $I->seeInThisFile("IBS([BST])");
     }
 
     public function checkForFails(CliGuy $I)
@@ -17,7 +17,7 @@ class OrderCest {
         $I->executeCommand('run order FailedCept.php --no-exit');
         $I->seeFileFound('order.txt','tests/_log');
         $I->expect('initialization, bootstrap, beforeSuite, before, bootstrap, test, after, fail, afterSuite');
-        $I->seeInThisFile("IB([BTF])");
+        $I->seeInThisFile("IBS([BSTF])");
     }
 
 
@@ -26,7 +26,7 @@ class OrderCest {
         $I->amInPath('tests/data/sandbox');
         $I->executeCommand('run order --no-exit');
         $I->seeFileFound('order.txt','tests/_log');
-        $I->seeInThisFile("IBBB([BT][BTF][BT])");
+        $I->seeInThisFile("IBSBSBS([BST][BSTF][BST])");
     }
 
 
