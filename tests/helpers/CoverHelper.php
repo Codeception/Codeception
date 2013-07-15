3249a0c4beb03b33d80a275bbc9d9bff490de767
davert <davert.githole@mailican.com>
2013-07-15 21:14:29 +0300

    fix to cli for php 5.3

--- tests/helpers/CoverHelper.php
+++ tests/helpers/CoverHelper.php
@@ -7,6 +7,5 @@ class CoverHelper extends \Codeception\Module
 {
     
     public function _before(\Codeception\TestCase $test) {
-        if (floatval(phpversion()) == '5.3') $test->markTestSkipped();
     }
 }
