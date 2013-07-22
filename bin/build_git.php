10afe0d430e08bc9d7a4ccb459888d80a445eff2
Aleksey Asiutin <sutok85@gmail.com>
2013-07-22 16:27:05 +0300

    marked as dev version

--- bin/build_git.php
+++ bin/build_git.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__.'/../autoload.php';
 $version = \Codeception\Codecept::VERSION;
+$branch = implode('.',array_pop(explode('.', $version)));
 system("git tag $version");
-system("git push origin master --tags");
+system("git push origin $branch --tags");
