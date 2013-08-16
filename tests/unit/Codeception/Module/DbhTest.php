c74efbef96a603aa881eb0cfa8a337df25f068fb
Aleksey Asiutin <sutok85@gmail.com>
2013-08-16 14:22:31 +0800

    dont rollback outside of a transaction

---
+++ tests/unit/Codeception/Module/DbhTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use Codeception\Util\Stub;
+use Codeception\Util\Driver\Db as Driver;
+
+class DbhTest extends \PHPUnit_Framework_TestCase
+{
+    protected $config = array(
+        'dsn' => 'sqlite:tests/data/sqlite.db',
+        'user' => 'root',
+        'password' => ''
+    );
+
+    protected $testCase = null;
+
+    /**
+     * @var \Codeception\Module\Dbh
+     */
+    protected $module = null;
+
+    public function setUp() {
+      $this->testCase = Stub::make('\Codeception\TestCase');
+
+      $module = new \Codeception\Module\Dbh();
+
+      try {
+        $driver = Driver::create($this->config['dsn'], $this->config['user'], $this->config['password']);
+        $module::$dbh = $driver->getDbh();
+      } catch (\PDOException $e) {
+          $this->markTestSkipped('Coudn\'t establish connection to database');
+          return;
+      }
+
+      $this->module = $module;
+    }
+
+    public function testDontRollbackOutsideATransaction() {
+      $this->module->_after($this->testCase);
+    }
+
+}
