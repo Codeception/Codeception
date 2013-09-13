36e93adbbfade86d31e4c227fcdd59d5c944e1fd
Guilherme Franco <g.francosilva@gmail.com>
2013-09-13 14:22:17 -0300

    Improving Step class code design

--- src/Codeception/Step.php
+++ src/Codeception/Step.php
@@ -15,19 +15,17 @@ abstract class Step
 
     protected $debugOutput;
 
-
     public $executed = false;
 
     public function __construct($action, array $arguments)
     {
-        $this->action = $action;
+        $this->action    = $action;
         $this->arguments = $arguments;
     }
 
     public function getName()
     {
-        $class = explode('\\', __CLASS__);
-        return end($class);
+        return end(explode('\\', __CLASS__));
     }
 
     public function pullDebugOutput()
@@ -42,34 +40,39 @@ abstract class Step
         return $this->action;
     }
 
-    public function getArguments($asString = FALSE)
+    public function getArguments($asString = false)
     {
-        if (!$asString) {
-            return $this->arguments;
-        } else {
-            $arguments = $this->arguments;
+        return ($asString) ? $this->getArgumentsAsString($this->arguments) : $this->arguments;
+    }
 
-            foreach ($arguments as $k => $argument) {
-                if (!is_string($argument) and is_callable($argument, true)) {
-                    $arguments[$k] = 'lambda function';
-                    continue;
-                }
-                if (is_object($argument)) {
-                    if (method_exists($argument, '__toString')) {
-                        $arguments[$k] = $argument->__toString();
-                    } elseif (isset($argument->__mocked)) {
-                        $arguments[$k] = $this->formatClassName($argument->__mocked);
-                    } else {
-                        $arguments[$k] = $this->formatClassName(get_class($argument));
-                    }
-                    continue;
-                }
-            }
-            if (defined('JSON_UNESCAPED_UNICODE')) {
-                return stripcslashes(trim(json_encode($arguments, JSON_UNESCAPED_UNICODE), '[]'));
-            }
-            return stripcslashes(trim(json_encode($arguments), '[]'));
+    protected function getArgumentsAsString(array $arguments) 
+    {
+        foreach ($arguments as $key => $argument) {
+            $arguments[$key] = (is_string($argument)) ? $argument : $this->parseArgumentAsString($argument);
         }
+
+        if (defined('JSON_UNESCAPED_UNICODE')) {
+            return stripcslashes(trim(json_encode($arguments, JSON_UNESCAPED_UNICODE), '[]'));
+        }
+
+        return stripcslashes(trim(json_encode($arguments), '[]'));
+    }
+
+    protected function parseArgumentAsString($argument)
+    {
+        if (is_callable($argument, true)) {
+            return 'lambda function';  
+        } 
+
+        if (!is_object($argument)) {
+            return $argument;
+        }
+
+        if (method_exists($argument, '__toString')) {
+            return $argument->__toString();    
+        } 
+
+        return (isset($argument->__mocked)) ? $this->formatClassName($argument->__mocked) : $this->formatClassName(get_class($argument));
     }
 
     protected function formatClassName($classname)
@@ -89,8 +92,7 @@ abstract class Step
 
     public function getHtmlAction()
     {
-        $args = $this->getHumanizedArguments();
-        $args = preg_replace('~\$(.*?)\s~', '$<span style="color: #3C3C89; font-weight: bold;">$1</span>', $args);
+        $args = preg_replace('~\$(.*?)\s~', '$<span style="color: #3C3C89; font-weight: bold;">$1</span>', $this->getHumanizedArguments());
         return $this->humanize($this->getAction()) . ' <span style="color: #732E81;">' . $args . '</span>';
     }
 
@@ -120,15 +122,13 @@ abstract class Step
     public function run()
     {
         $this->executed = true;
-        $activeModule = \Codeception\SuiteManager::$modules[\Codeception\SuiteManager::$actions[$this->action]];
+        $activeModule   = \Codeception\SuiteManager::$modules[\Codeception\SuiteManager::$actions[$this->action]];
 
-        if (is_callable(array($activeModule, $this->action))) {
-            $result = call_user_func_array(array($activeModule, $this->action), $this->arguments);
-        } else {
-            throw new \RuntimeException("Action can't be called");
-        }
+        if (!is_callable(array($activeModule, $this->action))) {
+            throw new \RuntimeException("Action can't be called");  
+        } 
+        
         $this->debugOutput = $activeModule->_getDebugOutput();
-        return $result;
+        return call_user_func_array(array($activeModule, $this->action), $this->arguments);
     }
-
 }
