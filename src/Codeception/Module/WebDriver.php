8a2b48e4493d7b190ece3ea36697c42c5153c409
michael.wang <wyh.michael@gmail.com>
2013-09-30 14:49:56 -0700

    add a return to separate select situations

--- src/Codeception/Module/WebDriver.php
+++ src/Codeception/Module/WebDriver.php
@@ -369,7 +369,7 @@ class WebDriver extends \Codeception\Module implements WebInterface {
                 $matched = true;
             } catch (\NoSuchElementWebDriverError $e) {}
         }
-	if ($matched) return;
+        if ($matched) return;
         foreach ($option as $opt) {
             try {
             $select->selectByValue($opt);
