1adcd0e680b011d6fbd93e2d905fc18e0eccc2a0
davert <davert.githole@mailican.com>
2013-10-11 20:54:00 +0300

    ElementNotFound is now PHPUnit_Framework_AssertionFailedError

--- src/Codeception/Exception/ElementNotFound.php
+++ src/Codeception/Exception/ElementNotFound.php
@@ -1,7 +1,7 @@
 <?php
 namespace Codeception\Exception;
 
-class ElementNotFound extends TestRuntime
+class ElementNotFound extends \PHPUnit_Framework_AssertionFailedError
 {
 
     public function __construct($selector, $message = null)
