003850aff2c45013ad2e1a93385395f3631e5e94
davert <davert.githole@mailican.com>
2013-10-12 00:58:18 +0300

    1.6.9

--- src/Codeception/Codecept.php
+++ src/Codeception/Codecept.php
@@ -9,7 +9,7 @@ use Codeception\Exception\Configuration as ConfigurationException;
 
 class Codecept
 {
-    const VERSION = "1.6.8.1";
+    const VERSION = "1.6.9";
 
     /**
      * @var \Codeception\PHPUnit\Runner
