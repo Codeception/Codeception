82e389a1a1a36c708543ef15225d25269cd15033
davert <davert.githole@mailican.com>
2013-10-14 00:21:07 +0300

    bugfix version released

--- src/Codeception/Codecept.php
+++ src/Codeception/Codecept.php
@@ -9,7 +9,7 @@ use Codeception\Exception\Configuration as ConfigurationException;
 
 class Codecept
 {
-    const VERSION = "1.7.0.1";
+    const VERSION = "1.7.0.2";
 
     /**
      * @var \Codeception\PHPUnit\Runner
