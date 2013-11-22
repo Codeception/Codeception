0404d0d92111879f29db4fffa88a054e322c9059
Gustavo Chain <gustavo@typeform.com>
2013-11-22 17:32:22 +0100

    Added missing test case for sendAjaxRequest

--- tests/unit/Codeception/Module/FrameworksTest.php
+++ tests/unit/Codeception/Module/FrameworksTest.php
@@ -1,4 +1,5 @@
 <?php
+
 class FrameworksTest extends \PHPUnit_Framework_TestCase
 {
     /**
@@ -328,6 +329,12 @@ class FrameworksTest extends \PHPUnit_Framework_TestCase
         $this->assertArrayHasKey('HTTP_X_REQUESTED_WITH', $_SERVER);
         $post = data::get('form');
         $this->assertEquals('author', $post['show']);
+
+        $this->module->sendAjaxRequest('/articles', 'DELETE');
+        $this->assertEquals('DELETE', $_SERVER['REQUEST_METHOD']);
+
+        $this->module->sendAjaxRequest('/articles/1', 'PUT', array('title' => 'foo'));
+        $this->assertEquals('PUT', $_SERVER['REQUEST_METHOD']);
     }
 
     public function testSeeWithNonLatin() {
