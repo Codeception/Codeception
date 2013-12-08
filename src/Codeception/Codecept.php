6e9429b2d4a4be507b7f56a950eeb267a568994d
davert <davert.githole@mailican.com>
2013-12-08 05:20:47 +0200

    dev version set to 1.9

--- src/Codeception/Codecept.php
+++ src/Codeception/Codecept.php
@@ -9,7 +9,7 @@ use Codeception\Exception\Configuration as ConfigurationException;
 
 class Codecept
 {
-    const VERSION = "1.8.1";
+    const VERSION = "1.9-dev";
 
     /**
      * @var \Codeception\PHPUnit\Runner
