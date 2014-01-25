10f2da5d31e348d73eea1bc45a15d40352f6d8bc
Davert <davert.php@resend.cc>
2014-01-25 02:39:09 +0200

    removed build scrips in favor of robo

--- bin/build_git.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__.'/../autoload.php';
-$version = \Codeception\Codecept::VERSION;
-$branch = explode('.', $version);
-array_pop($branch);
-$branch = implode('.',$branch);
-system("git tag $version");
-system("git push origin $branch --tags");
