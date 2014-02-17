5ef99bcacc88ce38dbbf67958151a1cbdc01bcb3
Davert <davert.php@resend.cc>
2014-02-17 07:07:39 +0200

    fix to assrt module

--- tests/unit/Codeception/Module/AssertsTest.php
+++ tests/unit/Codeception/Module/AssertsTest.php
@@ -4,8 +4,8 @@ class AssertsTest extends PHPUnit_Framework_TestCase {
     public function testAsserts()
     {
         $module = new \Codeception\Module\Asserts;
-        $module->assertEquals(1,1);
-        $module->assertContains(1,[1,2]);
+        $module->seeEquals(1,1);
+        $module->seeContains(1,[1,2]);
     }
 
 }
