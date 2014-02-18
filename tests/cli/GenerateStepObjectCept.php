1b1e1459a714e095ce9c342dc60545c456cf4e98
Davert <davert.php@resend.cc>
2014-02-18 02:26:44 +0200

    command test fixed

--- tests/cli/GenerateStepObjectCept.php
+++ tests/cli/GenerateStepObjectCept.php
@@ -2,7 +2,7 @@
 $I = new CliGuy\GeneratorSteps($scenario);
 $I->wantTo('generate step object');
 $I->amInPath('tests/data/sandbox');
-$I->executeCommand('generate:stepobject dummy Login --force');
+$I->executeCommand('generate:stepobject dummy Login --silent');
 $I->seeFileWithGeneratedClass('LoginSteps','tests/dummy/_steps');
 $I->seeInThisFile('LoginSteps extends \DumbGuy');
 $I->seeAutoloaderWasAdded('Steps','tests/dummy');
