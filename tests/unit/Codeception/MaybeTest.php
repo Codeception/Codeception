63046ea244082e99b76325a2d006912fcafa9d57
Mauro Asprea <mauroasprea@gmail.com>
2014-02-27 19:21:04 +0100

    Add clone and unset capabilites to Maybe

--- tests/unit/Codeception/MaybeTest.php
+++ tests/unit/Codeception/MaybeTest.php
@@ -14,9 +14,36 @@ class MaybeTest extends \Codeception\TestCase\Test
      */
     protected $maybe;
 
-    public function testMaybe() {
+    public function testMaybe()
+    {
         $this->maybe = new \Codeception\Maybe("Hello");
         $this->assertEquals('Hello', $this->maybe);
     }
 
+    public function testMaybeClone()
+    {
+        // Cloning with an object
+        $obj = new StdClass();
+        $maybe = new \Codeception\Maybe($obj);
+        $clone = clone $maybe;
+        $this->assertNotSame($obj, $clone->__value());
+
+        // Non object clone
+        $maybe = new \Codeception\Maybe("Hello");
+        $this->assertEquals('Hello', clone $maybe);
+
+        $maybe = new \Codeception\Maybe(3);
+        $clone = clone $maybe;
+        $this->assertEquals(3, $clone->__value());
+
+        $maybe = new \Codeception\Maybe(false);
+        $clone = clone $maybe;
+        $this->assertFalse($clone->__value());
+
+        // Null clone
+        $maybe = new \Codeception\Maybe(null);
+        $clone = clone $maybe;
+        $this->assertNull($clone->__value());
+
+    }
 }
