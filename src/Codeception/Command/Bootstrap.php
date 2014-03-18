b9ad835e67892b25f1ca9dfbde682420a13dee00
David Albertson <albertson.d@gmail.com>
2014-03-18 17:49:18 -0400

    Fixing typo in _bootstrap.php file generator
    Fixing typo.

--- src/Codeception/Command/Bootstrap.php
+++ src/Codeception/Command/Bootstrap.php
@@ -208,7 +208,7 @@ class Bootstrap extends Command
 
         file_put_contents(
             'tests/unit/_bootstrap.php',
-            "<?php\n// Here you can initialize variables that will for your tests\n"
+            "<?php\n// Here you can initialize variables that will be used for your tests\n"
         );
         file_put_contents(
             'tests/_helpers/CodeHelper.php',
