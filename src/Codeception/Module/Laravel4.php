c230efde47ace3ce747399c5fab374ef7563efca
Andrei Komarov <andkmrv@gmail.com>
2014-04-03 19:31:12 +0300

    findRecord() fixed

--- src/Codeception/Module/Laravel4.php
+++ src/Codeception/Module/Laravel4.php
@@ -291,7 +291,7 @@ class Laravel4 extends Framework implements ActiveRecord
 
     protected function findRecord($model, $attributes = array())
     {
-        $query = $this->kernel['db']->table[$model];
+        $query = $this->kernel['db']->table($model);
         foreach ($attributes as $key => $value) {
             $query->where($key, $value);
         }
