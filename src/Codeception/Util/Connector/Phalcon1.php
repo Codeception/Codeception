16e123f1834af02d06dce3327972c0a0ed788a7f
Davert <davert.php@resend.cc>
2014-04-04 06:14:42 +0300

    fixed Phalcon 1.3 compatibility

--- src/Codeception/Util/Connector/Phalcon1.php
+++ src/Codeception/Util/Connector/Phalcon1.php
@@ -159,7 +159,7 @@ class PhalconMemorySession extends \Phalcon\Session\Adapter implements \Phalcon\
         return $this->isStarted;
     }
 
-    public function destroy()
+    public function destroy($session_id = NULL)
     {
         $this->isStarted = false;
         $this->data = array();
