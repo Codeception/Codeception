3cb91d83f37b2fed5cee64c50232c0f02f192ce0
Davert <davert.php@resend.cc>
2014-04-21 00:57:16 +0300

    removed unused declaration

--- src/Codeception/Lib/Framework.php
+++ src/Codeception/Lib/Framework.php
@@ -2,8 +2,6 @@
 
 namespace Codeception\Lib;
 
-use Codeception\Lib\InnerBrowser;
-
 /**
  * Abstract module for PHP frameworks connected via Symfony BrowserKit components
  * Each framework is connected with it's own connector defined in \Codeception\Lib\Connector
