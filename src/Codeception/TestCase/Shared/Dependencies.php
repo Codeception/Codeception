b851a7853df8ee85c680e89c428aaf08ffb6ed41
Davert <davert.php@resend.cc>
2014-04-22 06:25:45 +0300

    fixed usage of dependencies in tests'

--- src/Codeception/TestCase/Shared/Dependencies.php
+++ src/Codeception/TestCase/Shared/Dependencies.php
@@ -13,18 +13,19 @@ trait Dependencies
         }
 
         $passed = $this->getTestResultObject()->passed();
-        $testNames = array_map(function ($testname) {
+        $passedKeys = array_map(function ($testname) {
+                $testname = str_replace('Codeception\TestCase\Cest::', get_class($this->getTestClass()).'::', $testname);
                 return preg_replace('~with data set (.*?)~', '', $testname);
             }, array_keys($passed)
         );
 
-        $passedKeys = array_keys($passed);
         $dependencyInput = [];
 
         foreach ($this->dependencies as $dependency) {
             if (strpos($dependency, '::') === false) {
                 $dependency = str_replace($this->getName(false), $dependency, $this->getSignature());
             }
+
             if (!in_array($dependency, $passedKeys)) {
                 $this->getTestResultObject()->addError($this, new \PHPUnit_Framework_SkippedTestError(sprintf("This test depends on '$dependency' to pass.")), 0);
                 return false;
