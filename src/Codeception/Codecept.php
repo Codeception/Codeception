9350e5f23139150f9b1f5fe25d22a5f4596e955f
Davert <davert.php@resend.cc>
2014-05-14 04:15:02 +0300

    loading bootstrap before tests

--- src/Codeception/Codecept.php
+++ src/Codeception/Codecept.php
@@ -167,8 +167,8 @@ class Codecept
 
     public function runSuite($settings, $suite, $test = null) {
         $suiteManager = new SuiteManager($this->dispatcher, $suite, $settings);
-        $suiteManager->loadTests($test);
         $suiteManager->initialize();
+        $suiteManager->loadTests($test);
         $suiteManager->run($this->runner, $this->result, $this->options);
 
         return $this->result;
