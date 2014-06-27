2ca8334ab882a33eaf203707bc376c541dee22bd
Nathan Macnamara <nathan.macnamara@arrowecs.co.uk>
2014-06-27 09:52:44 +0100

    Minor corrections to DocBlocks to fix formatting issues in generated docs

--- src/Codeception/Module/FTP.php
+++ src/Codeception/Module/FTP.php
@@ -215,6 +215,7 @@ class FTP extends \Codeception\Module\Filesystem
      * <?php
      * $I->seeFileFoundMatches('/^UserModel_([0-9]{6}).php$/','app/models');
      * ?>
+     * ```
      *
      * @param $regex
      * @param string $path
@@ -421,7 +422,7 @@ class FTP extends \Codeception\Module\Filesystem
      * ```php
      * <?php
      * $files = $I->grabFileList();
-     * $count = $I->grabFileCount('TEST', false); // Include . .. .thumbs.db
+     * $count = $I->grabFileList('TEST', false); // Include . .. .thumbs.db
      * ?>
      * ```
      *
