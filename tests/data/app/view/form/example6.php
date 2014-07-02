dd177486d167a7a8b51b31e6dfdcf093b259d356
Vadim Slutsky <2trustnik@gmail.com>
2014-07-02 19:38:58 +0400

    fix for radio button select option

---
+++ tests/data/app/view/form/example6.php
@@ -0,0 +1,6 @@
+<form action="/form/example6">
+    <input id="radio_preset1" name="frequency" type="radio" value="week" >
+    <input id="radio_preset2" name="frequency" type="radio" value="hour" checked="checked">
+    <input id="radio_preset3" name="frequency" type="radio" value="day">
+    <button type="submit">Submit</button>
+</form>
