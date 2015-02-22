ee130f4e36fd9a9e19fc60fb842ee7a92a7f9da3
Davert <davert.php@resend.cc>
2015-02-22 03:08:25 +0200

    updated build test

--- tests/cli/BuildCept.php
+++ tests/cli/BuildCept.php
@@ -5,6 +5,6 @@ $I = new CliGuy($scenario);
 $I->wantToTest('build command');
 $I->runShellCommand('php codecept build');
 $I->seeInShellOutput('generated successfully');
-$I->seeFileFound('CodeGuy.php','tests/unit');
-$I->seeFileFound('CliGuy.php','tests/cli');
+$I->seeFileFound('CodeGuy.php','tests/_support');
+$I->seeFileFound('CliGuy.php','tests/_support');
 $I->seeInThisFile('seeFileFound(');
