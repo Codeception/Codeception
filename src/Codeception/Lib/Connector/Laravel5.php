3e937cd2075a734f0f312d55c01cbe93f6ecd40e
jedrzej <jedrzej.kurylo@gmail.com>
2015-02-27 10:58:47 +0100

    Fixed Call to undefined method Symfony\Component\HttpFoundation\Request::route()

--- src/Codeception/Lib/Connector/Laravel5.php
+++ src/Codeception/Lib/Connector/Laravel5.php
@@ -67,4 +67,16 @@ class Laravel5 extends Client implements HttpKernelInterface, TerminableInterfac
 	{
 		$this->httpKernel->terminate($request, $response);
 	}
+
+    /**
+     * Build Laravel5 request based on provided request instance
+     *
+     * @param DomRequest $request A DomRequest instance
+     *
+     * @return Response A Response instance
+     */
+    protected function doRequest($request)
+    {
+        return parent::doRequest(Request::createFromBase($request));
+    }
 }
