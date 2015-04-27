983f2fffe4d10dd53ba1e97a69fdb4464000034a
Alexander Turiak <alex@hexbrain.com>
2015-04-27 15:48:41 +0200

    Fixed test to comply to double new line at the end

--- tests/cli/GenerateScenariosCept.php
+++ tests/cli/GenerateScenariosCept.php
@@ -1,6 +1,6 @@
 <?php
 $I = new CliGuy($scenario);
-$I->wantTo('generate scenarios сценарий');
+$I->wantTo('generate test scenario');
 $I->amInPath('tests/data/sandbox');
 $I->executeCommand('generate:scenarios dummy');
 $I->seeFileFound('File_Exists.txt','tests/_data/scenarios');
@@ -8,5 +8,7 @@ $I->seeFileContentsEqual(<<<EOF
 I WANT TO CHECK CONFIG EXISTS
 
 I see file found "\$codeception"
+
+
 EOF
 );
