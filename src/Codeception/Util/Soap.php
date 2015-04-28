5e90907884df475e6e35f462dd931a840f58fb67
Davert <davert.php@resend.cc>
2015-04-28 03:24:19 +0300

    updated XML utils to work with REST module

--- src/Codeception/Util/Soap.php
+++ src/Codeception/Util/Soap.php
@@ -2,61 +2,15 @@
 
 namespace Codeception\Util;
 
-class Soap
+/**
+ * This class is left for BC compatibility.
+ * Most of its contents moved to parent
+ *
+ * Class Soap
+ * @package Codeception\Util
+ */
+class Soap extends Xml
 {
-    /**
-     * @static
-     *
-     * @param \DOMDocument $xml
-     * @param \DOMNode $node
-     * @param array $array
-     *
-     * @return \DOMDocument
-     */
-    public static function arrayToXml(\DOMDocument $xml, \DOMNode $node, $array = [])
-    {
-        foreach ($array as $el => $val) {
-            if (is_array($val)) {
-                self::arrayToXml($xml, $node->$el, $val);
-            } else {
-                $node->appendChild($xml->createElement($el, $val));
-            }
-        }
-        return $xml;
-    }
-
-    /**
-     * @static
-     *
-     * @param $xml
-     *
-     * @return \DOMDocument|\DOMNode
-     */
-    public static function toXml($xml)
-    {
-        if ($xml instanceof XmlBuilder) {
-            return $xml->getDom();
-        }
-        if ($xml instanceof \DOMDocument) {
-            return $xml;
-        }
-        $dom = new \DOMDocument();
-        $dom->preserveWhiteSpace = false;
-        if ($xml instanceof \DOMNode) {
-            $xml = $dom->importNode($xml, true);
-            $dom->appendChild($xml);
-            return $dom;
-        }
-
-        if (is_array($xml)) {
-            return self::arrayToXml($dom, $dom, $xml);
-        }
-        if (!empty($xml)) {
-            $dom->loadXML($xml);
-        }
-        return $dom;
-    }
-
     public static function request()
     {
         return new XmlBuilder();
@@ -66,4 +20,5 @@ class Soap
     {
         return new XmlBuilder();
     }
+
 }
---
+++ src/Codeception/Util/Xml.php
@@ -0,0 +1,63 @@
+<?php 
+namespace Codeception\Util;
+
+class Xml
+{
+    /**
+     * @static
+     *
+     * @param \DOMDocument $xml
+     * @param \DOMNode $node
+     * @param array $array
+     *
+     * @return \DOMDocument
+     */
+    public static function arrayToXml(\DOMDocument $xml, \DOMNode $node, $array = [])
+    {
+        foreach ($array as $el => $val) {
+            if (is_array($val)) {
+                self::arrayToXml($xml, $node->$el, $val);
+            } else {
+                $node->appendChild($xml->createElement($el, $val));
+            }
+        }
+        return $xml;
+    }
+
+    /**
+     * @static
+     *
+     * @param $xml
+     *
+     * @return \DOMDocument|\DOMNode
+     */
+    public static function toXml($xml)
+    {
+        if ($xml instanceof XmlBuilder) {
+            return $xml->getDom();
+        }
+        if ($xml instanceof \DOMDocument) {
+            return $xml;
+        }
+        $dom = new \DOMDocument();
+        $dom->preserveWhiteSpace = false;
+        if ($xml instanceof \DOMNode) {
+            $xml = $dom->importNode($xml, true);
+            $dom->appendChild($xml);
+            return $dom;
+        }
+
+        if (is_array($xml)) {
+            return self::arrayToXml($dom, $dom, $xml);
+        }
+        if (!empty($xml)) {
+            $dom->loadXML($xml);
+        }
+        return $dom;
+    }
+
+    public static function build()
+    {
+        return new XmlBuilder();
+    }
+}
