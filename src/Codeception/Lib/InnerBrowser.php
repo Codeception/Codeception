041ac9b66151d87b27ffcec3bb1becc1bd778898
Zaahid Bateson <zbateson@mail.ubc.ca>
2015-05-01 10:05:34 -0700

    Fixes GET requests on URLs with query params
    URLs were not properly constructed for GET form submissions on
    URLs requested with query parameters.  Fixes #1891

--- src/Codeception/Lib/InnerBrowser.php
+++ src/Codeception/Lib/InnerBrowser.php
@@ -102,9 +102,13 @@ class InnerBrowser extends Module implements Web
         $buttonText = str_replace('"',"'", $link);
         $button = $this->crawler->selectButton($buttonText);
         if (count($button)) {
+            $buttonValue = [];
+            if (strval($button->attr('name')) !== '' && $button->attr('value') !== null) {
+                $buttonValue = [$button->attr('name') => $button->attr('value')];
+            }
             $this->proceedSubmitForm(
                 $button->parents()->filter('form')->first(),
-                [$button->attr('name') => $button->attr('value')]
+                $buttonValue
             );
             return;
         }
@@ -128,9 +132,13 @@ class InnerBrowser extends Module implements Web
                 $this->debugResponse();
                 break;
             } elseif (in_array($tag, ['input', 'button']) && in_array($type, ['submit', 'image'])) {
+                $buttonValue = [];
+                if (strval($nodes->first()->attr('name')) !== '' && $nodes->first()->attr('value') !== null) {
+                    $buttonValue = [$nodes->first()->attr('name') => $nodes->first()->attr('value')];
+                }
                 $this->proceedSubmitForm(
                     $nodes->parents()->filter('form')->first(),
-                    [$nodes->first()->attr('name') => $nodes->first()->attr('value')]
+                    $buttonValue
                 );
                 break;
             }
@@ -401,17 +409,19 @@ class InnerBrowser extends Module implements Web
             }
         }
 
-        $query = '';
+        $urlparts = parse_url($this->getFormUrl($frmCrawl));
         if (strcasecmp($form->getMethod(), 'GET') === 0) {
-            $query = '?' . http_build_query($requestParams);
+            $urlparts = $this->mergeUrls($urlparts, ['query' => http_build_query($requestParams)]);
         }
-        $this->debugSection('Uri', $this->getFormUrl($frmCrawl));
+        $url = \GuzzleHttp\Url::buildUrl($urlparts);
+        
+        $this->debugSection('Uri', $url);
         $this->debugSection('Method', $form->getMethod());
         $this->debugSection('Parameters', $requestParams);
 
         $this->crawler = $this->client->request(
             $form->getMethod(),
-            $this->getFormUrl($frmCrawl) . $query,
+            $url,
             $requestParams,
             $form->getPhpFiles()
         );
