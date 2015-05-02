58fff4a6fdad40fd24b6e458bc43b498e1dc6e13
Davert <davert.php@resend.cc>
2015-05-02 05:02:01 +0300

    marked version as beta

--- src/Codeception/Codecept.php
+++ src/Codeception/Codecept.php
@@ -7,7 +7,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class Codecept
 {
-    const VERSION = "2.1.0";
+    const VERSION = "2.1.0-beta";
 
     /**
      * @var \Codeception\PHPUnit\Runner
