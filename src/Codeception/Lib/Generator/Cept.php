ed8ac8114f62748821bacf08766871e33ec95e18
Eugene Manuilov <eugene.manuilov@10up.com>
2015-05-15 11:38:15 +0300

    Fixed double slashes issue.

--- src/Codeception/Lib/Generator/Cept.php
+++ src/Codeception/Lib/Generator/Cept.php
@@ -23,9 +23,11 @@ EOF;
     public function produce()
     {
         $actor = $this->settings['class_name'];
-        $use = $this->settings['namespace']
-            ? "use {$this->settings['namespace']}\\$actor;"
-            : '';
+        $use = '';
+		if ( ! empty( $this->settings['namespace'] ) ) {
+			$namespace = rtrim( $this->settings['namespace'], '\\' );
+            $use = "use {$namespace}\\$actor;";
+		}
 
         return (new Template($this->template))
             ->place('actor', $actor)
