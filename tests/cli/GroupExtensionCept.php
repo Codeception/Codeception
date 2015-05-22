faeac0927500cd0c795a082e724f7cc7e8a7f380
Davert <davert.php@resend.cc>
2015-05-23 02:56:10 +0300

    all cli tests to run in non-interactive mode

--- tests/cli/GroupExtensionCept.php
+++ tests/cli/GroupExtensionCept.php
@@ -3,6 +3,6 @@ $I = new CliGuy($scenario);
 $I->wantTo('see that my group extension works');
 $I->amInPath('tests/data/sandbox');
 $I->executeCommand('run skipped -g notorun -c codeception_grouped.yml');
-$I->seeInShellOutput("======> Entering NoGroup Test Scope\nTrying to make it incomplete");
+$I->seeInShellOutput("======> Entering NoGroup Test Scope\nMake it incomplete");
 $I->seeInShellOutput('<====== Ending NoGroup Test Scope');
 
