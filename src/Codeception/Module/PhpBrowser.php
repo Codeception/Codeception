57c9a27702239bcd5ef74630986676c14a6ea79c
Gintautas Miselis <gintautas@miselis.lt>
2015-05-24 17:48:08 +0100

    Made subdomain match less greedy to remove the first component only #1757

--- src/Codeception/Module/PhpBrowser.php
+++ src/Codeception/Module/PhpBrowser.php
@@ -168,7 +168,7 @@ class PhpBrowser extends InnerBrowser implements Remote, MultiSession
     public function amOnSubdomain($subdomain)
     {
         $url = $this->config['url'];
-        $url = preg_replace('~(https?:\/\/)(.*\.)(.*\.)~', "$1$3", $url); // removing current subdomain
+        $url = preg_replace('~(https?:\/\/)(.*?\.)(.*\.)~', "$1$3", $url); // removing current subdomain
         $url = preg_replace('~(https?:\/\/)(.*)~', "$1$subdomain.$2", $url); // inserting new
         $this->_reconfigure(array('url' => $url));
     }
