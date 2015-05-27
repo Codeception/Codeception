f11812c099befdfa25beb14a782de17f0383d0c0
Gintautas Miselis <gintautas@miselis.lt>
2015-05-27 23:21:56 +0100

    Initialize module in setUpBeforeClass

--- tests/unit/Codeception/Module/DbTest.php
+++ tests/unit/Codeception/Module/DbTest.php
@@ -12,58 +12,57 @@ class DbTest extends \PHPUnit_Framework_TestCase
     /**
      * @var \Codeception\Module\Db
      */
-    protected $module = null;
+    protected static $module;
     
     public static function setUpBeforeClass()
     {
         try {
             $sqlite = \Codeception\Lib\Driver\Db::create(self::$config['dsn'], self::$config['user'], self::$config['password']);
             $sqlite->cleanup();
-            
-            $sql = file_get_contents(\Codeception\Configuration::dataDir() . '/dumps/sqlite.sql');
-            $sql = preg_replace('%/\*(?:(?!\*/).)*\*/%s', "", $sql);
-            $sql = explode("\n", $sql);
-            $sqlite->load($sql);
         } catch (\Exception $e) {
+            return;
         }
+                    
+        $sql = file_get_contents(\Codeception\Configuration::dataDir() . '/dumps/sqlite.sql');
+        $sql = preg_replace('%/\*(?:(?!\*/).)*\*/%s', "", $sql);
+        $sql = explode("\n", $sql);
+        $sqlite->load($sql);
         
+        self::$module = new \Codeception\Module\Db();
+        self::$module->_setConfig(self::$config);
+        self::$module->_initialize();
     }
 
-
-    public function setUp()
+    public function testSeeInDatabase()
     {
-        $this->module = new \Codeception\Module\Db();
-        $this->module->_setConfig(self::$config);
-        $this->module->_initialize();
-//        $this->loadDump(); // enable this when you want to change fixtures
+        self::$module->seeInDatabase('users', array('name' => 'davert'));
     }
 
-    public function testSeeInDatabase() {
-        $this->module->seeInDatabase('users', array('name' => 'davert'));
+    public function testDontSeeInDatabase()
+    {
+        self::$module->dontSeeInDatabase('users', array('name' => 'user1'));
     }
 
-    public function testDontSeeInDatabase() {
-        $this->module->dontSeeInDatabase('users', array('name' => 'user1'));
+    public function testDontSeeInDatabaseWithEmptyTable()
+    {
+        self::$module->dontSeeInDatabase('empty_table');
     }
 
-    public function testDontSeeInDatabaseWithEmptyTable() {
-        $this->module->dontSeeInDatabase('empty_table');
-    }
-
-    public function testGrabFromDatabase() {
-        $email = $this->module->grabFromDatabase('users', 'email', array('name' => 'davert'));
+    public function testGrabFromDatabase()
+    {
+        $email = self::$module->grabFromDatabase('users', 'email', array('name' => 'davert'));
         $this->assertEquals('davert@mail.ua', $email);
     }
 
     public function testHaveAndSeeInDatabase()
     {
-        $this->module->_before(\Codeception\Util\Stub::make('\Codeception\TestCase'));
-        $user_id = $this->module->haveInDatabase('users', array('name' => 'john', 'email' => 'john@jon.com'));
-        $group_id = $this->module->haveInDatabase('groups', array('name' => 'john', 'enabled' => false));
+        self::$module->_before(\Codeception\Util\Stub::make('\Codeception\TestCase'));
+        $user_id = self::$module->haveInDatabase('users', array('name' => 'john', 'email' => 'john@jon.com'));
+        $group_id = self::$module->haveInDatabase('groups', array('name' => 'john', 'enabled' => false));
         $this->assertInternalType('integer', $user_id);
-        $this->module->seeInDatabase('users', array('name' => 'john', 'email' => 'john@jon.com'));
-        $this->module->dontSeeInDatabase('users', array('name' => 'john', 'email' => null));
-        $this->module->_after(\Codeception\Util\Stub::make('\Codeception\TestCase'));
-        $this->module->dontSeeInDatabase('users', array('name' => 'john'));
+        self::$module->seeInDatabase('users', array('name' => 'john', 'email' => 'john@jon.com'));
+        self::$module->dontSeeInDatabase('users', array('name' => 'john', 'email' => null));
+        self::$module->_after(\Codeception\Util\Stub::make('\Codeception\TestCase'));
+        self::$module->dontSeeInDatabase('users', array('name' => 'john'));
     }
 }
