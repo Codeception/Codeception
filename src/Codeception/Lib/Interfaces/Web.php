254d9336763c3dd3f54bf9e4cae265daffe57e22
Zaahid Bateson <zbateson@gmail.com>
2015-06-02 22:18:04 -0700

    Updated documentation for submitForm
    - Wrong documentation in WebDriver
    - Clarification for submitting fields with names ending in square
      brackets.

--- src/Codeception/Lib/Interfaces/Web.php
+++ src/Codeception/Lib/Interfaces/Web.php
@@ -55,8 +55,8 @@ interface Web
     public function dontSee($text, $selector = null);
 
     /**
-     * Submits the given form on the page, optionally with the given form values.
-     * Give the form fields values as an array.
+     * Submits the given form on the page, optionally with the given form
+     * values.  Give the form fields values as an array.
      *
      * Skipped fields will be filled by their values from the page.
      * You don't need to click the 'Submit' button afterwards.
@@ -71,9 +71,15 @@ interface Web
      *
      * ``` php
      * <?php
-     * $I->submitForm('#login', array('login' => 'davert', 'password' => '123456'));
+     * $I->submitForm('#login', [
+     *     'login' => 'davert',
+     *     'password' => '123456'
+     * ]);
      * // or
-     * $I->submitForm('#login', array('login' => 'davert', 'password' => '123456'), 'submitButtonName');
+     * $I->submitForm('#login', [
+     *     'login' => 'davert',
+     *     'password' => '123456'
+     * ], 'submitButtonName');
      *
      * ```
      *
@@ -81,10 +87,17 @@ interface Web
      *
      * ``` html
      * <form action="/sign_up">
-     *     Login: <input type="text" name="user[login]" /><br/>
-     *     Password: <input type="password" name="user[password]" /><br/>
-     *     Do you agree to out terms? <input type="checkbox" name="user[agree]" /><br/>
-     *     Select pricing plan <select name="plan"><option value="1">Free</option><option value="2" selected="selected">Paid</option></select>
+     *     Login:
+     *     <input type="text" name="user[login]" /><br/>
+     *     Password:
+     *     <input type="password" name="user[password]" /><br/>
+     *     Do you agree to out terms?
+     *     <input type="checkbox" name="user[agree]" /><br/>
+     *     Select pricing plan:
+     *     <select name="plan">
+     *         <option value="1">Free</option>
+     *         <option value="2" selected="selected">Paid</option>
+     *     </select>
      *     <input type="submit" name="submitButton" value="Submit" />
      * </form>
      * ```
@@ -93,17 +106,36 @@ interface Web
      *
      * ``` php
      * <?php
-     * $I->submitForm('#userForm', array('user' => array('login' => 'Davert', 'password' => '123456', 'agree' => true)), 'submitButton');
-     *
+     * $I->submitForm(
+     *     '#userForm',
+     *     [
+     *         'user' => [
+     *             'login' => 'Davert',
+     *             'password' => '123456',
+     *             'agree' => true
+     *         ]
+     *     ],
+     *     'submitButton'
+     * );
      * ```
-     * Note that "2" will be the submitted value for the "plan" field, as it is the selected option.
+     * Note that "2" will be the submitted value for the "plan" field, as it is
+     * the selected option.
      * 
-     * You can also emulate a JavaScript submission by not specifying any buttons in the third parameter to submitForm.
+     * You can also emulate a JavaScript submission by not specifying any
+     * buttons in the third parameter to submitForm.
      * 
      * ```php
      * <?php
-     * $I->submitForm('#userForm', array('user' => array('login' => 'Davert', 'password' => '123456', 'agree' => true)));
-     * 
+     * $I->submitForm(
+     *     '#userForm',
+     *     [
+     *         'user' => [
+     *             'login' => 'Davert',
+     *             'password' => '123456',
+     *             'agree' => true
+     *         ]
+     *     ]
+     * );
      * ```
      * 
      * Pair this with seeInFormFields for quick testing magic.
@@ -148,8 +180,31 @@ interface Web
      * ?>
      * ```
      *
-     * Mixing string and boolean values for a checkbox's value is not
-     * supported and may produce unexpected results.
+     * Mixing string and boolean values for a checkbox's value is not supported
+     * and may produce unexpected results.
+     * 
+     * Field names ending in "[]" must be passed without the trailing square 
+     * bracket characters, and must contain an array for its value.  This allows
+     * submitting multiple values with the same name, consider:
+     * 
+     * ```php
+     * $I->submitForm('#my-form', [
+     *     'field[]' => 'value',
+     *     'field[]' => 'another value', // 'field[]' is already a defined key
+     * ]);
+     * ```
+     * 
+     * The solution is to pass an array value:
+     * 
+     * ```php
+     * // this way both values are submitted
+     * $I->submitForm('#my-form', [
+     *     'field' => [
+     *         'value',
+     *         'another value',
+     *     ]
+     * ]);
+     * ```
      * 
      * @param $selector
      * @param $params
