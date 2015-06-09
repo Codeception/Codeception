b55081469e976f45e14a03056fd554cfd7d3f838
Gintautas Miselis <gintautas@miselis.lt>
2015-06-09 20:34:51 +0100

    Copied content header fix from #2028

--- src/Codeception/Lib/Connector/ZF1.php
+++ src/Codeception/Lib/Connector/ZF1.php
@@ -127,7 +127,7 @@ class ZF1 extends Client
             $server['HTTP_HOST'] .= ':' . $port;
         }
 
-        $contentHeaders = array('Content-length' => true, 'Content-md5' => true, 'Content-type' => true);
+        $contentHeaders = array('Content-Length' => true, 'Content-Md5' => true, 'Content-Type' => true);
         foreach ($server as $header => $val) {
             $header = implode('-', array_map('ucfirst', explode('-', strtolower(str_replace('_', '-', $header)))));
 
--- src/Codeception/Lib/Connector/ZF2.php
+++ src/Codeception/Lib/Connector/ZF2.php
@@ -104,7 +104,7 @@ class ZF2 extends Client
             $server['HTTP_HOST'] .= ':' . $port;
         }
 
-        $contentHeaders = array('Content-length' => true, 'Content-md5' => true, 'Content-type' => true);
+        $contentHeaders = array('Content-Length' => true, 'Content-Md5' => true, 'Content-Type' => true);
         foreach ($server as $header => $val) {
             $header = implode('-', array_map('ucfirst', explode('-', strtolower(str_replace('_', '-', $header)))));
 
