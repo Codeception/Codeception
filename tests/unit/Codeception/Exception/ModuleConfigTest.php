166c59944e200515cef1f056dc9822976c10e06f
Davert <davert.php@resend.cc>
2015-06-13 02:34:58 +0300

    fixed module config tests

--- tests/unit/Codeception/Exception/ModuleConfigTest.php
+++ tests/unit/Codeception/Exception/ModuleConfigTest.php
@@ -6,13 +6,13 @@ class ModuleConfigTest extends \PHPUnit_Framework_TestCase
     public function testCanBeCreatedForModuleName()
     {
         $exception = new \Codeception\Exception\ModuleConfigException('Codeception\Module\WebDriver', "Hello world");
-        $this->assertEquals("WebDriver module is not configured!\n\nHello world", $exception->getMessage());
+        $this->assertEquals("WebDriver module is not configured!\n \nHello world", $exception->getMessage());
     }
 
     public function testCanBeCreatedForModuleObject()
     {
         $exception = new \Codeception\Exception\ModuleConfigException(new \Codeception\Module\CodeHelper(make_container()), "Hello world");
-        $this->assertEquals("CodeHelper module is not configured!\n\nHello world", $exception->getMessage());
+        $this->assertEquals("CodeHelper module is not configured!\n \nHello world", $exception->getMessage());
     }
 
 }
