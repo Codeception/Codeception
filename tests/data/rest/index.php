9e247ae751dde520906468f15492f798b8fdb535
Gintautas Miselis <gintautas@miselis.lt>
2015-06-20 13:00:13 +0100

    Reproduced issue #2070

--- tests/data/rest/index.php
+++ tests/data/rest/index.php
@@ -2,44 +2,62 @@
 
 include_once 'server.php';
 
-$GLOBALS['RESTmap'] = array();
+$GLOBALS['RESTmap'] = [];
 
-$GLOBALS['RESTmap']['GET'] = array('user' => function() {
-    return array(
-        'name'    => 'davert',
-        'email'   => 'davert@mail.ua',
-        'aliases' => array(
-            'DavertMik',
-            'davert.ua'
-        ),
-        'address' => array(
-            'city'    => 'Kyiv',
-            'country' => 'Ukraine',
-        ));
+$GLOBALS['RESTmap']['GET'] = [
+    'user' => function() {
+        return [
+            'name'    => 'davert',
+            'email'   => 'davert@mail.ua',
+            'aliases' => [
+                'DavertMik',
+                'davert.ua'
+            ],
+            'address' => [
+                'city'    => 'Kyiv',
+                'country' => 'Ukraine',
+            ]];
     },
     'ping' => function() {
-        $resp = "";
+        $resp = '';
         foreach (getallheaders() as $k => $v) {
             $resp .= strtolower($k) . ": $v\n";
         }
         return $resp;
+    },
+    'zeroes' => function() {
+        return [
+            'responseCode' => 0,
+            'message' => 'OK',
+            'data' => [
+                9,
+                0,
+                0
+            ],
+        ];
     }
-);
+];
 
-$GLOBALS['RESTmap']['POST'] = array('user' => function() {
-    $name = $_POST['name'];
-    return array('name' => $name);
-});
+$GLOBALS['RESTmap']['POST'] = [
+    'user' => function() {
+        $name = $_POST['name'];
+        return ['name' => $name];
+    }
+];
 
-$GLOBALS['RESTmap']['PUT'] = array('user' => function() {
-    $name = $_REQUEST['name'];
-    $user = array('name' => 'davert', 'email' => 'davert@mail.ua');;
-    $user['name'] = $name;
-    return $user;
-});
+$GLOBALS['RESTmap']['PUT'] = [
+    'user' => function() {
+        $name = $_REQUEST['name'];
+        $user = ['name' => 'davert', 'email' => 'davert@mail.ua'];
+        $user['name'] = $name;
+        return $user;
+    }
+];
 
-$GLOBALS['RESTmap']['DELETE'] = array('user' => function() {
-    header('error', false, 404);
-});
+$GLOBALS['RESTmap']['DELETE'] = [
+    'user' => function() {
+        header('error', false, 404);
+    }
+];
 
 RESTServer();
