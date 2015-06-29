e4dcd75cf38908c48fab94a9bc5b1218679083eb
Davert <davert.php@resend.cc>
2015-06-30 01:10:24 +0300

    version bump

--- src/Codeception/Codecept.php
+++ src/Codeception/Codecept.php
@@ -7,7 +7,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class Codecept
 {
-    const VERSION = "2.1.0-rc2";
+    const VERSION = "2.1.0";
 
     /**
      * @var \Codeception\PHPUnit\Runner
