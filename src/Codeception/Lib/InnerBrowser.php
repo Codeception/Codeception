37174bab6aca5a32830d1beca5b728f2bc0eb549
Zaahid Bateson <zbateson@mail.ubc.ca>
2015-08-13 09:10:39 -0700

    Fix for matchOption evaluating '0' to false
    Fixes #2033 in 2.1 - from pull request by @onshop into 2.0

--- src/Codeception/Lib/InnerBrowser.php
+++ src/Codeception/Lib/InnerBrowser.php
@@ -738,7 +738,7 @@ class InnerBrowser extends Module implements Web
             } else {
                 $options->getNode(0)->setAttribute('checked', 'checked');
             }
-            if ($options->first()->attr('value')) {
+            if ($options->first()->attr('value') !== false) {
                 return $options->first()->attr('value');
             }
             return $options->first()->text();
