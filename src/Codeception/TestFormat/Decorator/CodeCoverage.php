14aaf1599df54f6084187610be8b88469e61dba9
Davert <davert.php@resend.cc>
2015-10-01 23:51:09 +0300

    added commented exceptions to codecoverage

--- src/Codeception/TestFormat/Decorator/CodeCoverage.php
+++ src/Codeception/TestFormat/Decorator/CodeCoverage.php
@@ -26,34 +26,35 @@ trait CodeCoverage
         $linesToBeCovered = \PHPUnit_Util_Test::getLinesToBeCovered(get_class($this->testClassInstance), 'test');
         $linesToBeUsed = \PHPUnit_Util_Test::getLinesToBeUsed(get_class($this->testClassInstance), 'test');
 
-        try {
+//        try {
             $codeCoverage->stop(true, $linesToBeCovered, $linesToBeUsed);
-        } catch (\PHP_CodeCoverage_Exception_UnintentionallyCoveredCode $cce) {
-            $this->getTestResult()->addFailure(
-                $this,
-                new \PHPUnit_Framework_UnintentionallyCoveredCodeError(
-                    'This test executed code that is not listed as code to be covered or used:' .
-                    PHP_EOL . $cce->getMessage()
-                ),
-                $time
-            );
-        } catch (PHPUnit_Framework_InvalidCoversTargetException $cce) {
-            $this->addFailure(
-                $test,
-                new PHPUnit_Framework_InvalidCoversTargetError(
-                    $cce->getMessage()
-                ),
-                $time
-            );
-        } catch (PHP_CodeCoverage_Exception $cce) {
-            $status = \Codeception\Test::STATUS_ERROR;
-
-            if (!isset($e)) {
-                $e = $cce;
-            }
-        }
+        // should this be included???
+//        } catch (\PHP_CodeCoverage_Exception_UnintentionallyCoveredCode $cce) {
+//            $this->getTestResult()->addFailure(
+//                $this,
+//                new \PHPUnit_Framework_UnintentionallyCoveredCodeError(
+//                    'This test executed code that is not listed as code to be covered or used:' .
+//                    PHP_EOL . $cce->getMessage()
+//                ),
+//                $time
+//            );
+//        } catch (\PHPUnit_Framework_InvalidCoversTargetException $cce) {
+//            $this->addFailure(
+//                $test,
+//                new PHPUnit_Framework_InvalidCoversTargetError(
+//                    $cce->getMessage()
+//                ),
+//                $time
+//            );
+//        } catch (PHP_CodeCoverage_Exception $cce) {
+//            $status = \Codeception\Test::STATUS_ERROR;
+//
+//            if (!isset($e)) {
+//                $e = $cce;
+//            }
+//        }
 
     }
     
-    
+
 }
