50a7cb6d6e79c9123f26c56d73f3029d184889df
Davert <davert.php@resend.cc>
2015-10-02 16:01:39 +0300

    updated changelog

--- src/Codeception/Codecept.php
+++ src/Codeception/Codecept.php
@@ -7,7 +7,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class Codecept
 {
-    const VERSION = "2.1.3";
+    const VERSION = "2.1.4";
 
     /**
      * @var \Codeception\PHPUnit\Runner
