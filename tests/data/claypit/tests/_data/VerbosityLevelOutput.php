1ae8146962d862a4bfbecfbf41872de5f0df08d9
Davert <davert.php@resend.cc>
2015-12-16 21:42:59 +0200

    reverted symfony to v2.7gst

--- tests/data/claypit/tests/_data/VerbosityLevelOutput.php
+++ tests/data/claypit/tests/_data/VerbosityLevelOutput.php
@@ -12,6 +12,7 @@ class VerbosityLevelOutput extends Extension
 
     public function printResult(PrintResultEvent $e)
     {
+        $this->writeln(var_export($this->options, false));
         $this->writeln("Modules used: " . implode(', ', $this->getCurrentModuleNames()));
 
         if ($this->options['verbosity'] <= 1) {
