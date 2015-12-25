ee6c809070d5ad5246703a43e01ea72578f31088
Tommy Goode <tommy@tgoode.com>
2015-12-25 02:39:13 -0500

    Fixing the failing test case: the Postgres driver now deletes all custom types during cleanup().

--- src/Codeception/Lib/Driver/PostgreSql.php
+++ src/Codeception/Lib/Driver/PostgreSql.php
@@ -62,7 +62,7 @@ class PostgreSql extends Db
             ->fetchAll();
 
         $types = $this->dbh
-            ->query("SELECT 'DROP TYPE IF EXISTS \"' || pg_type.typname || '\" cascade;' FROM pg_type JOIN pg_enum ON pg_enum.enumtypid = pg_type.oid GROUP BY pg_type.typname;")
+            ->query("SELECT 'DROP TYPE IF EXISTS \"' || t.typname || '\" cascade;' FROM pg_catalog.pg_type t LEFT JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace WHERE (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid)) AND NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid) AND n.nspname <> 'pg_catalog' AND n.nspname <> 'information_schema' AND pg_catalog.pg_type_is_visible(t.oid);")
             ->fetchAll();
 
         $drops = array_merge($tables, $sequences, $types);
