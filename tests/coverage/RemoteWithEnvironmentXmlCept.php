bed0fe7ab258bebdf7191511f7359943ab99d380
Davert <davert.php@resend.cc>
2016-01-22 01:50:00 +0200

    added a test for #2634

---
+++ tests/coverage/RemoteWithEnvironmentXmlCept.php
@@ -0,0 +1,9 @@
+<?php
+$I = new CoverGuy($scenario);
+$I->wantTo('try generate codecoverage xml report with environment');
+$I->amInPath('tests/data/sandbox');
+$I->executeCommand('run remote --coverage-xml --env default');
+$I->seeInShellOutput('Code Coverage Report');
+$I->seeFileFound('coverage.xml','tests/_output');
+$I->seeCoverageStatsNotEmpty();
+
