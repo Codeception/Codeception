06e8f23267fe2ee99377075bff82b8546d51a5b8
Tobias Matthaiou <tm@solutiondrive.de>
2016-01-25 17:26:20 +0100

    Create a Application Class to extend the Console Command

---
+++ src/Codeception/Application.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Codeception;
+
+use Symfony\Component\Console\Application as BaseApplication;
+
+class Application extends BaseApplication
+{
+    /**
+     * Register Commands from a Config file
+     */
+    public function registerCustomCommands()
+    {
+        //Todo: logic to register custom Commands from a config file.
+    }
+}
