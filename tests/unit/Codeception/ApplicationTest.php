7ac068de58a6fbf2247d1e38f22a000f1feb6ad5
Tobias Matthaiou <tm@solutiondrive.de>
2016-01-27 11:38:57 +0100

    Feature Custom Commands: Unit tests

---
+++ tests/unit/Codeception/ApplicationTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Codeception\Application;
+
+class ApplicationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRegisterCustomCommand()
+    {
+        Codeception\Configuration::append(array('extensions' => array(
+            'commands' => array(
+                'Codeception\Command\MyCustomCommand'))));
+
+        $application = new Application();
+        $application->registerCustomCommands();
+
+        $command = null;
+
+        try {
+            $command = $application->find('myProjekt:myCommand');
+        } catch (Exception $e) {}
+
+        $this->assertNotEmpty($command, "Custom command not found.");
+    }
+}
---
+++ tests/unit/Codeception/Command/MyCustomCommandTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class MyCustomCommandTest extends \PHPUnit_Framework_TestCase {
+
+    public function testHasCodeceptionCustomCommandInterface()
+    {
+        $command = new Codeception\Command\MyCustomCommand('commandName');
+        $this->assertInstanceOf('Codeception\Lib\Interfaces\CustomCommands', $command);
+    }
+
+    public function testHasCommandName()
+    {
+        $commandName = Codeception\Command\MyCustomCommand::getCommandName();
+        $this->assertEquals('myProjekt:myCommand', $commandName);
+    }
+
+}
