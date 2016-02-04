e8a2a1ea26480da07dd62f9901edd48b930110fc
Davert <davert.php@resend.cc>
2016-02-05 01:45:08 +0200

    improved error message

--- src/Codeception/Exception/ModuleConflictException.php
+++ src/Codeception/Exception/ModuleConflictException.php
@@ -16,6 +16,6 @@ class ModuleConflictException extends \Exception
         $this->message = "$module module conflicts with $conflicted\n\n--\n"
             . "This usually happens when you enable two modules with the same actions but with different backends.\n"
             . "For instance, you can't run PhpBrowser, WebDriver, Laravel4 modules in one suite,\n"
-            . "as they implement similar methods but use different drivers to execute them." . $additional;
+            . "as they implement similar methods but use different drivers to execute them.\n" . $additional;
     }
 }
