c7ef5d71ec3066964a0243cb0c779b61d283feb5
Davert <davert.php@resend.cc>
2016-03-10 14:18:17 +0200

    fixed loading tests

--- tests/unit/Codeception/TestLoaderTest.php
+++ tests/unit/Codeception/TestLoaderTest.php
@@ -53,6 +53,7 @@ class TestLoaderTest extends \PHPUnit_Framework_TestCase
     public function testLoadAllTests()
     {
         Codeception\Util\Autoload::addNamespace('Math', codecept_data_dir().'claypit/tests/_support/Math'); // to autoload dependencies
+        Codeception\Util\Autoload::addNamespace('Codeception\Module', codecept_data_dir().'claypit/tests/_support');
 
         $this->testLoader = new \Codeception\Lib\TestLoader(codecept_data_dir().'claypit/tests');
         $this->testLoader->loadTests();
