c1c8db6cc6959167c943af3aec901c16c20c8d0e
omgnull <doomsmile@gmail.com>
2016-03-18 15:12:15 +0300

    Tests message with utf-8 strings

--- tests/unit/Codeception/Lib/Console/MessageTest.php
+++ tests/unit/Codeception/Lib/Console/MessageTest.php
@@ -14,5 +14,26 @@ class MessageTest extends \Codeception\TestCase\Test
     
     //test message cutting
     public function testVeryLongTestNameVeryLongTestNameVeryLongTestNameVeryLongTestNameVeryLongTestNameVeryLongTestNameVeryLongTestName() {}
+
+    // skip if mb_substr not exists
+    public function testUTF8Message()
+    {
+        if (function_exists('mb_substr')) {
+            $message = new Message('очень длинный текст');
+            $this->assertEquals('очень длин', $message->cut(10)->getMessage());
+        }
+    }
+
+    // skip if mb_strlen not exists
+    public function testUTF8Width()
+    {
+        if (function_exists('mb_strlen')) {
+            $message = new Message('пример текста');
+            $this->assertEquals(
+                'пример текста                 ',
+                $message->width(30)->getMessage())
+            ;
+        }
+    }
 }
  
