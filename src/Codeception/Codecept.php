fb21771548f8aec2b2fdf8cdb91a8ea2f35fa75c
Davert <davert.php@resend.cc>
2016-04-14 02:38:20 +0300

    RC version

--- src/Codeception/Codecept.php
+++ src/Codeception/Codecept.php
@@ -7,7 +7,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class Codecept
 {
-    const VERSION = "2.2.0-beta";
+    const VERSION = "2.2.0-RC";
 
     /**
      * @var \Codeception\PHPUnit\Runner
