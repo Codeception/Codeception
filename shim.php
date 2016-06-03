f1d0cd5a12ab59fc70c956f924838e26053ef2c4
Davert <davert.php@resend.cc>
2016-06-03 13:54:15 +0300

    PHPUnit 5.4 compat

--- shim.php
+++ shim.php
@@ -40,6 +40,14 @@ namespace Codeception\Platform {
 }
 namespace {
     class_alias('Codeception\TestInterface', 'Codeception\TestCase');
+
+    // phpunit codecoverage updates
+    class_alias('SebastianBergmann\CodeCoverage\CodeCoverage', 'PHP_CodeCoverage');
+    class_alias('SebastianBergmann\CodeCoverage\Report\Text', 'PHP_CodeCoverage_Report_Text');
+    class_alias('SebastianBergmann\CodeCoverage\Report\PHP', 'PHP_CodeCoverage_Report_PHP');
+    class_alias('SebastianBergmann\CodeCoverage\Report\Clover', 'PHP_CodeCoverage_Report_Clover');
+    class_alias('SebastianBergmann\CodeCoverage\Report\Html\Facade', 'PHP_CodeCoverage_Report_HTML');
+    class_alias('SebastianBergmann\CodeCoverage\Exception', 'PHP_CodeCoverage_Exception');
 }
 
 // @codingStandardsIgnoreEnd
