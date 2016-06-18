eec8bce71341ceb0c57ef012459739fcaa3bb0a0
Philipp Kretzschmar <philipp.kretzschmar@dreamlines.de>
2016-06-18 13:50:25 +0200

    Diff shows only changed lines
    The diff used to show both complete strings instead of only the changed lines,
    I've added a unit test for the case and refactored a bit inside the Console.php
    
    Todo: Add color output as this got lost during the diff changes.
    
    fixes issue 3230

---
+++ src/Codeception/Lib/Console/MessageFactory.php
@@ -0,0 +1,67 @@
+<?php
+namespace Codeception\Lib\Console;
+
+use SebastianBergmann\Comparator\ComparisonFailure;
+use SebastianBergmann\Diff\Differ;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * MessageFactory
+ **/
+class MessageFactory
+{
+    /**
+     * @var OutputInterface
+     */
+    private $output;
+
+    /**
+     * MessageFactory constructor.
+     * @param Output $output
+     */
+    public function __construct(Output $output)
+    {
+        $this->output = $output;
+    }
+
+    /**
+     * @param string $text
+     * @return Message
+     */
+    public function message($text = '')
+    {
+        return new Message($text, $this->output);
+    }
+
+    /**
+     * @param ComparisonFailure $failure
+     * @return Message|null
+     */
+    public function prepareComparisonFailureMessage(ComparisonFailure $failure)
+    {
+        $diff = $this->getDiff($failure->getExpectedAsString(), $failure->getActualAsString());
+        if (!$diff) {
+            return null;
+        }
+
+        return $this
+            ->message($failure->getMessage())
+            ->append($diff);
+    }
+
+    /**
+     * @param string $expected
+     * @param string $actual
+     * @return string
+     */
+    private function getDiff($expected = '', $actual = '')
+    {
+        if (!$actual && !$expected) {
+            return '';
+        }
+
+        $differ = new Differ("- Expected | + Actual\n");
+
+        return $differ->diff($expected, $actual);
+    }
+}
