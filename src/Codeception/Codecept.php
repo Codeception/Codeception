8d80bb4ec7470e8df5de0e4c401785bc3fa1f4f6
Davert <davert.php@resend.cc>
2016-06-29 03:59:28 +0300

    fixed bad merge

--- src/Codeception/Codecept.php
+++ src/Codeception/Codecept.php
@@ -7,7 +7,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class Codecept
 {
-    const VERSION = "2.2.3";
+    const VERSION = "2.2.2";
 
     /**
      * @var \Codeception\PHPUnit\Runner
