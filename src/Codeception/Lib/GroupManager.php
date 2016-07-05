acbef5fcdb3e135710597ff42c059defe255f7dc
eXorus <exorus.spam@gmail.com>
2016-07-05 14:23:15 +0200

    Update GroupManager.php

--- src/Codeception/Lib/GroupManager.php
+++ src/Codeception/Lib/GroupManager.php
@@ -105,6 +105,7 @@ class GroupManager
             $groups = array_merge($groups, \PHPUnit_Util_Test::getGroups(get_class($test), $test->getName(false)));
         }
         if ($test instanceof \PHPUnit_Framework_TestSuite_DataProvider) {
+
             if ($test->testAt(0) != false && $test->testAt(0) instanceof TestInterface) {
                 $groups = array_merge($groups, $test->testAt(0)->getMetadata()->getGroups());
                 $filename = Descriptor::getTestFileName($test->testAt(0));
