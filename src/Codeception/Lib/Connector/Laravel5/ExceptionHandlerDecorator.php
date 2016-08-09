935ebe761d6fc7b828bec6097b6d5befa27080ea
Ivo <bonsi@users.noreply.github.com>
2016-08-09 15:28:43 +0200

    Implementing missing methods for the Illuminate\Contracts\Debug\ExceptionHandler contract
    fixes #3420

--- src/Codeception/Lib/Connector/Laravel5/ExceptionHandlerDecorator.php
+++ src/Codeception/Lib/Connector/Laravel5/ExceptionHandlerDecorator.php
@@ -2,13 +2,14 @@
 namespace Codeception\Lib\Connector\Laravel5;
 
 use Exception;
+use Illuminate\Contracts\Debug\ExceptionHandler as ExceptionHandlerContract;
 
 /**
  * Class ExceptionHandlerDecorator
  *
  * @package Codeception\Lib\Connector\Laravel5
  */
-class ExceptionHandlerDecorator
+class ExceptionHandlerDecorator implements ExceptionHandlerContract
 {
     /**
      * @var ExceptionHandlerContract
@@ -68,5 +69,26 @@ class ExceptionHandlerDecorator
     public function __call($method, $args)
     {
         return call_user_func_array(array($this->laravelExceptionHandler, $method), $args);
+    }
+
+    /**
+     * Report or log an exception.
+     * @param  \Exception $e
+     * @return void
+     */
+    public function report( Exception $e )
+    {
+        $this->laravelExceptionHandler->report($e);
+    }
+
+    /**
+     * Render an exception to the console.
+     * @param  \Symfony\Component\Console\Output\OutputInterface $output
+     * @param  \Exception $e
+     * @return void
+     */
+    public function renderForConsole( $output, Exception $e )
+    {
+        $this->laravelExceptionHandler->renderForConsole($output, $e);
     }
 }
