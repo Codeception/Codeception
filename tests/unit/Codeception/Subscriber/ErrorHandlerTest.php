0a7395ee142b6de3b3a6e5ec61455b16b5fd8cce
Gintautas Miselis <naktibalda@gmail.com>
2016-08-26 22:10:39 +0100

    Handle deprecation messages according to error_level setting (#3460)
    * All error messages are handled according to error_level setting
    
    Fixes #3424
    
    * Only E_USER_DEPRECATED messages will respect error_level setting
    
    Fixes #3424
    
    * Changed assertion to make test easier to debug
    
    * Fixed the test

---
+++ tests/unit/Codeception/Subscriber/ErrorHandlerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Codeception\Event\SuiteEvent;
+use Codeception\Lib\Notification;
+use Codeception\Subscriber\ErrorHandler;
+use Codeception\Suite;
+
+class ErrorHandlerTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testDeprecationMessagesRespectErrorLevelSetting()
+    {
+        $errorHandler = new ErrorHandler();
+
+        $suiteEvent = new SuiteEvent(new Suite(), null, ['error_level' => 'E_ERROR']);
+        $errorHandler->handle($suiteEvent);
+
+        Notification::all(); //clear the messages
+        $errorHandler->errorHandler(E_USER_DEPRECATED, 'deprecated message', __FILE__, __LINE__, []);
+
+        $this->assertEquals([], Notification::all(), 'Deprecation message was added to notifications');
+    }
+}
