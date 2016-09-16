08c42c7906db700d555a308aba3e8227bb42fc3b
Max Kalyabin <maksim@kalyabin.ru>
2016-09-16 07:48:31 +0300

    fixes #3520 nested transactions in yii2 module

--- src/Codeception/Module/Yii2.php
+++ src/Codeception/Module/Yii2.php
@@ -141,6 +141,11 @@ class Yii2 extends Framework implements ActiveRecord, PartedModule
         $this->client->configFile = Configuration::projectDir() . $this->config['configFile'];
         $this->app = $this->client->getApplication();
 
+        // load fixtures before db transaction
+        if (method_exists($test, 'fixtures') && ($fixtures = $test->fixtures()) !== false) {
+            $this->haveFixtures($fixtures);
+        }
+
         if ($this->config['cleanup'] && $this->app->has('db')) {
             $this->transaction = $this->app->db->beginTransaction();
         }
