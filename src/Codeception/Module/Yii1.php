fe09d40bb10527008331ed1f8543ec4b98c9cd9c
Ngoc Phan <ngoc.phan@lazada.com>
2016-10-05 15:09:27 +0700

    fix to get right domain host, not the whole url

--- src/Codeception/Module/Yii1.php
+++ src/Codeception/Module/Yii1.php
@@ -230,7 +230,11 @@ class Yii1 extends Framework implements PartedModule
         if (strpos($template, '<') !== false) {
             $template = str_replace(['<', '>'], '#', $template);
         }
-        $template = preg_quote($template);
+
+		$template = parse_url($template, PHP_URL_HOST);
+
+		$template = preg_quote($template);
+
         foreach ($parameters as $name => $value) {
             $template = str_replace("#$name#", $value, $template);
         }
