747d89b9e9f78f35d64eda5c882587f120fb4c0e
Stefan Kusnir <kusnir@hpe.com>
2016-10-10 14:45:03 +0200

    Add terminal detection

--- src/Codeception/Command/Console.php
+++ src/Codeception/Command/Console.php
@@ -59,7 +59,13 @@ class Console extends Command
         $options['debug'] = true;
         $options['silent'] = true;
         $options['interactive'] = false;
-        $options['colors'] = isset($config['settings']['colors']) ? $config['settings']['colors'] : true;
+        if (isset($config['settings']['colors'])) {
+            $options['colors'] = (bool) $config['settings']['colors'];
+        } else {
+            $options['colors'] = DIRECTORY_SEPARATOR === '\\'
+                ? getenv('ANSICON') !== false || getenv('ConEmuANSI') === 'ON'
+                : function_exists('posix_isatty') && @posix_isatty(STDOUT);
+        }
 
         Debug::setOutput(new Output($options));
 
