0b6ca9d97c6afc6bd6897aa7179154661acf026b
amashigeseiji <tenjuu99@gmail.com>
2016-10-11 00:16:22 +0900

    fix to return right value Yii1::getDomainRegex

--- src/Codeception/Module/Yii1.php
+++ src/Codeception/Module/Yii1.php
@@ -224,8 +224,8 @@ class Yii1 extends Framework implements PartedModule
      */
     private function getDomainRegex($template, $parameters = [])
     {
-        if (preg_match('#https?://(.*?)/#', $template, $matches)) {
-            $template = $matches[1];
+        if ($host = parse_url($template, PHP_URL_HOST)) {
+            $template = $host;
         }
         if (strpos($template, '<') !== false) {
             $template = str_replace(['<', '>'], '#', $template);
