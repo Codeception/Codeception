f64592bbaf33c25a6aa8d6423e16de6f6ee327f5
niclopez <gestoline@hotmail.com>
2016-10-12 00:21:20 +0200

    Fixed meta step detection when xdebug is disabled (#3600)

--- src/Codeception/Step.php
+++ src/Codeception/Step.php
@@ -42,16 +42,13 @@ abstract class Step
 
     public function saveTrace()
     {
-        if (!function_exists('xdebug_get_function_stack')) {
-            return;
-        }
-        ini_set('xdebug.collect_params', '1');
-        $stack = xdebug_get_function_stack();
-        ini_set('xdebug.collect_params', 0);
+        $stack = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT);
+
         if (count($stack) <= self::STACK_POSITION) {
             return;
         }
-        $traceLine = $stack[count($stack) - self::STACK_POSITION];
+
+        $traceLine = $stack[self::STACK_POSITION - 1];
 
         if (!isset($traceLine['file'])) {
             return;
@@ -294,7 +291,7 @@ abstract class Step
                 continue;
             }
 
-            $this->metaStep = new Step\Meta($step['function'], array_values($step['params']));
+            $this->metaStep = new Step\Meta($step['function'], array_values($step['args']));
             $this->metaStep->setTraceInfo($step['file'], $step['line']);
 
             // pageobjects or other classes should not be included with "I"
