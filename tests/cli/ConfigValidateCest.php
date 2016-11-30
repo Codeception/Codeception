871fbc3cc35d27b7acebed4ee61280aa1c5fc1a7
Davert <davert.php@resend.cc>
2016-11-30 22:08:26 +0200

    added --no-ansi for tests

--- tests/cli/ConfigValidateCest.php
+++ tests/cli/ConfigValidateCest.php
@@ -9,7 +9,7 @@ class ConfigValidateCest
 
     public function printsValidConfig(CliGuy $I)
     {
-        $I->executeCommand('config:validate', false);
+        $I->executeCommand('config:validate --no-ansi', false);
         $I->dontSeeInShellOutput('ConfigurationException');
         $I->seeInShellOutput('tests => tests');
         $I->seeInShellOutput('data => tests/_data');
@@ -17,21 +17,21 @@ class ConfigValidateCest
 
     public function validatesInvalidConfigOnParse(CliGuy $I)
     {
-        $I->executeCommand('config:validate -c codeception_invalid.yml', false);
+        $I->executeCommand('config:validate -c codeception_invalid.yml --no-ansi', false);
         $I->seeInShellOutput('Unable to parse at line 8');
         $I->seeInShellOutput('codeception_invalid.yml');
     }
 
     public function validatesInvalidConfigBeforeRun(CliGuy $I)
     {
-        $I->executeCommand('config:validate -c codeception_invalid.yml', false);
+        $I->executeCommand('config:validate -c codeception_invalid.yml --no-ansi', false);
         $I->seeInShellOutput('Unable to parse at line 8');
         $I->seeInShellOutput('codeception_invalid.yml');
     }
 
     public function validatesConfigWithOverrideOption(CliGuy $I)
     {
-        $I->executeCommand('config:validate -o "reporters: report: \Custom\Reporter"');
+        $I->executeCommand('config:validate -o "reporters: report: \Custom\Reporter" --no-ansi');
         $I->seeInShellOutput('report => \Custom\Reporter');
     }
 
