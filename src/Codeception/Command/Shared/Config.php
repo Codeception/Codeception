56aafd11bfb2ea5a2b88e4101c00ed773fc76b34
Michael Bodnarchuk <DavertMik@users.noreply.github.com>
2016-11-30 23:59:22 +0200

    Added validate:config command (#3784)
    * Added validate:config command, to print config and validate them.
    Better validation message added.
    Closes #3766
    
    * added --override option to config:validate
    
    * Added better docs for config:validate command
    
    * style fixes
    
    * config:validate to show current directories, tests fixed
    
    * better messages for per suite config
    
    * added --no-ansi for tests

--- src/Codeception/Command/Shared/Config.php
+++ src/Codeception/Command/Shared/Config.php
@@ -2,6 +2,7 @@
 namespace Codeception\Command\Shared;
 
 use Codeception\Configuration;
+use Symfony\Component\Yaml\Yaml;
 
 trait Config
 {
@@ -20,5 +21,21 @@ trait Config
     {
         Configuration::config($conf);
         return Configuration::suites();
+    }
+
+    protected function overrideConfig($configOptions)
+    {
+        $updatedConfig = [];
+        foreach ($configOptions as $option) {
+            $keys = explode(':', $option);
+            if (count($keys) < 2) {
+                throw new \InvalidArgumentException('--config-option should have config passed as "key:value"');
+            }
+            $value = array_pop($keys);
+            $key = implode(":\n  ", $keys);
+            $config = Yaml::parse("$key:$value");
+            $updatedConfig = array_merge_recursive($updatedConfig, $config);
+        }
+        return Configuration::append($updatedConfig);
     }
 }
