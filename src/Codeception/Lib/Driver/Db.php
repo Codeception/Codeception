07030b46618c069ecc898d59ef987f666d8c0b73
Chuck Burgess <cburgess@progressrail.com>
2016-12-15 09:04:56 -0600

    use PDO attribute to control resultset array index casing

--- src/Codeception/Lib/Driver/Db.php
+++ src/Codeception/Lib/Driver/Db.php
@@ -32,6 +32,7 @@ class Db
     {
         $dbh = new \PDO($dsn, $user, $password);
         $dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->dbh->setAttribute(\PDO::ATTR_CASE, \PDO::CASE_LOWER);
 
         return $dbh;
     }
@@ -76,6 +77,7 @@ class Db
     {
         $this->dbh = new \PDO($dsn, $user, $password);
         $this->dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->dbh->setAttribute(\PDO::ATTR_CASE, \PDO::CASE_LOWER);
 
         $this->dsn = $dsn;
         $this->user = $user;
