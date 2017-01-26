dc24c5c4208dd8821e2830dae9626b825a4acb5a
Aleksandr Kozhevnikov <iamdevice@gmail.com>
2017-01-26 11:59:07 +0400

    psr

--- src/Codeception/Module/MultiDb.php
+++ src/Codeception/Module/MultiDb.php
@@ -337,7 +337,8 @@ class MultiDb extends CodeceptionModule implements DbInterface
      *
      * @param $database string
      */
-    public function amConnectedToDatabase($database) {
+    public function amConnectedToDatabase($database)
+    {
         $this->currentConnection = $database;
         $this->currentDriver = $this->drivers[$database];
     }
