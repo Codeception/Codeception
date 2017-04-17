e83b69040ba11abd3915580eb36f946bd7cff3c3
Luis Montealegre <montealegreluis@gmail.com>
2017-04-17 15:23:06 -0500

    Use an updated implementation of TestListener for PHPUnit 6
    * Add an empty implementation for the
      method addWarning, added to the
      TestListener interface in PHPUnit6
    * Load this implementation only if
      PHPUnit 6 is being used, otherwise use
      the current implementation

--- src/Codeception/PHPUnit/Listener.php
+++ src/Codeception/PHPUnit/Listener.php
@@ -10,119 +10,245 @@ use Exception;
 use PHPUnit_Framework_Test;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
-class Listener implements \PHPUnit_Framework_TestListener
-{
-    /**
-     * @var \Symfony\Component\EventDispatcher\EventDispatcher
-     */
-    protected $dispatcher;
-
-    protected $unsuccessfulTests = [];
-    protected $skippedTests = [];
-    protected $startedTests = [];
-
-    public function __construct(EventDispatcher $dispatcher)
+// PHPUnit 6 definition for the TestListener class includes a new method `addWarning`
+if (interface_exists('PHPUnit\Framework\TestListener')) {
+    class Listener implements \PHPUnit_Framework_TestListener
     {
-        $this->dispatcher = $dispatcher;
-    }
+        /**
+         * @var \Symfony\Component\EventDispatcher\EventDispatcher
+         */
+        protected $dispatcher;
 
-    /**
-     * Risky test.
-     *
-     * @param PHPUnit_Framework_Test $test
-     * @param Exception $e
-     * @param float $time
-     * @since  Method available since Release 4.0.0
-     */
-    public function addRiskyTest(PHPUnit_Framework_Test $test, Exception $e, $time)
-    {
-    }
+        protected $unsuccessfulTests = [];
+        protected $skippedTests = [];
+        protected $startedTests = [];
 
-    public function addFailure(\PHPUnit_Framework_Test $test, \PHPUnit_Framework_AssertionFailedError $e, $time)
-    {
-        $this->unsuccessfulTests[] = spl_object_hash($test);
-        $this->fire(Events::TEST_FAIL, new FailEvent($test, $time, $e));
-    }
-
-    public function addError(\PHPUnit_Framework_Test $test, \Exception $e, $time)
-    {
-        $this->unsuccessfulTests[] = spl_object_hash($test);
-        $this->fire(Events::TEST_ERROR, new FailEvent($test, $time, $e));
-    }
-
-    public function addIncompleteTest(\PHPUnit_Framework_Test $test, \Exception $e, $time)
-    {
-        if (in_array(spl_object_hash($test), $this->skippedTests)) {
-            return;
-        }
-        $this->unsuccessfulTests[] = spl_object_hash($test);
-        $this->fire(Events::TEST_INCOMPLETE, new FailEvent($test, $time, $e));
-        $this->skippedTests[] = spl_object_hash($test);
-    }
-
-    public function addSkippedTest(\PHPUnit_Framework_Test $test, \Exception $e, $time)
-    {
-        if (in_array(spl_object_hash($test), $this->skippedTests)) {
-            return;
-        }
-        $this->unsuccessfulTests[] = spl_object_hash($test);
-        $this->fire(Events::TEST_SKIPPED, new FailEvent($test, $time, $e));
-        $this->skippedTests[] = spl_object_hash($test);
-    }
-
-    public function startTestSuite(\PHPUnit_Framework_TestSuite $suite)
-    {
-        $this->dispatcher->dispatch('suite.start', new SuiteEvent($suite));
-    }
-
-    public function endTestSuite(\PHPUnit_Framework_TestSuite $suite)
-    {
-        $this->dispatcher->dispatch('suite.end', new SuiteEvent($suite));
-    }
-
-    public function startTest(\PHPUnit_Framework_Test $test)
-    {
-        $this->dispatcher->dispatch(Events::TEST_START, new TestEvent($test));
-        if (!$test instanceof TestInterface) {
-            return;
-        }
-        if ($test->getMetadata()->isBlocked()) {
-            return;
+        public function __construct(EventDispatcher $dispatcher)
+        {
+            $this->dispatcher = $dispatcher;
         }
 
-        try {
-            $this->startedTests[] = spl_object_hash($test);
-            $this->fire(Events::TEST_BEFORE, new TestEvent($test));
-        } catch (\PHPUnit_Framework_IncompleteTestError $e) {
-            $test->getTestResultObject()->addFailure($test, $e, 0);
-        } catch (\PHPUnit_Framework_SkippedTestError $e) {
-            $test->getTestResultObject()->addFailure($test, $e, 0);
-        } catch (\Exception $e) {
-            $test->getTestResultObject()->addError($test, $e, 0);
-        }
-    }
-
-    public function endTest(\PHPUnit_Framework_Test $test, $time)
-    {
-        $hash = spl_object_hash($test);
-        if (!in_array($hash, $this->unsuccessfulTests)) {
-            $this->fire(Events::TEST_SUCCESS, new TestEvent($test, $time));
-        }
-        if (in_array($hash, $this->startedTests)) {
-            $this->fire(Events::TEST_AFTER, new TestEvent($test, $time));
+        /**
+         * Risky test.
+         *
+         * @param PHPUnit_Framework_Test $test
+         * @param Exception $e
+         * @param float $time
+         * @since  Method available since Release 4.0.0
+         */
+        public function addRiskyTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+        {
         }
 
-        $this->dispatcher->dispatch(Events::TEST_END, new TestEvent($test, $time));
-    }
+        public function addFailure(\PHPUnit_Framework_Test $test, \PHPUnit_Framework_AssertionFailedError $e, $time)
+        {
+            $this->unsuccessfulTests[] = spl_object_hash($test);
+            $this->fire(Events::TEST_FAIL, new FailEvent($test, $time, $e));
+        }
 
-    protected function fire($event, TestEvent $eventType)
-    {
-        $test = $eventType->getTest();
-        if ($test instanceof TestInterface) {
-            foreach ($test->getMetadata()->getGroups() as $group) {
-                $this->dispatcher->dispatch($event . '.' . $group, $eventType);
+        public function addError(\PHPUnit_Framework_Test $test, \Exception $e, $time)
+        {
+            $this->unsuccessfulTests[] = spl_object_hash($test);
+            $this->fire(Events::TEST_ERROR, new FailEvent($test, $time, $e));
+        }
+
+        // This method was added in PHPUnit 6
+        public function addWarning(\PhpUnit\Framework\Test $test, \PHPUnit\Framework\Warning $e, $time)
+        {
+        }
+
+        public function addIncompleteTest(\PHPUnit_Framework_Test $test, \Exception $e, $time)
+        {
+            if (in_array(spl_object_hash($test), $this->skippedTests)) {
+                return;
+            }
+            $this->unsuccessfulTests[] = spl_object_hash($test);
+            $this->fire(Events::TEST_INCOMPLETE, new FailEvent($test, $time, $e));
+            $this->skippedTests[] = spl_object_hash($test);
+        }
+
+        public function addSkippedTest(\PHPUnit_Framework_Test $test, \Exception $e, $time)
+        {
+            if (in_array(spl_object_hash($test), $this->skippedTests)) {
+                return;
+            }
+            $this->unsuccessfulTests[] = spl_object_hash($test);
+            $this->fire(Events::TEST_SKIPPED, new FailEvent($test, $time, $e));
+            $this->skippedTests[] = spl_object_hash($test);
+        }
+
+        public function startTestSuite(\PHPUnit_Framework_TestSuite $suite)
+        {
+            $this->dispatcher->dispatch('suite.start', new SuiteEvent($suite));
+        }
+
+        public function endTestSuite(\PHPUnit_Framework_TestSuite $suite)
+        {
+            $this->dispatcher->dispatch('suite.end', new SuiteEvent($suite));
+        }
+
+        public function startTest(\PHPUnit_Framework_Test $test)
+        {
+            $this->dispatcher->dispatch(Events::TEST_START, new TestEvent($test));
+            if (!$test instanceof TestInterface) {
+                return;
+            }
+            if ($test->getMetadata()->isBlocked()) {
+                return;
+            }
+
+            try {
+                $this->startedTests[] = spl_object_hash($test);
+                $this->fire(Events::TEST_BEFORE, new TestEvent($test));
+            } catch (\PHPUnit_Framework_IncompleteTestError $e) {
+                $test->getTestResultObject()->addFailure($test, $e, 0);
+            } catch (\PHPUnit_Framework_SkippedTestError $e) {
+                $test->getTestResultObject()->addFailure($test, $e, 0);
+            } catch (\Exception $e) {
+                $test->getTestResultObject()->addError($test, $e, 0);
             }
         }
-        $this->dispatcher->dispatch($event, $eventType);
+
+        public function endTest(\PHPUnit_Framework_Test $test, $time)
+        {
+            $hash = spl_object_hash($test);
+            if (!in_array($hash, $this->unsuccessfulTests)) {
+                $this->fire(Events::TEST_SUCCESS, new TestEvent($test, $time));
+            }
+            if (in_array($hash, $this->startedTests)) {
+                $this->fire(Events::TEST_AFTER, new TestEvent($test, $time));
+            }
+
+            $this->dispatcher->dispatch(Events::TEST_END, new TestEvent($test, $time));
+        }
+
+        protected function fire($event, TestEvent $eventType)
+        {
+            $test = $eventType->getTest();
+            if ($test instanceof TestInterface) {
+                foreach ($test->getMetadata()->getGroups() as $group) {
+                    $this->dispatcher->dispatch($event . '.' . $group, $eventType);
+                }
+            }
+            $this->dispatcher->dispatch($event, $eventType);
+        }
+    }
+} else {
+    // Use PHPUnit 5 TestListener definition
+    class Listener implements \PHPUnit_Framework_TestListener
+    {
+        /**
+         * @var \Symfony\Component\EventDispatcher\EventDispatcher
+         */
+        protected $dispatcher;
+
+        protected $unsuccessfulTests = [];
+        protected $skippedTests = [];
+        protected $startedTests = [];
+
+        public function __construct(EventDispatcher $dispatcher)
+        {
+            $this->dispatcher = $dispatcher;
+        }
+
+        /**
+         * Risky test.
+         *
+         * @param PHPUnit_Framework_Test $test
+         * @param Exception $e
+         * @param float $time
+         * @since  Method available since Release 4.0.0
+         */
+        public function addRiskyTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+        {
+        }
+
+        public function addFailure(\PHPUnit_Framework_Test $test, \PHPUnit_Framework_AssertionFailedError $e, $time)
+        {
+            $this->unsuccessfulTests[] = spl_object_hash($test);
+            $this->fire(Events::TEST_FAIL, new FailEvent($test, $time, $e));
+        }
+
+        public function addError(\PHPUnit_Framework_Test $test, \Exception $e, $time)
+        {
+            $this->unsuccessfulTests[] = spl_object_hash($test);
+            $this->fire(Events::TEST_ERROR, new FailEvent($test, $time, $e));
+        }
+
+        public function addIncompleteTest(\PHPUnit_Framework_Test $test, \Exception $e, $time)
+        {
+            if (in_array(spl_object_hash($test), $this->skippedTests)) {
+                return;
+            }
+            $this->unsuccessfulTests[] = spl_object_hash($test);
+            $this->fire(Events::TEST_INCOMPLETE, new FailEvent($test, $time, $e));
+            $this->skippedTests[] = spl_object_hash($test);
+        }
+
+        public function addSkippedTest(\PHPUnit_Framework_Test $test, \Exception $e, $time)
+        {
+            if (in_array(spl_object_hash($test), $this->skippedTests)) {
+                return;
+            }
+            $this->unsuccessfulTests[] = spl_object_hash($test);
+            $this->fire(Events::TEST_SKIPPED, new FailEvent($test, $time, $e));
+            $this->skippedTests[] = spl_object_hash($test);
+        }
+
+        public function startTestSuite(\PHPUnit_Framework_TestSuite $suite)
+        {
+            $this->dispatcher->dispatch('suite.start', new SuiteEvent($suite));
+        }
+
+        public function endTestSuite(\PHPUnit_Framework_TestSuite $suite)
+        {
+            $this->dispatcher->dispatch('suite.end', new SuiteEvent($suite));
+        }
+
+        public function startTest(\PHPUnit_Framework_Test $test)
+        {
+            $this->dispatcher->dispatch(Events::TEST_START, new TestEvent($test));
+            if (!$test instanceof TestInterface) {
+                return;
+            }
+            if ($test->getMetadata()->isBlocked()) {
+                return;
+            }
+
+            try {
+                $this->startedTests[] = spl_object_hash($test);
+                $this->fire(Events::TEST_BEFORE, new TestEvent($test));
+            } catch (\PHPUnit_Framework_IncompleteTestError $e) {
+                $test->getTestResultObject()->addFailure($test, $e, 0);
+            } catch (\PHPUnit_Framework_SkippedTestError $e) {
+                $test->getTestResultObject()->addFailure($test, $e, 0);
+            } catch (\Exception $e) {
+                $test->getTestResultObject()->addError($test, $e, 0);
+            }
+        }
+
+        public function endTest(\PHPUnit_Framework_Test $test, $time)
+        {
+            $hash = spl_object_hash($test);
+            if (!in_array($hash, $this->unsuccessfulTests)) {
+                $this->fire(Events::TEST_SUCCESS, new TestEvent($test, $time));
+            }
+            if (in_array($hash, $this->startedTests)) {
+                $this->fire(Events::TEST_AFTER, new TestEvent($test, $time));
+            }
+
+            $this->dispatcher->dispatch(Events::TEST_END, new TestEvent($test, $time));
+        }
+
+        protected function fire($event, TestEvent $eventType)
+        {
+            $test = $eventType->getTest();
+            if ($test instanceof TestInterface) {
+                foreach ($test->getMetadata()->getGroups() as $group) {
+                    $this->dispatcher->dispatch($event . '.' . $group, $eventType);
+                }
+            }
+            $this->dispatcher->dispatch($event, $eventType);
+        }
     }
 }
