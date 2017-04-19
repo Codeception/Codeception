43ed899d864969d320bcd3f5e8e6132a81c8d13b
Luis Montealegre <montealegreluis@gmail.com>
2017-04-19 15:35:08 -0500

    Use method_exists instead of class_exists
    * Check if the method expectException is present.
    * Use if statement instead of the && operator to call the other 2 methods.

--- src/Codeception/Test/Unit.php
+++ src/Codeception/Test/Unit.php
@@ -80,10 +80,14 @@ class Unit extends \PHPUnit_Framework_TestCase implements
      */
     public function setExpectedException($exception, $message = '', $code = null)
     {
-        if (class_exists('PHPUnit\Framework\TestCase')) {
+        if (method_exists($this, 'expectException')) {
             $this->expectException($exception);
-            $message !== '' && $this->expectExceptionMessage($message);
-            $code !== null && $this->expectExceptionCode($code);
+            if ($message !== '') {
+                $this->expectExceptionMessage($message);
+            }
+            if ($code !== null) {
+                $this->expectExceptionCode($code);
+            }
         } else {
             parent::setExpectedException($exception, $message, $code);
         }
