a645b5bf27f682a1847b66936faa1ca24c3b5a56
Gintautas Miselis <gintautas@miselis.lt>
2017-05-05 20:34:33 +0100

    Created tests for execution when exception is thrown in _before of module

--- tests/cli/ExceptionInBeforeDoesNotMakeFatalErrorCept.php
+++ tests/cli/ExceptionInBeforeDoesNotMakeFatalErrorCept.php
@@ -3,4 +3,12 @@ $I = new CliGuy($scenario);
 $I->wantTo('see that exception in before does not cause fatal error in after');
 $I->amInPath('tests/data/exception_in_before');
 $I->executeFailCommand('run --xml --no-ansi');
-$I->seeInShellOutput('Tests: 1, Assertions: 0, Errors: 1');
+$I->seeInShellOutput('[Exception] in before');
+$I->dontSeeInShellOutput('[RuntimeException] in cept');
+$I->dontSeeInShellOutput('[RuntimeException] in cest');
+$I->dontSeeInShellOutput('[RuntimeException] in gherkin');
+$I->seeInShellOutput('Tests: 4, Assertions: 0, Errors: 5');
+
+//@todo if Unit format is ever fixed in PHPUnit, uncomment these lines
+//$I->dontSeeInShellOutput('[RuntimeException] in test');
+//$I->seeInShellOutput('Tests: 4, Assertions: 0, Errors: 4');
