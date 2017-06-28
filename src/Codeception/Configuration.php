64166e899a8fba7731c0f8aa95914668be90ef0a
Ivan Kochergin <i.kochergin@it.ecommpay.com>
2017-06-28 18:51:11 +0300

    Fixed by code review

--- src/Codeception/Configuration.php
+++ src/Codeception/Configuration.php
@@ -519,8 +519,12 @@ class Configuration
             $dir = self::$dir . DIRECTORY_SEPARATOR . $dir;
         }
 
-        if (!is_writable($dir)) {
+        if (!file_exists($dir)) {
             @mkdir($dir, 0777, true);
+        }
+
+        if (!is_writable($dir)) {
+            @chmod($dir, 0777);
         }
 
         if (!is_writable($dir)) {
