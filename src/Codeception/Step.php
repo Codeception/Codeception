33a888ec71e918f5514686c13ad79a820757c6b0
Andrew Lambe <alambe@users.noreply.github.com>
2017-07-11 12:01:05 -0500

    fix Step::addMetaStep by dereferencing stack arguments that may have been passed-by-reference, so that later modules like console do not clobber original referenced value by accident

--- src/Codeception/Step.php
+++ src/Codeception/Step.php
@@ -295,7 +295,11 @@ abstract class Step
                 continue;
             }
 
-            $this->metaStep = new Step\Meta($step['function'], array_values($step['args']));
+            // in case arguments were passed by reference, copy args array to ensure dereference.  array_values() does not dereference values
+            $args=[];
+            if(is_array($step['args'])) foreach($step['args'] as $a) $args[]=$a;
+
+            $this->metaStep = new Step\Meta($step['function'], $args);
             $this->metaStep->setTraceInfo($step['file'], $step['line']);
 
             // pageobjects or other classes should not be included with "I"
