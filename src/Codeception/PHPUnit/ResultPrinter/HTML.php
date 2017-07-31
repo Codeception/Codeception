1f0c488cd5c5ec6ee0875dd12b3bcb3609299bed
Martin Pfleger <m.pfleger@gmail.com>
2017-08-01 08:27:44 +1000

    Fix another HTML report encoding issue #4423 (#4422)
    * Fix another HTML report encoding issue #3819
    
    * Use fixed variable #4423

--- src/Codeception/PHPUnit/ResultPrinter/HTML.php
+++ src/Codeception/PHPUnit/ResultPrinter/HTML.php
@@ -150,7 +150,8 @@ class HTML extends CodeceptionResultPrinter
 
         $toggle = $stepsBuffer ? '<span class="toggle">+</span>' : '';
 
-        $testString = preg_replace('~^([\s\w\\\]+):\s~', '<span class="quiet">$1 &raquo;</span> ', ucfirst(Descriptor::getTestAsString($test)));
+        $testString = htmlspecialchars(ucfirst(Descriptor::getTestAsString($test)));
+        $testString = preg_replace('~^([\s\w\\\]+):\s~', '<span class="quiet">$1 &raquo;</span> ', $testString);
 
         $scenarioTemplate->setVar(
             [
