775b0abc441af2976bf6ca4c8672da5385ba031f
Serhey Dolgushev <dolgushev.serhey@gmail.com>
2017-08-09 02:55:38 +0100

    _capabilities documentation fix (#4434)
    $capabilityFunction should return array, otherwise on second test run you got:
    ```
    [PHPUnit_Framework_Exception] Argument 1 passed to Facebook\WebDriver\Remote\DesiredCapabilities::__construct() must be of the type array, object given
    ```

--- src/Codeception/Module/WebDriver.php
+++ src/Codeception/Module/WebDriver.php
@@ -388,7 +388,7 @@ class WebDriver extends CodeceptionModule implements
      *      $name = $test->getMetadata()->getName();
      *      $this->getModule('WebDriver')->_capabilities(function($currentCapabilities) use ($name) {
      *          $currentCapabilities['name'] = $name;
-     *          return new DesiredCapabilities($currentCapabilities);
+     *          return $currentCapabilities;
      *      });
      * }
      * ```
