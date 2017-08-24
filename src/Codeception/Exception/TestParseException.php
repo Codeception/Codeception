215bab02fbe069470376a5fb846ba9a25130b9f3
gaainf <infinum@mail.ru>
2017-08-24 20:50:02 +0300

    add line number to TestParseException exception message (#4446)

--- src/Codeception/Exception/TestParseException.php
+++ src/Codeception/Exception/TestParseException.php
@@ -3,9 +3,13 @@ namespace Codeception\Exception;
 
 class TestParseException extends \Exception
 {
-    public function __construct($fileName, $errors = null)
+    public function __construct($fileName, $errors = null, $line = null)
     {
-        $this->message = "Couldn't parse test '$fileName'";
+        if ($line) {
+            $this->message = "Couldn't parse test '$fileName' on line $line";
+        } else {
+            $this->message = "Couldn't parse test '$fileName'";
+        }
         if ($errors) {
             $this->message .= "\n$errors";
         }
--- src/Codeception/Lib/Parser.php
+++ src/Codeception/Lib/Parser.php
@@ -125,7 +125,7 @@ class Parser
         try {
             self::includeFile($file);
         } catch (\ParseError $e) {
-            throw new TestParseException($file, $e->getMessage());
+            throw new TestParseException($file, $e->getMessage(), $e->getLine());
         } catch (\Exception $e) {
             // file is valid otherwise
         }
