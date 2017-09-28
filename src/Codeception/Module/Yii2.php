949f5ba025a5b5b6e5259a632fc21571c8b3b7a0
Devon Liu <devonliu02@users.noreply.github.com>
2017-09-29 00:34:55 +0800

    unloadFixtures() should run after db transaction (#4497)

--- src/Codeception/Module/Yii2.php
+++ src/Codeception/Module/Yii2.php
@@ -223,16 +223,16 @@ class Yii2 extends Framework implements ActiveRecord, PartedModule
         $_COOKIE = [];
         $_REQUEST = [];
 
+        if ($this->config['transaction'] && $this->transaction) {
+            $this->transaction->rollBack();
+            $this->debugSection('Database', 'Transaction cancelled; all changes reverted.');
+        }
+
         if ($this->config['cleanup']) {
             foreach ($this->loadedFixtures as $fixture) {
                 $fixture->unloadFixtures();
             }
             $this->loadedFixtures = [];
-        }
-      
-        if ($this->config['transaction'] && $this->transaction) {
-            $this->transaction->rollBack();
-            $this->debugSection('Database', 'Transaction cancelled; all changes reverted.');
         }
 
         if ($this->client) {
