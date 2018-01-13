63afeca8912af5f7471333aa293ad58ffc26a39a
Peeratchai Petpadriew <p.petpadriew@gmail.com>
2018-01-13 09:54:18 +0700

    chore(RunsingleTestWithIncludeCesst): remove extra + character

--- tests/cli/RunSingleTestWithIncludeCest.php
+++ tests/cli/RunSingleTestWithIncludeCest.php
@@ -6,11 +6,11 @@ class RunSingleTestWithIncludeCest
     {
         $I->amInPath('tests/data/single_test_with_include');
         $I->wantTo('execute one test with include in config');
-        
+
         $I->executeCommand('run unit/ExampleCest.php');
 
         $I->seeResultCodeIs(0);
-        +$I->dontSeeInShellOutput('RuntimeException');
-        +$I->dontSeeInShellOutput('could not be found');
+        $I->dontSeeInShellOutput('RuntimeException');
+        $I->dontSeeInShellOutput('could not be found');
     }
 }
