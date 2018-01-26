9bac2481c9e5f2cc81f974f6a216232c974a4758
Peeratchai Petpadriew <p.petpadriew@gmail.com>
2018-01-27 04:37:25 +0700

    Fix: fail to run single test with include (#4735)
    * fix: Suite 'path/to/sing-test' cannot be found
    
    Runtime error that has been thrown if we run a single test with include section in the config file
    
    * test: run single test file with include section in config
    
    * fix(Run): $suite and $test have been cleared after call matchSingleTest()
    
    * chore(RunsingleTestWithIncludeCesst): remove extra + character

--- src/Codeception/Command/Run.php
+++ src/Codeception/Command/Run.php
@@ -323,6 +323,11 @@ class Run extends Command
                             // Incorrect include match, continue trying to find one
                             continue;
                         }
+                    } else {
+                        $result = $this->matchSingleTest($suite, $config);
+                        if ($result) {
+                            list(, $suite, $test) = $result;
+                        }
                     }
                 }
 
@@ -331,15 +336,9 @@ class Run extends Command
                     $config = Configuration::config($projectDir);
                 }
             } elseif (!empty($suite)) {
-                // Workaround when codeception.yml is inside tests directory and tests path is set to "."
-                // @see https://github.com/Codeception/Codeception/issues/4432
-                if ($config['paths']['tests'] === '.' && !preg_match('~^\.[/\\\]~', $suite)) {
-                    $suite = './' . $suite;
-                }
-                
-                // Run single test without included tests
-                if (! Configuration::isEmpty() && strpos($suite, $config['paths']['tests']) === 0) {
-                    list(, $suite, $test) = $this->matchTestFromFilename($suite, $config['paths']['tests']);
+                $result = $this->matchSingleTest($suite, $config);
+                if ($result) {
+                    list(, $suite, $test) = $result;
                 }
             }
         }
@@ -379,6 +378,20 @@ class Run extends Command
             if (!$this->codecept->getResult()->wasSuccessful()) {
                 exit(1);
             }
+        }
+    }
+
+    protected function matchSingleTest($suite, $config)
+    {
+        // Workaround when codeception.yml is inside tests directory and tests path is set to "."
+        // @see https://github.com/Codeception/Codeception/issues/4432
+        if ($config['paths']['tests'] === '.' && !preg_match('~^\.[/\\\]~', $suite)) {
+            $suite = './' . $suite;
+        }
+
+        // Run single test without included tests
+        if (! Configuration::isEmpty() && strpos($suite, $config['paths']['tests']) === 0) {
+            return $this->matchTestFromFilename($suite, $config['paths']['tests']);
         }
     }
 
@@ -478,11 +491,11 @@ class Run extends Command
         $tokens = explode(' ', $request);
         foreach ($tokens as $token) {
             $token = preg_replace('~=.*~', '', $token); // strip = from options
-            
+
             if (empty($token)) {
                 continue;
             }
-            
+
             if ($token == '--') {
                 break; // there should be no options after ' -- ', only arguments
             }
