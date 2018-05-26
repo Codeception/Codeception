f280db96c06291e0d33e89b01f4bbfd84d22eab2
Davert <davert.php@resend.cc>
2018-05-27 01:16:48 +0300

    removed incorrect coverage tests

--- tests/coverage/RemoteServerWithCrap4jCept.php
+++
@@ -1,7 +0,0 @@
-<?php
-$I = new CoverGuy($scenario);
-$I->wantTo('try generate remote codecoverage crap4j report');
-$I->amInPath('tests/data/sandbox');
-$I->executeCommand('run remote_server --coverage-crap4j remote_crap.xml');
-$I->seeFileFound('remote_crap.xml', 'tests/_output');
-$I->seeInThisFile('Method Crap Stats');
