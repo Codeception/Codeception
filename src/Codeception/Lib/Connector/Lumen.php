c86a69137dab521f67e34fc0b7934a84065266d3
Istvan Katona <r.etnek@gmail.com>
2018-08-01 00:03:36 +0200

    fix Lumen file upload via REST module (#5082)
    * fix Lumen file upload via REST module
    
    * missing dependency

--- src/Codeception/Lib/Connector/Lumen.php
+++ src/Codeception/Lib/Connector/Lumen.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Facade;
 use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Client;
+use Illuminate\Http\UploadedFile;
 
 class Lumen extends Client
 {
@@ -123,5 +124,44 @@ class Lumen extends Client
         }
 
         $this->module->setApplication($this->app);
+    }
+
+    /**
+     * Make sure files are \Illuminate\Http\UploadedFile instances with the private $test property set to true.
+     * Fixes issue https://github.com/Codeception/Codeception/pull/3417.
+     *
+     * @param array $files
+     * @return array
+     */
+    protected function filterFiles(array $files)
+    {
+        $files = parent::filterFiles($files);
+
+        if (! class_exists('Illuminate\Http\UploadedFile')) {
+            // The \Illuminate\Http\UploadedFile class was introduced in Laravel 5.2.15,
+            // so don't change the $files array if it does not exist.
+            return $files;
+        }
+
+        return $this->convertToTestFiles($files);
+    }
+
+    /**
+     * @param array $files
+     * @return array
+     */
+    private function convertToTestFiles(array $files)
+    {
+        $filtered = [];
+
+        foreach ($files as $key => $value) {
+            if (is_array($value)) {
+                $filtered[$key] = $this->convertToTestFiles($value);
+            } else {
+                $filtered[$key] = UploadedFile::createFromBase($value, true);
+            }
+        }
+
+        return $filtered;
     }
 }
