13f2a1f6a7c461cedb7ea43fc2283cea848aab3e
Arjan <arjanwestdorp@gmail.com>
2018-09-19 16:47:56 +0200

    Make usage of single value more visible

--- src/Codeception/Module/Phalcon.php
+++ src/Codeception/Module/Phalcon.php
@@ -573,10 +573,12 @@ class Phalcon extends Framework implements ActiveRecord, PartedModule
         }
         $query = implode(' AND ', $conditions);
         $this->debugSection('Query', $query);
-        return call_user_func_array([$model, 'findFirst'], [[
-            'conditions' => $query,
-            'bind'       => $bind,
-        ]]);
+        return call_user_func_array([$model, 'findFirst'], [
+            [
+                'conditions' => $query,
+                'bind'       => $bind,
+            ]
+        ]);
     }
 
     /**
