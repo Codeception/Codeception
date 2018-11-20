2cd19a28a342bf46a10ef837b35c1d8a30030b92
ellisgl <xelliss@gmail.com>
2018-11-19 18:24:56 -0600

    ISSUE-2501 Fixes regex filter handling.

--- src/Codeception/Util/JsonType.php
+++ src/Codeception/Util/JsonType.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Codeception\Util;
 
 /**
@@ -41,7 +42,8 @@ class JsonType
      */
     public function __construct($jsonArray)
     {
-        if ($jsonArray instanceof JsonArray) {
+        if ($jsonArray instanceof JsonArray)
+        {
             $jsonArray = $jsonArray->toArray();
         }
         $this->jsonArray = $jsonArray;
@@ -59,7 +61,6 @@ class JsonType
      *     return strpos(' ', $value) !== false;
      * });
      * // => use it as 'string:slug'
-
      *
      * // add custom function to matcher with `len($val)` syntax
      * // parameter matching patterns should be valid regex and start with `/` char
@@ -70,7 +71,7 @@ class JsonType
      * ?>
      * ```
      *
-     * @param $name
+     * @param          $name
      * @param callable $callable
      */
     public static function addCustomFilter($name, callable $callable)
@@ -96,16 +97,20 @@ class JsonType
      */
     public function matches(array $jsonType)
     {
-        if (array_key_exists(0, $this->jsonArray) && is_array($this->jsonArray[0])) {
+        if (array_key_exists(0, $this->jsonArray) && is_array($this->jsonArray[0]))
+        {
             // a list of items
             $msg = '';
-            foreach ($this->jsonArray as $array) {
+            foreach ($this->jsonArray as $array)
+            {
                 $res = $this->typeComparison($array, $jsonType);
-                if ($res !== true) {
+                if ($res !== true)
+                {
                     $msg .= "\n" . $res;
                 }
             }
-            if ($msg) {
+            if ($msg)
+            {
                 return $msg;
             }
             return true;
@@ -115,40 +120,69 @@ class JsonType
 
     protected function typeComparison($data, $jsonType)
     {
-        foreach ($jsonType as $key => $type) {
-            if (!array_key_exists($key, $data)) {
+        foreach ($jsonType as $key => $type)
+        {
+            if (!array_key_exists($key, $data))
+            {
                 return "Key `$key` doesn't exist in " . json_encode($data);
             }
-            if (is_array($jsonType[$key])) {
+            if (is_array($jsonType[$key]))
+            {
                 $message = $this->typeComparison($data[$key], $jsonType[$key]);
-                if (is_string($message)) {
+                if (is_string($message))
+                {
                     return $message;
                 }
                 continue;
             }
-            $matchTypes = preg_split("#(?![^]\(]*\))\|#", $type);
-            $matched = false;
+
+            $regexes = [];
+
+            preg_match_all('/:regex\((.*?[!"#$%&\'*+,.\/:;=?@^_`|~-])\)/', $type, $regexes);
+
+            $type = preg_replace_callback('/:regex\((.*?[!"#$%&\'*+,.\/:;=?@^_`|~-])\)/', function ($m)
+            {
+                static $count = 0;
+
+                return ':regex($$' . $count++ . ')';
+            }, $type);
+
+            $matchTypes  = preg_split("#(?![^]\(]*\))\|#", $type);
+            $matched     = false;
             $currentType = strtolower(gettype($data[$key]));
-            if ($currentType == 'double') {
+            if ($currentType == 'double')
+            {
                 $currentType = 'float';
             }
-            foreach ($matchTypes as $matchType) {
-                $filters = preg_split("#(?![^]\(]*\))\:#", $matchType);
+            foreach ($matchTypes as $matchType)
+            {
+                $filters      = preg_split("#(?![^]\(]*\))\:#", $matchType);
                 $expectedType = trim(strtolower(array_shift($filters)));
 
-                if ($expectedType != $currentType) {
+                if ($expectedType != $currentType)
+                {
                     continue;
                 }
                 $matched = true;
 
-                foreach ($filters as $filter) {
+                foreach ($filters as $filter)
+                {
+                    // Fill regex back in.
+                    $filter   = preg_replace_callback('/\$\$\d+/', function ($m) use ($regexes)
+                    {
+                        $pos = intval(substr($m[0], 2));
+                        return $regexes[1][$pos];
+                    }, $filter);
+
                     $matched = $matched && $this->matchFilter($filter, $data[$key]);
                 }
-                if ($matched) {
+                if ($matched)
+                {
                     break;
                 }
             }
-            if (!$matched) {
+            if (!$matched)
+            {
                 return sprintf("`$key: %s` is of type `$type`", var_export($data[$key], true));
             }
         }
@@ -158,50 +192,63 @@ class JsonType
     protected function matchFilter($filter, $value)
     {
         $filter = trim($filter);
-        if (strpos($filter, '!') === 0) {
+        if (strpos($filter, '!') === 0)
+        {
             return !$this->matchFilter(substr($filter, 1), $value);
         }
 
         // apply custom filters
-        foreach (static::$customFilters as $customFilter => $callable) {
-            if (strpos($customFilter, '/') === 0) {
-                if (preg_match($customFilter, $filter, $matches)) {
+        foreach (static::$customFilters as $customFilter => $callable)
+        {
+            if (strpos($customFilter, '/') === 0)
+            {
+                if (preg_match($customFilter, $filter, $matches))
+                {
                     array_shift($matches);
                     return call_user_func_array($callable, array_merge([$value], $matches));
                 }
             }
-            if ($customFilter == $filter) {
+            if ($customFilter == $filter)
+            {
                 return $callable($value);
             }
         }
 
-        if (strpos($filter, '=') === 0) {
+        if (strpos($filter, '=') === 0)
+        {
             return $value == substr($filter, 1);
         }
-        if ($filter === 'url') {
+        if ($filter === 'url')
+        {
             return filter_var($value, FILTER_VALIDATE_URL);
         }
-        if ($filter === 'date') {
+        if ($filter === 'date')
+        {
             return preg_match(
                 '/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(\.\d+)?(?:Z|(\+|-)([\d|:]*))?$/',
                 $value
             );
         }
-        if ($filter === 'email') { // from http://emailregex.com/
+        if ($filter === 'email')
+        { // from http://emailregex.com/
             // @codingStandardsIgnoreStart
             return preg_match('/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/iD', $value);
             // @codingStandardsIgnoreEnd
         }
-        if ($filter === 'empty') {
+        if ($filter === 'empty')
+        {
             return empty($value);
         }
-        if (preg_match('~^regex\((.*?)\)$~', $filter, $matches)) {
+        if (preg_match('~^regex\((.*?)\)$~', $filter, $matches))
+        {
             return preg_match($matches[1], $value);
         }
-        if (preg_match('~^>([\d\.]+)$~', $filter, $matches)) {
+        if (preg_match('~^>([\d\.]+)$~', $filter, $matches))
+        {
             return (float)$value > (float)$matches[1];
         }
-        if (preg_match('~^<([\d\.]+)$~', $filter, $matches)) {
+        if (preg_match('~^<([\d\.]+)$~', $filter, $matches))
+        {
             return (float)$value < (float)$matches[1];
         }
     }
