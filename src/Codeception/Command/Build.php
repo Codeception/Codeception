654ec852dadaaea3ec2c025e735ed9ce71895ce1
Jeferson Almeida <jeferson.almeida@codelathe.com>
2019-03-05 11:47:36 -0300

    Fixing the bad build command message.

--- src/Codeception/Command/Build.php
+++ src/Codeception/Command/Build.php
@@ -61,13 +61,13 @@ class Build extends Command
     private function buildActions(array $settings)
     {
         $actionsGenerator = new ActionsGenerator($settings);
+        $content = $actionsGenerator->produce();
         $this->output->writeln(
             " -> {$settings['actor']}Actions.php generated successfully. "
             . $actionsGenerator->getNumMethods() . " methods added"
         );
-        
-        $content = $actionsGenerator->produce();
-        
+
+
         $file = $this->createDirectoryFor(Configuration::supportDir() . '_generated', $settings['actor']);
         $file .= $this->getShortClassName($settings['actor']) . 'Actions.php';
         return $this->createFile($file, $content, true);
