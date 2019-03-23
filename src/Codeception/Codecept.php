547a64cb31edcf1902b296c511f5ca74101bcb4c
Gintautas Miselis <gintautas@miselis.lt>
2019-03-23 19:57:45 +0200

    Changelog for 2.5.5

--- src/Codeception/Codecept.php
+++ src/Codeception/Codecept.php
@@ -7,7 +7,7 @@ use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class Codecept
 {
-    const VERSION = "2.5.4";
+    const VERSION = "2.5.5";
 
     /**
      * @var \Codeception\PHPUnit\Runner
