779fc2cdb5477278102fd6ae3d83a0b1a0907380
Daniel Ruf <daniel.ruf@ueberbit.de>
2019-05-07 11:14:52 +0200

    Set the VERSION constant to 3.0.0

--- src/Codeception/Codecept.php
+++ src/Codeception/Codecept.php
@@ -7,7 +7,7 @@ use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class Codecept
 {
-    const VERSION = "2.6.0";
+    const VERSION = "3.0.0";
 
     /**
      * @var \Codeception\PHPUnit\Runner
