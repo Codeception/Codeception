532045e05c076d6c9934b83a9bd6fc5fe13211b1
kartavik <roman.varkuta@gmail.com>
2019-05-24 12:34:38 +0300

    Change wrapper class for assertion method

--- src/Codeception/Module/Cli.php
+++ src/Codeception/Module/Cli.php
@@ -62,7 +62,7 @@ class Cli extends CodeceptionModule
      */
     public function seeInShellOutput($text)
     {
-        \PHPUnit\Framework\Assert::assertStringContainsString($text, $this->output);
+        \Codeception\PHPUnit\TestCase::assertStringContainsString($text, $this->output);
     }
 
     /**
