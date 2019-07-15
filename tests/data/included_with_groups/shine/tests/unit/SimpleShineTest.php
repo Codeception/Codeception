1a4136bad8beaea1c56934bd3b2447ed0ebba951
Filippo Tessarotto <zoeslam@gmail.com>
2019-07-15 15:20:51 +0200

    Bug proof: include and groups don't work together

---
+++ tests/data/included_with_groups/shine/tests/unit/SimpleShineTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Jazz;
+
+final class SimpleShineTest extends \Codeception\Test\Unit
+{
+    public function testTwo()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testThree()
+    {
+        $this->assertTrue(true);
+    }
+}
