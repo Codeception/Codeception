e9740f490ae9937932d04cd12df354b0d91497dd
Davert <davert.ua@gmail.com>
2019-08-10 13:12:40 +0300

    minor fixes

--- src/Codeception/Util/PathResolver.php
+++ src/Codeception/Util/PathResolver.php
@@ -128,7 +128,7 @@ class PathResolver
     public static function isPathAbsolute($path)
     {
         if (DIRECTORY_SEPARATOR === '/') {
-            return mb_substr($path, 0, 1) === DIRECTORY_SEPARATOR;
+            return substr($path, 0, 1) === DIRECTORY_SEPARATOR;
         }
 
         return preg_match('#^[A-Z]:(?![^/\\\])#i', $path) === 1;
