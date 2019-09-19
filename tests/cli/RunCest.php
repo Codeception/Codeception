57977b3b43c947fc4fc967ce3e37935114efd56e
Reinhold Füreder <r.fuereder@xortex.com>
2019-09-19 15:18:18 +0200

    #5568 Missing HTML snapshots of failing PhpBrowser acceptance tests in HTML Report after update to v3.0.x
    Fix: Failed again in M$ Windows CI because not the full output uses platform dependent directory separator (see https://ci.appveyor.com/project/DavertMik/codeception/builds/27523181) => make the test hopefully succeed at long last by also hard-coding the relative report path

--- tests/cli/RunCest.php
+++ tests/cli/RunCest.php
@@ -643,11 +643,12 @@ EOF
         $I->wantTo('execute tests with PhpBrowser with html output and check html');
         $I->executeFailCommand('run phpbrowser_html_report --html');
         $I->seeResultCodeIsNot(0);
+        $expectedRelReportPath = 'tests/_output';
         $expectedReportFilename    = 'CodeceptionIssue5568Cest.failureShouldCreateHtmlSnapshot.fail.html';
-        $expectedReportAbsFilename = join(DIRECTORY_SEPARATOR, array(getcwd(), 'tests', '_output', $expectedReportFilename));
+        $expectedReportAbsFilename = join(DIRECTORY_SEPARATOR, array(getcwd(), $expectedRelReportPath, $expectedReportFilename));
         $I->seeInShellOutput('Html: ' . $expectedReportAbsFilename);
         $I->seeInShellOutput('Response: ' . $expectedReportAbsFilename);
-        $I->seeFileFound('report.html', 'tests/_output');
+        $I->seeFileFound('report.html', $expectedRelReportPath);
         $I->seeInThisFile("See <a href='" . $expectedReportFilename . "' target='_blank'>HTML snapshot</a> of a failed page");
     }
 
