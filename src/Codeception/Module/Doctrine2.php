d3d4daf39fd532c454a53edd2735c94a42f7dd86
Ole Rößner <o.roessner@neusta.de>
2019-09-24 14:28:41 +0200

    catch correct MappingException.

--- src/Codeception/Module/Doctrine2.php
+++ src/Codeception/Module/Doctrine2.php
@@ -19,7 +19,7 @@ use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\DataFixtures\Loader;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
-use Doctrine\ORM\Mapping\MappingException;
+use Doctrine\Common\Persistence\Mapping\MappingException;
 use Doctrine\ORM\QueryBuilder;
 use Exception;
 use InvalidArgumentException;
