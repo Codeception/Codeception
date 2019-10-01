a5c24ec0e186cdc2ef7e9e68309b28ee7ab504c3
Bernd Stellwag <burned@zerties.org>
2019-10-01 17:11:55 +0200

    fix return type
    grabFromRepository returns the result of `getSingleScalarResult`, which according to the documentation returns `mixed`:
    https://www.doctrine-project.org/api/orm/2.6/Doctrine/ORM/Query.html#method_getSingleScalarResult

--- src/Codeception/Module/Doctrine2.php
+++ src/Codeception/Module/Doctrine2.php
@@ -880,7 +880,7 @@ EOF;
      * @param $entity
      * @param $field
      * @param array $params
-     * @return array
+     * @return mixed
      */
     public function grabFromRepository($entity, $field, $params = [])
     {
