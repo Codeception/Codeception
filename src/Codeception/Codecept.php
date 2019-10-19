9c69ec708f15140be20341d342c330d68fb04197
Gintautas Miselis <gintautas@miselis.lt>
2019-10-19 17:41:56 +0300

    Changed version number to 4.0.0

--- src/Codeception/Codecept.php
+++ src/Codeception/Codecept.php
@@ -7,7 +7,7 @@ use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class Codecept
 {
-    const VERSION = "3.1.2";
+    const VERSION = "4.0.0";
 
     /**
      * @var \Codeception\PHPUnit\Runner
