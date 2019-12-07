bc4e1e8741956e3627e3a44467db4456d35d328f
Tai Poh Nean <tai.pohnean@propsage.com>
2019-12-07 12:58:54 +0800

    Add ability to stash/unstash commands in interactive mode.

--- src/Codeception/Lib/Actor/Shared/Pause.php
+++ src/Codeception/Lib/Actor/Shared/Pause.php
@@ -15,15 +15,91 @@ trait Pause
             throw new \Exception('Hoa Console is not installed. Please add `hoa/console` to composer.json');
         }
 
+        $autoStash = false;
+        $stashedCommands = [];
+
         $I = $this;
+        $output = new ConsoleOutput();
         $readline = new \Hoa\Console\Readline\Readline();
 
         $readline->setAutocompleter(
             new \Hoa\Console\Readline\Autocompleter\Word(get_class_methods($this))
         );
-        $output = new ConsoleOutput();
+
+        $stashFn = function (\Hoa\Console\Readline\Readline $self, $isManual = true) use (&$stashedCommands) {
+            $lastCommand = $self->previousHistory();
+
+            \Hoa\Console\Cursor::clear('↔');
+
+            if (strlen($lastCommand) > 0) {
+                $stashedCommands[] = "\$I->{$lastCommand};";
+                codecept_debug("Command stashed: \$I->{$lastCommand};");
+            } else {
+                codecept_debug("Nothing to stash.");
+            }
+
+            if ($isManual) {
+                \Hoa\Console\Console::getOutput()->writeAll($self->getPrefix() . $self->getLine());
+            }
+
+            $self->nextHistory();
+
+            return \Hoa\Console\Readline\Readline::STATE_CONTINUE;
+        };
+
+        $clearStashFn = function (\Hoa\Console\Readline\Readline $self) use (&$stashedCommands) {
+            $stashedCommands = [];
+
+            \Hoa\Console\Cursor::clear('↔');
+
+            codecept_debug("Stash cleared.");
+
+            \Hoa\Console\Console::getOutput()->writeAll($self->getPrefix() . $self->getLine());
+
+            return \Hoa\Console\Readline\Readline::STATE_CONTINUE;
+        };
+
+        $viewStashedFn = function (\Hoa\Console\Readline\Readline $self) use (&$stashedCommands, $output) {
+            \Hoa\Console\Cursor::clear('↔');
+
+            if (!empty($stashedCommands)) {
+                $output->writeln("\n<comment>Stashed commands:</comment>");
+                codecept_debug(implode("\n", $stashedCommands) . "\n");
+            } else {
+                codecept_debug("No commands stashed.");
+            }
+
+            \Hoa\Console\Console::getOutput()->writeAll($self->getPrefix() . $self->getLine());
+
+            return \Hoa\Console\Readline\Readline::STATE_CONTINUE;
+        };
+
+        $toggleAutoStashFn = function (\Hoa\Console\Readline\Readline $self) use (&$autoStash) {
+            \Hoa\Console\Cursor::clear('↔');
+
+            $autoStash = !$autoStash;
+
+            codecept_debug("Autostash " . ($autoStash ? 'enabled' : 'disabled') . '.');
+
+            \Hoa\Console\Console::getOutput()->writeAll($self->getPrefix() . $self->getLine());
+
+            return \Hoa\Console\Readline\Readline::STATE_CONTINUE;
+        };
+
+        $tput = \Hoa\Console\Console::getTput();
+        $readline->addMapping($tput->get('key_f5'), $stashFn);
+        $readline->addMapping($tput->get('key_f6'), $toggleAutoStashFn);
+        $readline->addMapping($tput->get('key_f8'), $viewStashedFn);
+        $readline->addMapping($tput->get('key_f10'), $clearStashFn);
+
         $output->writeln("  <comment>Execution PAUSED, starting interactive shell...</comment>");
-        $output->writeln("  Type in commands to try them, ENTER to continue, TAB to auto-complete");
+        $output->writeln("  Type in commands to try them:");
+        $output->writeln("  - <info>ENTER</info> to continue");
+        $output->writeln("  - <info>TAB</info> to auto-complete");
+        $output->writeln("  - <info>F5</info> to stash a command");
+        $output->writeln("  - <info>F6</info> to toggle auto-stashing of successful commands");
+        $output->writeln("  - <info>F8</info> to view stashed commands");
+        $output->writeln("  - <info>F10</info> to clear stashed commands");
 
         $result = '';
 
@@ -43,7 +119,11 @@ trait Pause
                     if (!is_object($result)) {
                         codecept_debug($result);
                     }
-                    codecept_debug('>> Result saved to $result variable, you can use it in next commands');
+                    codecept_debug('>> Result saved to $result variable, you can usZZe it in next commands');
+                }
+
+                if ($autoStash) {
+                    call_user_func($stashFn, $readline, false);
                 }
             } catch (\PHPUnit\Framework\AssertionFailedError $fail) {
                 $output->writeln("<error>fail</error> " . $fail->getMessage());
