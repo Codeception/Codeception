bc588b0f4c9b5a963ad3357d0b95472d767b9326
Gintautas Miselis <gintautas@miselis.lt>
2020-02-07 18:08:18 +0200

    8.0 Use tests from Codeception 4.0 branch (#75)
    * Test against Codeception 4.0 branch
    
    * Got tests working
    
    * Fixed build
    
    * Don't test constraints
    
    They were moved to lib-innerbrowser
    
    * Run Asserts tests

--- RoboFile.php
+++ RoboFile.php
@@ -7,21 +7,40 @@
 class RoboFile extends \Robo\Tasks
 {
     // define public methods as commands
-    public function prepare()
+    public function prepareDependencies()
     {
         $config = json_decode(file_get_contents(__DIR__ . '/composer.json'), true);
 
         $config['name'] = 'codeception/phpunit-wrapper-test';
         $config['require-dev']['codeception/codeception'] = getenv('CODECEPTION_VERSION');
+        $config['require-dev']['codeception/module-asserts'] = 'dev-master';
+        $config['require-dev']['codeception/module-cli'] = '*';
+        $config['require-dev']['codeception/module-db'] = '*';
+        $config['require-dev']['codeception/module-filesystem'] = '*';
+        $config['require-dev']['codeception/module-phpbrowser'] = '*';
+        $config['require-dev']['codeception/util-universalframework'] = '*';
         $config['replace'] = ['codeception/phpunit-wrapper' => '*'];
 
-        file_put_contents(__DIR__ . '/composer.json', json_encode($config));
+        file_put_contents(__DIR__ . '/composer.json', json_encode($config, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
     }
 
-    public function test($params)
+    public function prepareTests()
     {
-        return $this->taskExec(__DIR__ . '/vendor/bin/codecept run ' . $params)
-            ->dir(__DIR__ .'/vendor/codeception/codeception')
-            ->run();
+        $this->_copyDir(__DIR__ . '/vendor/codeception/codeception/tests', __DIR__ . '/tests');
+        $this->_copy(__DIR__ . '/vendor/codeception/codeception/codeception.yml', __DIR__ .'/codeception.yml');
+        $this->_symlink(__DIR__ . '/vendor/bin/codecept', __DIR__ . '/codecept');
+    }
+
+    public function prepareTestAutoloading()
+    {
+        $config = json_decode(file_get_contents(__DIR__ . '/composer.json'), true);
+        $config['autoload-dev'] = [
+            'classmap' => [
+                'tests/cli/_steps',
+                'tests/data/DummyClass.php',
+                'tests/data/claypit/tests/_data'
+            ]
+        ];
+        file_put_contents(__DIR__ . '/composer.json', json_encode($config, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
     }
 }
