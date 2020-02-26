7841ebda784c3398f616d4fda37134e76fca26f0
Liam Toohey <lt@amp.co>
2020-02-26 09:24:53 +0000

    import Symfony\Component\Console\Input\InputOption class

--- src/Codeception/Command/Build.php
+++ src/Codeception/Command/Build.php
@@ -7,6 +7,7 @@ use Codeception\Lib\Generator\Actor as ActorGenerator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * Generates Actor classes (initially Guy classes) from suite configs.
