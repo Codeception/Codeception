8f6c0eb7633185082affe6fd957585bacb1525de
Gintautas Miselis <gintautas@miselis.lt>
2020-03-20 10:27:08 +0200

    Fix custom reporter test on PHP 5.6

---
+++ tests/data/claypit/tests/_data/MyReportPrinter9.php
@@ -0,0 +1,42 @@
+<?php 
+use Codeception\PHPUnit\ConsolePrinter;
+use Codeception\PHPUnit\ResultPrinter;
+
+/**
+ * For testing PHPUnit 9+
+ */
+class MyReportPrinter extends ResultPrinter implements ConsolePrinter
+{
+    public function endTest(\PHPUnit\Framework\Test $test, float $time) : void
+    {
+        $name = \Codeception\Test\Descriptor::getTestAsString($test);
+        if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_FAILURE) {
+            $this->write('×');
+        } else {
+            if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_SKIPPED) {
+                $this->write('S');
+            } else {
+                if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_INCOMPLETE) {
+                    $this->write('I');
+                } else {
+                    if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_ERROR) {
+                        $this->write('E');
+                    } else {
+                        $this->write('✔');
+                    }
+                }
+            }
+        }
+
+        if (strlen($name) > 75) {
+            $name = substr($name, 0, 70);
+        }
+        $this->write(" $name \n");
+    }
+
+    public function printResult(\PHPUnit\Framework\TestResult $result): void
+    {
+
+    }
+
+}
