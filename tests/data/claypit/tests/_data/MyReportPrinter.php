2852c4aa4f4e46292b9cdc652a9567518939b7fa
Gintautas Miselis <gintautas@miselis.lt>
2020-03-20 19:41:09 +0200

    Revert "Test custom reporter against all versions of PHPUnit"
    This reverts commit 920c8d32f038fd082c8d414cb692df387619a711.

--- tests/data/claypit/tests/_data/MyReportPrinter.php
+++ tests/data/claypit/tests/_data/MyReportPrinter.php
@@ -2,112 +2,38 @@
 use Codeception\PHPUnit\ConsolePrinter;
 use Codeception\PHPUnit\ResultPrinter;
 
-if (\PHPUnit\Runner\Version::series() < 7) {
-    class MyReportPrinter extends ResultPrinter implements ConsolePrinter
+class MyReportPrinter extends ResultPrinter implements ConsolePrinter
+{
+    public function endTest(\PHPUnit\Framework\Test $test, $time)
     {
-        public function endTest(\PHPUnit\Framework\Test $test, $time)
-        {
-            $name = \Codeception\Test\Descriptor::getTestAsString($test);
-            if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_FAILURE) {
-                $this->write('×');
+        $name = \Codeception\Test\Descriptor::getTestAsString($test);
+        if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_FAILURE) {
+            $this->write('×');
+        } else {
+            if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_SKIPPED) {
+                $this->write('S');
             } else {
-                if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_SKIPPED) {
-                    $this->write('S');
+                if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_INCOMPLETE) {
+                    $this->write('I');
                 } else {
-                    if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_INCOMPLETE) {
-                        $this->write('I');
+                    if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_ERROR) {
+                        $this->write('E');
                     } else {
-                        if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_ERROR) {
-                            $this->write('E');
-                        } else {
-                            $this->write('✔');
-                        }
+                        $this->write('✔');
                     }
                 }
             }
-
-            if (strlen($name) > 75) {
-                $name = substr($name, 0, 70);
-            }
-            $this->write(" $name \n");
         }
 
-        public function printResult(\PHPUnit\Framework\TestResult $result)
-        {
-
+        if (strlen($name) > 75) {
+            $name = substr($name, 0, 70);
         }
-
+        $this->write(" $name \n");
     }
-} else if (\PHPUnit\Runner\Version::series() < 9) {
-    class MyReportPrinter extends ResultPrinter implements ConsolePrinter
+
+    public function printResult(\PHPUnit\Framework\TestResult $result)
     {
-        public function endTest(\PHPUnit\Framework\Test $test, float $time) : void
-        {
-            $name = \Codeception\Test\Descriptor::getTestAsString($test);
-            if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_FAILURE) {
-                $this->write('×');
-            } else {
-                if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_SKIPPED) {
-                    $this->write('S');
-                } else {
-                    if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_INCOMPLETE) {
-                        $this->write('I');
-                    } else {
-                        if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_ERROR) {
-                            $this->write('E');
-                        } else {
-                            $this->write('✔');
-                        }
-                    }
-                }
-            }
-
-            if (strlen($name) > 75) {
-                $name = substr($name, 0, 70);
-            }
-            $this->write(" $name \n");
-        }
-
-        public function printResult(\PHPUnit\Framework\TestResult $result)
-        {
-
-        }
-
+        
     }
-} else {
-    class MyReportPrinter extends ResultPrinter implements ConsolePrinter
-    {
-        public function endTest(\PHPUnit\Framework\Test $test, float $time) : void
-        {
-            $name = \Codeception\Test\Descriptor::getTestAsString($test);
-            if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_FAILURE) {
-                $this->write('×');
-            } else {
-                if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_SKIPPED) {
-                    $this->write('S');
-                } else {
-                    if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_INCOMPLETE) {
-                        $this->write('I');
-                    } else {
-                        if ($this->testStatus == \PHPUnit\Runner\BaseTestRunner::STATUS_ERROR) {
-                            $this->write('E');
-                        } else {
-                            $this->write('✔');
-                        }
-                    }
-                }
-            }
 
-            if (strlen($name) > 75) {
-                $name = substr($name, 0, 70);
-            }
-            $this->write(" $name \n");
-        }
-
-        public function printResult(\PHPUnit\Framework\TestResult $result): void
-        {
-
-        }
-
-    }
 }
