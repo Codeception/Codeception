5e6c2fa43bd7d8cbc0e05745625cd4147764aa71
Istvan Katona <r.etnek@gmail.com>
2020-11-03 07:24:23 +0100

    add ServerConstAdapter for phpdotenv v5 (#6015)

--- src/Codeception/Lib/ParamsLoader.php
+++ src/Codeception/Lib/ParamsLoader.php
@@ -119,6 +119,7 @@ class ParamsLoader
                 //dotenv v5
                 $repository = \Dotenv\Repository\RepositoryBuilder::createWithNoAdapters()
                     ->addAdapter(\Dotenv\Repository\Adapter\EnvConstAdapter::class)
+                    ->addAdapter(\Dotenv\Repository\Adapter\ServerConstAdapter::class)
                     ->make();
                 $dotEnv = \Dotenv\Dotenv::create($repository, codecept_root_dir(), $this->paramStorage);
             } elseif (class_exists('Dotenv\Repository\RepositoryBuilder')) {
