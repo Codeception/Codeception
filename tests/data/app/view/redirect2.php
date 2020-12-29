f180f92f07d087a8702d9c835de2384a058dd55f
Gintautas Miselis <gintautas@miselis.lt>
2020-12-29 20:02:45 +0200

    CI: Run module-phpbrowser tests

--- tests/data/app/view/redirect2.php
+++ tests/data/app/view/redirect2.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <meta http-equiv="refresh" content="0; url=http://localhost:8000/info" />
+    <meta http-equiv="refresh" content="0; url=http://localhost:8008/info" />
 </head>
 <body>
     <h1>Redirecting...</h1>
--- tests/data/app/view/redirect_interval.php
+++ tests/data/app/view/redirect_interval.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <meta http-equiv="refresh" content="1800; url=http://localhost:8000/info" />
+    <meta http-equiv="refresh" content="1800; url=http://localhost:8008/info" />
 </head>
 <body>
     <h1>Redirecting in 1800 seconds</h1>
