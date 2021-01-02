4773f3716dd671309984e9d08cea2c6553187fb7
Gintautas Miselis <gintautas@miselis.lt>
2021-01-02 18:43:02 +0200

    Ignore . namespace in generators
    When someone pass path as a class name, e.g. ./foo
    
    Fixes #5818

--- src/Codeception/Util/Shared/Namespaces.php
+++ src/Codeception/Util/Shared/Namespaces.php
@@ -5,15 +5,9 @@ trait Namespaces
 {
     protected function breakParts($class)
     {
-        $class      = str_replace('/', '\\', $class);
-        $namespaces = explode('\\', $class);
-        if (count($namespaces)) {
-            $namespaces[0] = ltrim($namespaces[0], '\\');
-        }
-        if (!$namespaces[0]) {
-            array_shift($namespaces);
-        } // remove empty namespace caused of \\
-        return $namespaces;
+        // removing leading slashes and dots first
+        $class = str_replace('/', '\\', ltrim($class, './\\'));
+        return explode('\\', $class);
     }
 
     protected function getShortClassName($class)
