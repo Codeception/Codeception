11c78475c7c7b9dca3abf19403461129c54cc46f
Tavo Nieves J <ganieves@outlook.com>
2021-01-17 17:42:51 -0500

    Use strict types in exceptions

--- src/Codeception/Exception/ConditionalAssertionFailed.php
+++ src/Codeception/Exception/ConditionalAssertionFailed.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Codeception\Exception;
 
 class ConditionalAssertionFailed extends \PHPUnit\Framework\AssertionFailedError
--- src/Codeception/Exception/ContentNotFound.php
+++ src/Codeception/Exception/ContentNotFound.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Codeception\Exception;
 
 class ContentNotFound extends \PHPUnit\Framework\AssertionFailedError
