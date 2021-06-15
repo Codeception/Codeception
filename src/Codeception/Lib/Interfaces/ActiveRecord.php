b1ab3aa2481b56fdf9b54a295495a62e4a20290b
Tavo Nieves J <ganieves@outlook.com>
2021-06-14 19:42:32 -0500

    Fix ActiveRecord::haveRecord

--- src/Codeception/Lib/Interfaces/ActiveRecord.php
+++ src/Codeception/Lib/Interfaces/ActiveRecord.php
@@ -4,7 +4,7 @@ namespace Codeception\Lib\Interfaces;
 
 interface ActiveRecord extends ORM
 {
-    public function haveRecord(string $model, array $attributes = []): void;
+    public function haveRecord(string $model, array $attributes = []);
 
     public function seeRecord(string $model, array $attributes = []): void;
 
