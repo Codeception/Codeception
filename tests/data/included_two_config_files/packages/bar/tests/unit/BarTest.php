bfd2b2fac05c96b278c9042d7d392164ee54b0dd
Calvin Alkan <calvin@snicco.de>
2022-01-21 23:38:22 +0100

    Fix duplicate test runs when codeception.yml and dist.yml are present in multi-app setup

---
+++ tests/data/included_two_config_files/packages/bar/tests/unit/BarTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace bar\unit;
+
+final class BarTest extends \PHPUnit\Framework\TestCase
+{
+    public function testBar()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/data/included_two_config_files/packages/foo/tests/unit/FooTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace foo\unit;
+
+final class FooTest extends \PHPUnit\Framework\TestCase
+{
+    public function testFoo()
+    {
+        $this->assertTrue(true);
+    }
+}
