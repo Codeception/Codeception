dc7a002fbe236515e3c358d9380bd40402c5db7f
Gintautas Miselis <gintautas@miselis.lt>
2022-03-05 18:49:20 +0200

    Fix handling of previous exception in ExtensionException #6370

--- src/Codeception/Exception/ExtensionException.php
+++ src/Codeception/Exception/ExtensionException.php
@@ -5,7 +5,7 @@ class ExtensionException extends \Exception
 {
     public function __construct($extension, $message, \Exception $previous = null)
     {
-        parent::__construct($message, $previous);
+        parent::__construct($message, 0, $previous);
         if (is_object($extension)) {
             $extension = get_class($extension);
         }
