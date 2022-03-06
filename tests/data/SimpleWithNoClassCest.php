be37f3027a7afe00f151b74abb15abebe5ffb9d8
Gintautas Miselis <gintautas@miselis.lt>
2022-03-06 13:28:48 +0200

    Fix mistype in method name

--- tests/data/SimpleWithNoClassCest.php
+++ tests/data/SimpleWithNoClassCest.php
@@ -2,7 +2,7 @@
 
 class SimpleWithNoClassCest
 {
-    public function phpFuncitons(CodeGuy $I)
+    public function phpFunctions(CodeGuy $I)
     {
         $I->execute(fn () => strtoupper('hello'));
         $I->seeResultEquals('HELLO');
