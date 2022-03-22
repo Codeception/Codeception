366938435a300d56bb335ca3be92424bcfa3ba60
W0rma <beck.worma@gmail.com>
2022-03-22 12:01:16 +0100

    Remove BC layer for php-code-coverage < 9 since version ^9.2 is already a direct dependency

--- src/Codeception/Coverage/Filter.php
+++ src/Codeception/Coverage/Filter.php
@@ -65,13 +65,7 @@ class Filter
                     : $this->matchWildcardPattern($fileOrDir);
 
                 foreach ($finder as $file) {
-                    if (method_exists($filter, 'addFileToWhitelist')) {
-                        //php-code-coverage 8 or older
-                        $filter->addFileToWhitelist((string)$file);
-                    } else {
-                        //php-code-coverage 9+
-                        $filter->includeFile((string)$file);
-                    }
+                    $filter->includeFile((string)$file);
                 }
             }
         }
@@ -87,13 +81,7 @@ class Filter
                         : $this->matchWildcardPattern($fileOrDir);
 
                     foreach ($finder as $file) {
-                        if (method_exists($filter, 'removeFileFromWhitelist')) {
-                            //php-code-coverage 8 or older
-                            $filter->removeFileFromWhitelist((string)$file);
-                        } else {
-                            //php-code-coverage 9+
-                            $filter->excludeFile((string)$file);
-                        }
+                        $filter->excludeFile((string)$file);
                     }
                 } catch (DirectoryNotFoundException) {
                     continue;
