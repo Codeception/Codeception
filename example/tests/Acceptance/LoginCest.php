e031b6cbf6586b73855b2296583cf9fe926382bc
Gintautas Miselis <gintautas@miselis.lt>
2022-07-27 17:03:35 +0300

    Generate test examples

---
+++ example/tests/Acceptance/LoginCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class LoginCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
---
+++ example/tests/Functional/LoginCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Tests\Functional;
+
+use Tests\Support\FunctionalTester;
+
+class LoginCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+    }
+}
