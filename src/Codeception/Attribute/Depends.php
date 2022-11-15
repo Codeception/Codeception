87faeb84c616f6e4cb6d402ac10c6c18f5b93952
Malte Dölker <mdoelker@me.com>
2022-11-15 19:02:30 +0100

    Allow multiple params on Depends attribute

--- src/Codeception/Attribute/Depends.php
+++ src/Codeception/Attribute/Depends.php
@@ -9,7 +9,7 @@ use Attribute;
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 final class Depends
 {
-    public function __construct(string $testName)
+    public function __construct(string ...$testNames)
     {
     }
 }
