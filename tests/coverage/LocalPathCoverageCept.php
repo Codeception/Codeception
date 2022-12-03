102a034a41c8370d9e09616266263b0266e4259f
Gintautas Miselis <gintautas@miselis.lt>
2022-12-03 12:29:52 +0200

    Fix code style of previous commit

--- tests/coverage/LocalPathCoverageCept.php
+++ tests/coverage/LocalPathCoverageCept.php
@@ -4,6 +4,7 @@
  * @skip Path coverage is broken since symfony/console 5.4.16, 6.0.16, 6.1.8 and 6.2.0
  * https://bugs.xdebug.org/view.php?id=2108
  */
+
 $I = new CoverGuy($scenario);
 $I->wantTo('measure path coverage');
 $I->amGoingTo('run local code coverage with path and branch coverage');
