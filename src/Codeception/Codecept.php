8791a7c5bef338d3c6bb1bfa8cc9f8abb55cf918
Gustavo Nieves <ganieves@outlook.com>
2025-02-15 11:11:55 -0500

    5.2.0

--- src/Codeception/Codecept.php
+++ src/Codeception/Codecept.php
@@ -36,7 +36,7 @@ class Codecept
     /**
      * @var string
      */
-    public const VERSION = '5.1.2';
+    public const VERSION = '5.2.0';
 
     protected ResultAggregator $resultAggregator;
 
