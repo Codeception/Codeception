e653567d59270965e30c9da517b4b7045da702c3
Thomas Landauer <thomas@landauer.at>
2025-11-30 15:02:54 +0100

    Update Cest.php: Minor rewording

--- src/Codeception/Lib/Generator/Cest.php
+++ src/Codeception/Lib/Generator/Cest.php
@@ -25,12 +25,13 @@ final class {{name}}Cest
 {
     public function _before({{actor}} \$I): void
     {
-        // Code here will be executed before each test.
+        // Code here will be executed before each test function.
     }
 
+    // All `public` methods will be executed as tests.
     public function tryToTest({{actor}} \$I): void
     {
-        // Write your tests here. All `public` methods will be executed as tests.
+        // Write your test content here.
     }
 }
 
