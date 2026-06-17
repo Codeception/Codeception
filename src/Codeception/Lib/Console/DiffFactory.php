a0c973e23c52ed9cdb5e66cf798850955165232f
Dieter Beck <beck.worma@gmail.com>
2026-06-17 14:37:47 +0200

    Fix compatibility with sebastian/diff v8.3.0 + v9.0.0 (#6941)
    * Fix compatibility with sebastian/diff v8.3.0
    
    * Add compatibility for sebastian/diff v9

--- src/Codeception/Lib/Console/DiffFactory.php
+++ src/Codeception/Lib/Console/DiffFactory.php
@@ -6,6 +6,7 @@ namespace Codeception\Lib\Console;
 
 use SebastianBergmann\Comparator\ComparisonFailure;
 use SebastianBergmann\Diff\Differ;
+use SebastianBergmann\Diff\Output\StrictUnifiedDiffOutputBuilder;
 use SebastianBergmann\Diff\Output\UnifiedDiffOutputBuilder;
 
 class DiffFactory
@@ -17,7 +18,23 @@ class DiffFactory
 
     private function getDiff(string $expected = '', string $actual = ''): string
     {
-        $differ = new Differ(new UnifiedDiffOutputBuilder(''));
+        // Ensure compatibility with sebastian/diff v8.3.0
+        // v8.3.0 added the parameter $emitNoLineEndEofWarning
+        // v9.0.0 removed the class UnifiedDiffOutputBuilder, therefore
+        // use StrictUnifiedDiffOutputBuilder right away.
+        if (
+            !class_exists(UnifiedDiffOutputBuilder::class)
+            || property_exists(UnifiedDiffOutputBuilder::class, 'emitNoLineEndEofWarning')
+        ) {
+            $outputBuilder = new StrictUnifiedDiffOutputBuilder([
+                'addLineNumbers' => false,
+                'emitNoLineEndEofWarning' => false,
+                'header' => '',
+            ]);
+        } else {
+            $outputBuilder = new UnifiedDiffOutputBuilder('');
+        }
+        $differ = new Differ($outputBuilder);
         return ($expected || $actual) ? $differ->diff($expected, $actual) : '';
     }
 }
