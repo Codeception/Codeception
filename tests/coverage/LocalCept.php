86b64272a2286a4eef45b16074e4b5f0380339ba
W0rma <beck.worma@gmail.com>
2026-09-25 08:27:14 +0200

    Relax expected shell output
    The latest php-code-coverage versions changed the number of whitespaces.

--- tests/coverage/LocalCept.php
+++ tests/coverage/LocalCept.php
@@ -6,15 +6,15 @@ $I = new CoverTester($scenario);
 $I->wantTo('run local code coverage for cest and test');
 $I->amInPath('tests/data/sandbox');
 $I->executeCommand('run math MathTest --coverage', false, '-d pcov.directory=' . getcwd());
-$I->seeInShellOutput('Classes: 100.00%');
-$I->seeInShellOutput('Methods: 100.00%');
+$I->seeShellOutputMatches('#Classes:\s+100\.00\%#');
+$I->seeShellOutputMatches('#Methods:\s+100\.00\%#');
 
 $I->amGoingTo('run local codecoverage in cest');
 $I->executeCommand('run math MathCest --coverage', false, '-d pcov.directory=' . getcwd());
-$I->seeInShellOutput('Classes: 100.00%');
-$I->seeInShellOutput('Methods: 100.00%');
+$I->seeShellOutputMatches('#Classes:\s+100\.00\%#');
+$I->seeShellOutputMatches('#Methods:\s+100\.00\%#');
 
 $I->amGoingTo('run local code coverage with path and branch coverage');
 $I->executeCommand("run -o 'coverage: path_coverage: true' math MathCest --coverage", false);
-$I->seeInShellOutput('Paths:   66.67%');
-$I->seeInShellOutput('Branches:   80.00%');
+$I->seeShellOutputMatches('#Paths:\s+66\.67\%#');
+$I->seeShellOutputMatches('#Branches:\s+80\.00\%#');
